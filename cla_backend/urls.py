4b2ad928c58212f01afbe95c2934d61bbfdaa3a9
Marco Fucci <marcofucci@gmail.com>
2014-02-10 12:38:35 +0000

    added django rest framework to dependencies

--- cla_backend/urls.py
+++ cla_backend/urls.py
@@ -12,4 +12,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # url(r'^admin/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

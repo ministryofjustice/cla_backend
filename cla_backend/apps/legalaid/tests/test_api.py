6cc16c3f415ead0182ba4810f7a002562984d391
Marco Fucci <info@marcofucci.com>
2014-02-13 17:00:37 +0000

    tested eligibility_check endpoints with property_set as data

--- cla_backend/apps/legalaid/tests/test_api.py
+++ cla_backend/apps/legalaid/tests/test_api.py
@@ -17,7 +17,8 @@ category_recipe = Recipe(Category,
 eligibility_check_recipe = Recipe(EligibilityCheck)
 
 property_recipe = Recipe(Property,
-                         eligibility_check=foreign_key(eligibility_check_recipe))
+    eligibility_check=foreign_key(eligibility_check_recipe)
+)
 
 
 class CategoryTests(CLABaseApiTestMixin, APITestCase):
@@ -74,6 +75,20 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
             kwargs={'reference': str(self.check.reference)}
         )
 
+    def test_methods_not_allowed(self):
+        """
+        Ensure that we can't POST, PUT or DELETE
+        """
+        ### LIST
+        self._test_get_not_allowed(self.list_url)
+        self._test_put_not_allowed(self.list_url)
+        self._test_delete_not_allowed(self.list_url)
+
+        ### DETAIL
+        self._test_post_not_allowed(self.detail_url)
+        self._test_delete_not_allowed(self.detail_url)
+
+    # CREATE
 
     def test_create_no_data(self):
         """
@@ -84,12 +99,12 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes'])
+        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes', 'property_set'])
         self.assertTrue(len(response.data['reference']) > 30)
         self.assertEqual(response.data['category'], None)
         self.assertEqual(response.data['notes'], '')
 
-    def test_create_full_data(self):
+    def test_create_basic_data(self):
         """
         CREATE data is not empty
         """
@@ -104,24 +119,70 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes'])
+        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes', 'property_set'])
         self.assertTrue(len(response.data['reference']) > 30)
         self.assertEqual(response.data['category'], data['category'])
         self.assertEqual(response.data['notes'], data['notes'])
+        self.assertEqual(response.data['property_set'], [])
 
-    def test_methods_not_allowed(self):
+    def test_create_with_properties(self):
         """
-        Ensure that we can't POST, PUT or DELETE
+        CREATE data with properties
         """
-        ### LIST
-        self._test_get_not_allowed(self.list_url)
-        self._test_put_not_allowed(self.list_url)
-        self._test_delete_not_allowed(self.list_url)
+        data={
+            'property_set': [
+                {'value': 111, 'equity': 222, 'share': 33},
+                {'value': 999, 'equity': 888, 'share': 77}
+            ]
+        }
+        response = self.client.post(
+            self.list_url, data, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        ### DETAIL
-        self._test_get_not_allowed(self.detail_url)
-        self._test_post_not_allowed(self.detail_url)
-        self._test_delete_not_allowed(self.detail_url)
+        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes', 'property_set'])
+        self.assertTrue(len(response.data['reference']) > 30)
+        self.assertEqual(response.data['category'], None)
+        self.assertEqual(response.data['notes'], '')
+        self.assertEqual(len(response.data['property_set']), 2)
+        self.assertItemsEqual([p['id'] for p in response.data['property_set']], [1,2])
+        self.assertItemsEqual([p['value'] for p in response.data['property_set']], [111, 999])
+        self.assertItemsEqual([p['equity'] for p in response.data['property_set']], [222, 888])
+        self.assertItemsEqual([p['share'] for p in response.data['property_set']], [33, 77])
+
+    # GET OBJECT
+
+    def test_get_not_found(self):
+        """
+        Invalid reference => 404
+        """
+        not_found_detail_url = reverse(
+            'eligibility_check-detail', args=(),
+            kwargs={'reference': 'invalid'}
+        )
+
+        response = self.client.get(not_found_detail_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_object(self):
+        """
+        GET should not return properties of other eligibility check objects
+        """
+        property_recipe.make(eligibility_check=self.check, _quantity=4)
+
+        # making extra properties
+        property_recipe.make(_quantity=5)
+
+        self.assertEqual(Property.objects.count(), 9)
+
+        response = self.client.get(self.detail_url, format='json')
+        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes', 'property_set'])
+        self.assertEqual(response.data['reference'], str(self.check.reference))
+        self.assertEqual(response.data['category'], 1)
+        self.assertEqual(response.data['notes'], 'lorem ipsum')
+        self.assertEqual(len(response.data['property_set']), 4)
+
+    # PATCH
 
     def test_patch_no_data(self):
         """
@@ -132,12 +193,13 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes'])
-        self.assertTrue(response.data['reference'] > self.check.reference)
+        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes', 'property_set'])
+        self.assertEqual(response.data['reference'], str(self.check.reference))
         self.assertEqual(response.data['category'], self.check.category.pk)
         self.assertEqual(response.data['notes'], self.check.notes)
+        self.assertEqual(response.data['property_set'], [])
 
-    def test_patch_full_data(self):
+    def test_patch_basic_data(self):
         """
         PATCH data is not empty so the object should change
         """
@@ -153,31 +215,75 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes'])
         self.assertEqual(response.data['reference'], str(self.check.reference))
         self.assertEqual(response.data['category'], data['category'])
         self.assertEqual(response.data['notes'], data['notes'])
+        self.assertEqual(response.data['property_set'], [])
 
-    def test_put_full_data(self):
+    def test_patch_properties(self):
         """
-        PUT data is not empty so the object should change
+        PATCH should add/remove/change properties.
         """
+        property_recipe.make(eligibility_check=self.check, _quantity=4)
+
+        # making extra properties
+        property_recipe.make(_quantity=5)
+
+        self.assertItemsEqual(self.check.property_set.values_list('id', flat=True), [1,2,3,4])
+
+        # changing property with id == 1, removing all the others and adding
+        # an extra one
+        data={
+            'property_set': [
+                {'value': 111, 'equity': 222, 'share': 33, 'id': 1},
+                {'value': 999, 'equity': 888, 'share': 77}
+            ]
+        }
+        response = self.client.patch(
+            self.detail_url, data=data, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # nothing should have changed here
+        self.assertEqual(response.data['reference'], str(self.check.reference))
+        self.assertEqual(response.data['category'], self.check.category.id)
+        self.assertEqual(response.data['notes'], self.check.notes)
+
+        # properties should have changed. The new property should have id == 10
+        self.assertEqual(len(response.data['property_set']), 2)
+        self.assertItemsEqual([p['id'] for p in response.data['property_set']], [1,10])
+        self.assertItemsEqual([p['value'] for p in response.data['property_set']], [111, 999])
+        self.assertItemsEqual([p['equity'] for p in response.data['property_set']], [222, 888])
+        self.assertItemsEqual([p['share'] for p in response.data['property_set']], [33, 77])
+
+        # checking the db just in case
+        self.assertItemsEqual(self.check.property_set.values_list('id', flat=True), [1,10])
+
+    # PUT
+
+    def test_put_basic_data(self):
+        """
+        PUT should override the values
+        """
+        property_recipe.make(eligibility_check=self.check, _quantity=4)
+
         category2 = category_recipe.make()
 
         data={
             'reference': 'just-trying...', # reference should never change
             'category': category2.pk,
-            'notes': 'lorem ipsum2'
+            'notes': 'lorem2',
+            'property_set': []
         }
         response = self.client.put(
             self.detail_url, data=data, format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes'])
+        self.assertItemsEqual(response.data.keys(), ['reference', 'category', 'notes', 'property_set'])
         self.assertEqual(response.data['reference'], str(self.check.reference))
         self.assertEqual(response.data['category'], data['category'])
-        self.assertEqual(response.data['notes'], data['notes'])
+        self.assertEqual(response.data['notes'], 'lorem2')
+        self.assertEqual(response.data['property_set'], [])
 
 
 class EligibilityCheckPropertyTests(CLABaseApiTestMixin, APITestCase):

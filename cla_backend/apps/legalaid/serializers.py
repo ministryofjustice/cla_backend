ef16cfff337a8098d37ab4a3fbf39c7a4c3dc6a9
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-02-14 14:22:05 +0000

    allow updating your and partner finances from a post to eligibility_check

--- cla_backend/apps/legalaid/serializers.py
+++ cla_backend/apps/legalaid/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category, EligibilityCheck, Property
+from .models import Category, EligibilityCheck, Property, Finance
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
@@ -15,12 +15,35 @@ class PropertySerializer(serializers.ModelSerializer):
         model = Property
         fields = ('value', 'equity', 'share', 'id')
 
+class FinanceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Finance
+        fields = (
+            "bank_balance",
+            "investment_balance",
+            "asset_balance",
+            "credit_balance",
+            "earnings",
+            "other_income",
+            "self_employed"
+        )
+
 
 class EligibilityCheckSerializer(serializers.ModelSerializer):
     notes = serializers.CharField(max_length=500, required=False)
     property_set = PropertySerializer(allow_add_remove=True, many=True, required=False)
+    your_finances = FinanceSerializer(required=False)
+    partner_finances = FinanceSerializer(required=False)
 
     class Meta:
         model = EligibilityCheck
-        fields = ('reference', 'category', 'notes', 'property_set')
+        fields = (
+            'reference',
+            'category',
+            'notes',
+            'property_set',
+            'your_finances',
+            'partner_finances'
+        )
 

a36332d50777fd47bbc5b087676363a1dc1ef890
Marco Fucci <info@marcofucci.com>
2014-02-17 10:28:40 +0000

    tested endpoints with finances data

--- cla_backend/apps/legalaid/tests/test_api.py
+++ cla_backend/apps/legalaid/tests/test_api.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APITransactionTestCase
 
-from ..models import Category, EligibilityCheck, Property
+from ..models import Category, EligibilityCheck, Property, Finance
 
 from .test_base import CLABaseApiTestMixin
 
@@ -15,13 +15,18 @@ category_recipe = Recipe(Category,
     name=seq('Name'), order = seq(0)
 )
 
+finance_recipe = Recipe(Finance)
+
 eligibility_check_recipe = Recipe(EligibilityCheck,
-    dependants_young=5, dependants_old=6
+    dependants_young=5, dependants_old=6,
+    your_finances=foreign_key(finance_recipe),
+    partner_finances=foreign_key(finance_recipe)
 )
 
 property_recipe = Recipe(Property,
     eligibility_check=foreign_key(eligibility_check_recipe)
 )
+
 
 
 class CategoryTests(CLABaseApiTestMixin, APITestCase):
@@ -87,6 +92,23 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
              'partner_finances']
         )
 
+    def assertFinanceEqual(self, data, obj):
+        if data is None or obj is None:
+            self.assertEqual(data, obj)
+        else:
+            for prop in ['bank_balance', 'investment_balance', 'asset_balance', 'credit_balance', 'earnings', 'other_income', 'self_employed']:
+                self.assertEqual(getattr(obj, prop), data[prop])
+
+    def assertEligibilityCheckEqual(self, data, check):
+        self.assertEqual(data['reference'], str(check.reference))
+        self.assertEqual(data['category'], check.category.id if check.category else None)
+        self.assertEqual(data['notes'], check.notes)
+        self.assertEqual(len(data['property_set']), check.property_set.count())
+        self.assertEqual(data['dependants_young'], check.dependants_young)
+        self.assertEqual(data['dependants_old'], check.dependants_old)
+        self.assertFinanceEqual(data['your_finances'], check.your_finances)
+        self.assertFinanceEqual(data['partner_finances'], check.partner_finances)
+
     def test_methods_not_allowed(self):
         """
         Ensure that we can't POST, PUT or DELETE
@@ -113,10 +135,11 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
 
         self.assertResponseKeys(response)
         self.assertTrue(len(response.data['reference']) > 30)
-        self.assertEqual(response.data['category'], None)
-        self.assertEqual(response.data['notes'], '')
-        self.assertEqual(response.data['dependants_young'], 0)
-        self.assertEqual(response.data['dependants_old'], 0)
+        self.assertEligibilityCheckEqual(response.data,
+            EligibilityCheck(
+                reference=response.data['reference']
+            )
+        )
 
     def test_create_basic_data(self):
         """
@@ -124,8 +147,9 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         """
         category_recipe.make()
 
+        category = Category.objects.all()[0]
         data={
-            'category': Category.objects.all()[0].pk,
+            'category': category.pk,
             'notes': 'lorem',
             'dependants_young': 2,
             'dependants_old': 3,
@@ -137,11 +161,13 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
 
         self.assertResponseKeys(response)
         self.assertTrue(len(response.data['reference']) > 30)
-        self.assertEqual(response.data['category'], data['category'])
-        self.assertEqual(response.data['notes'], data['notes'])
-        self.assertEqual(response.data['property_set'], [])
-        self.assertEqual(response.data['dependants_young'], 2)
-        self.assertEqual(response.data['dependants_old'], 3)
+        self.assertEligibilityCheckEqual(response.data,
+            EligibilityCheck(
+                reference=response.data['reference'],
+                category=category, notes=data['notes'],
+                dependants_young=2, dependants_old=3
+            )
+        )
 
     def test_create_with_properties(self):
         """
@@ -170,7 +196,50 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         self.assertItemsEqual([p['equity'] for p in response.data['property_set']], [222, 888])
         self.assertItemsEqual([p['share'] for p in response.data['property_set']], [33, 77])
 
-    def test_create_in_error(self):
+    def test_create_with_finances(self):
+        """
+        CREATE data with finances
+        """
+        data={
+            'your_finances': {
+                "bank_balance": 100,
+                "investment_balance": 200,
+                "asset_balance": 300,
+                "credit_balance": 400,
+                "earnings": 500,
+                "other_income": 600,
+                "self_employed": True
+            },
+            'partner_finances': {
+                "bank_balance": 1000,
+                "investment_balance": 2000,
+                "asset_balance": 3000,
+                "credit_balance": 4000,
+                "earnings": 5000,
+                "other_income": 6000,
+                "self_employed": False
+            },
+        }
+        response = self.client.post(
+            self.list_url, data, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertResponseKeys(response)
+        self.assertTrue(len(response.data['reference']) > 30)
+        self.assertEqual(response.data['category'], None)
+        self.assertEqual(response.data['notes'], '')
+        self.assertEqual(len(response.data['property_set']), 0)
+        self.assertEqual(response.data['dependants_young'], 0)
+        self.assertEqual(response.data['dependants_old'], 0)
+        self.assertDictEqual(response.data['your_finances'], data['your_finances'])
+        self.assertDictEqual(response.data['partner_finances'], data['partner_finances'])
+
+    def _test_method_in_error(self, method, url):
+        """
+        Generic method called by 'create' and 'patch' to test against validation
+        errors.
+        """
         data={
             'category': -1,
             'notes': 'a'*501,
@@ -181,14 +250,36 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
             ],
             'dependants_young': -1,
             'dependants_old': -1,
+            'your_finances': {
+                "bank_balance": -1,
+                "investment_balance": -1,
+                "asset_balance": -1,
+                "credit_balance": -1,
+                "earnings": -1,
+                "other_income": -1
+            },
+            'partner_finances': {
+                "bank_balance": -1,
+                "investment_balance": -1,
+                "asset_balance": -1,
+                "credit_balance": -1,
+                "earnings": -1,
+                "other_income": -1
+            },
         }
-        response = self.client.post(
-            self.list_url, data, format='json'
-        )
+
+        method_callable = getattr(self.client, method)
+        response = method_callable(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         errors = response.data
-        self.assertItemsEqual(errors.keys(), ['category', 'notes', 'property_set', 'dependants_young', 'dependants_old'])
+        self.assertItemsEqual(
+            errors.keys(),
+            [
+                'category', 'notes', 'property_set', 'dependants_young',
+                'dependants_old', 'your_finances', 'partner_finances'
+            ]
+        )
         self.assertEqual(errors['category'], [u"Invalid pk '-1' - object does not exist."])
         self.assertEqual(errors['notes'], [u'Ensure this value has at most 500 characters (it has 501).'])
         self.assertItemsEqual(errors['property_set'], [
@@ -202,6 +293,35 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         ])
         self.assertEqual(errors['dependants_young'], [u'Ensure this value is greater than or equal to 0.'])
         self.assertEqual(errors['dependants_old'], [u'Ensure this value is greater than or equal to 0.'])
+        self.assertItemsEqual(
+            errors['your_finances'],
+            [
+                {
+                    'credit_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'asset_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'investment_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'earnings': [u'Ensure this value is greater than or equal to 0.'],
+                    'bank_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'other_income': [u'Ensure this value is greater than or equal to 0.']
+                }
+            ]
+        )
+        self.assertItemsEqual(
+            errors['partner_finances'],
+            [
+                {
+                    'credit_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'asset_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'investment_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'earnings': [u'Ensure this value is greater than or equal to 0.'],
+                    'bank_balance': [u'Ensure this value is greater than or equal to 0.'],
+                    'other_income': [u'Ensure this value is greater than or equal to 0.']
+                }
+            ]
+        )
+
+    def test_create_in_error(self):
+        self._test_method_in_error('post', self.list_url)
 
     # GET OBJECT
 
@@ -230,13 +350,7 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
 
         response = self.client.get(self.detail_url, format='json')
         self.assertResponseKeys(response)
-        self.assertEqual(response.data['reference'], str(self.check.reference))
-        self.assertEqual(response.data['category'], 1)
-        self.assertEqual(response.data['notes'], 'lorem ipsum')
-        self.assertEqual(len(response.data['property_set']), 4)
-        self.assertEqual(len(response.data['property_set']), 4)
-        self.assertEqual(response.data['dependants_young'], 5)
-        self.assertEqual(response.data['dependants_old'], 6)
+        self.assertEligibilityCheckEqual(response.data, self.check)
 
     # PATCH
 
@@ -250,12 +364,7 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertResponseKeys(response)
-        self.assertEqual(response.data['reference'], str(self.check.reference))
-        self.assertEqual(response.data['category'], self.check.category.pk)
-        self.assertEqual(response.data['notes'], self.check.notes)
-        self.assertEqual(response.data['property_set'], [])
-        self.assertEqual(response.data['dependants_young'], self.check.dependants_young)
-        self.assertEqual(response.data['dependants_old'], self.check.dependants_old)
+        self.assertEligibilityCheckEqual(response.data, self.check)
 
     def test_patch_basic_data(self):
         """
@@ -281,6 +390,8 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         self.assertEqual(response.data['property_set'], [])
         self.assertEqual(response.data['dependants_young'], data['dependants_young'])
         self.assertEqual(response.data['dependants_old'], data['dependants_old'])
+        self.assertFinanceEqual(response.data['your_finances'], Finance())
+        self.assertFinanceEqual(response.data['partner_finances'], Finance())
 
     def test_patch_properties(self):
         """
@@ -322,38 +433,46 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         # checking the db just in case
         self.assertItemsEqual(self.check.property_set.values_list('id', flat=True), [1,10])
 
-    def test_patch_in_error(self):
+    def test_patch_with_finances(self):
+        """
+        PATCH should change finances.
+        """
         data={
-            'category': -1,
-            'notes': 'a'*501,
-            'property_set': [
-                {'value': 111, 'equity': 222, 'share': 33},  # valid
-                {'value': -1, 'equity': -1, 'share': -1},  # invalid
-                {'value': 0, 'equity': 0, 'share': 101},  # invalid
-            ],
-            'dependants_young': -1,
-            'dependants_old': -1,
+            'your_finances': {
+                "bank_balance": 100,
+                "investment_balance": 200,
+                "asset_balance": 300,
+                "credit_balance": 400,
+                "earnings": 500,
+                "other_income": 600,
+                "self_employed": True
+            },
+            'partner_finances': {
+                "bank_balance": 1000,
+                "investment_balance": 2000,
+                "asset_balance": 3000,
+                "credit_balance": 4000,
+                "earnings": 5000,
+                "other_income": 6000,
+                "self_employed": False
+            },
         }
         response = self.client.patch(
             self.detail_url, data, format='json'
         )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        errors = response.data
-        self.assertItemsEqual(errors.keys(), ['category', 'notes', 'property_set', 'dependants_young', 'dependants_old'])
-        self.assertEqual(errors['category'], [u"Invalid pk '-1' - object does not exist."])
-        self.assertEqual(errors['notes'], [u'Ensure this value has at most 500 characters (it has 501).'])
-        self.assertItemsEqual(errors['property_set'], [
-            {},
-            {
-                'share': [u'Ensure this value is greater than or equal to 0.'],
-                'value': [u'Ensure this value is greater than or equal to 0.'],
-                'equity': [u'Ensure this value is greater than or equal to 0.']
-            },
-            {'share': [u'Ensure this value is less than or equal to 100.']}
-        ])
-        self.assertEqual(errors['dependants_young'], [u'Ensure this value is greater than or equal to 0.'])
-        self.assertEqual(errors['dependants_old'], [u'Ensure this value is greater than or equal to 0.'])
+        self.assertEqual(response.data['reference'], str(self.check.reference))
+        self.assertEqual(response.data['category'], self.check.category.id)
+        self.assertEqual(response.data['notes'], self.check.notes)
+        self.assertEqual(response.data['dependants_young'], self.check.dependants_young)
+        self.assertEqual(response.data['dependants_old'], self.check.dependants_old)
+
+        self.assertDictEqual(response.data['your_finances'], data['your_finances'])
+        self.assertDictEqual(response.data['partner_finances'], data['partner_finances'])
+
+    def test_patch_in_error(self):
+        self._test_method_in_error('patch', self.detail_url)
 
     def test_others_property_cannot_be_set(self):
         """
@@ -407,6 +526,8 @@ class EligibilityCheckTests(CLABaseApiTestMixin, APITestCase):
         self.assertEqual(response.data['property_set'], [])
         self.assertEqual(response.data['dependants_young'], data['dependants_young'])
         self.assertEqual(response.data['dependants_old'], data['dependants_old'])
+        self.assertFinanceEqual(response.data['your_finances'], Finance())
+        self.assertFinanceEqual(response.data['partner_finances'], Finance())
 
 
 class EligibilityCheckPropertyTests(CLABaseApiTestMixin, APITestCase):

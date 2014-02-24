16f159407c377a66b2fe1408530896574e964af2
Marco Fucci <info@marcofucci.com>
2014-02-24 09:43:16 +0000

    added integration settings

---
+++ cla_backend/settings/integration.py
@@ -0,0 +1,15 @@
+from .base import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Marco Fucci', 'marco.fucci@digital.justice.co.uk'),
+    ('Rai Kotecha', 'ravi.kotecha@digital.justice.gov.uk'),
+)
+
+MANAGERS = ADMINS
+
+
+#HOST_NAME = "http://"

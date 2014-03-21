8eedbbab603f62f7f20ac8767207d113f702751e
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-03-21 12:14:25 +0000

    remove todo list

--- cla_backend/apps/checker/__init__.py
+++
@@ -1,4 +0,0 @@
-""" TODO:
- * test the authentication for call_centre/operator. 3 levels, client has some permissions, user at client has
- subset of those permissions, user at client with superuser has different permissions.
-"""

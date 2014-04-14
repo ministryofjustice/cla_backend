dcb01f8c1cd7cdfcf28f6e24d77f2119fbd2f86f
Ravi Kotecha <kotecha.ravi@gmail.com>
2014-04-14 10:48:58 +0100

    security changes for API

--- cla_backend/apps/call_centre/views.py
+++ cla_backend/apps/call_centre/views.py
@@ -1,18 +1,23 @@
 import json
 from call_centre.forms import ProviderAllocationForm
+from call_centre.permissions import CallCentreClientIDPermission
 from call_centre.serializers import EligibilityCheckSerializer, CategorySerializer, \
     CaseSerializer, ProviderSerializer
-from cla_common.constants import CASE_STATE_CLOSED, CASE_STATE_OPEN
+from cla_common.constants import CASE_STATE_CLOSED, CASE_STATE_OPEN, \
+    CASE_STATE_CHOICES
 from cla_provider.models import Provider
 from django import http
 from django.contrib.auth.models import AnonymousUser
-from django.shortcuts import redirect
 from legalaid.models import Category, EligibilityCheck, Case
 from rest_framework import viewsets, mixins
-from rest_framework.decorators import action
+# from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 
-class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+class CallCentrePermissionsViewSetMixin(object):
+    permission_classes = (CallCentreClientIDPermission,)
+
+class CategoryViewSet(CallCentrePermissionsViewSetMixin, viewsets.ReadOnlyModelViewSet):
     model = Category
     serializer_class = CategorySerializer
 
@@ -20,6 +25,7 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class EligibilityCheckViewSet(
+    CallCentrePermissionsViewSetMixin,
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
     mixins.RetrieveModelMixin,
@@ -32,21 +38,37 @@ class EligibilityCheckViewSet(
 
 
 class CaseViewSet(
+    CallCentrePermissionsViewSetMixin,
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
     mixins.RetrieveModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet
 ):
-    queryset = Case.objects.filter(state=CASE_STATE_OPEN, provider=None).order_by('-modified')
+    queryset = Case.objects.filter(state=CASE_STATE_OPEN, provider=None)
     model = Case
     lookup_field = 'reference'
     serializer_class = CaseSerializer
+
+    filter_backends = (
+        OrderingFilter,
+        SearchFilter,
+    )
+
+    ordering_fields = ('modified', 'created')
+    ordering = '-modified'
+
+    search_fields = ('personal_details__full_name',
+                     'personal_details__postcode',)
+
+    default_state_filter = CASE_STATE_OPEN
+    all_states = dict(CASE_STATE_CHOICES).keys()
 
     def pre_save(self, obj):
         user = self.request.user
         if not obj.pk and not isinstance(user, AnonymousUser):
             obj.created_by = user
+
 
     # TODO: this is not needed yet - front end can handle \
     # this logic for now.
@@ -72,7 +94,7 @@ class CaseViewSet(
     #     return http.HttpResponseBadRequest(content=json.dumps(form.errors))
 
 
-class ProviderViewSet(viewsets.ReadOnlyModelViewSet):
+class ProviderViewSet(CallCentrePermissionsViewSetMixin, viewsets.ReadOnlyModelViewSet):
     model = Provider
     serializer_class = ProviderSerializer
 

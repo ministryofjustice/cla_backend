532dd8c47e5c9a36164b5b37e86c1dc960f732a9
Si Parker <si.parker@digital.justice.gov.uk>
2014-06-23 14:42:56 +0100

    added: laa_reference field & eligibility_state lookup to serialiser for call_centre

--- cla_backend/apps/call_centre/serializers.py
+++ cla_backend/apps/call_centre/serializers.py
@@ -114,6 +114,7 @@ class CaseSerializer(CaseSerializerBase):
     provider_notes = serializers.CharField(max_length=500, required=False, read_only=True)
     caseoutcome_set = serializers.SerializerMethodField('get_caseoutcome_set')
     full_name = serializers.CharField(source='personal_details.full_name', read_only=True)
+    eligibility_state = serializers.CharField(source='eligibility_check.state', read_only=True)
 
     def get_caseoutcome_set(self, case):
         case_outcomes = case.caselog_set.filter(logtype__subtype=CASELOGTYPE_SUBTYPES.OUTCOME)
@@ -122,9 +123,10 @@ class CaseSerializer(CaseSerializerBase):
 
     class Meta(CaseSerializerBase.Meta):
         fields = (
-            'eligibility_check', 'personal_details',
-            'reference', 'created', 'modified', 'created_by', 'state',
-            'provider', 'caseoutcome_set', 'notes', 'provider_notes', 'in_scope', 'full_name'
+            'eligibility_check', 'personal_details', 'reference', 'created',
+            'modified', 'created_by', 'state', 'provider', 'caseoutcome_set',
+            'notes', 'provider_notes', 'in_scope', 'full_name', 'laa_reference',
+            'eligibility_state'
         )
 
 
--- cla_backend/apps/call_centre/views.py
+++ cla_backend/apps/call_centre/views.py
@@ -91,8 +91,8 @@ class CaseViewSet(
 
     search_fields = ('personal_details__full_name',
                      'personal_details__postcode',
-                     'reference')
-
+                     'reference',
+                     'laa_reference')
     paginate_by = 20
     paginate_by_param = 'page_size'
     max_paginate_by = 100

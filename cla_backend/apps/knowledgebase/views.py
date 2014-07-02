d8b9a225aac9d0f1f5b0e66e740ac19399faf0bf
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-07-02 16:34:28 +0100

    Added basic knowledgebase search with search= querystring param on /call_centre/api/v1/knowledgebase/ REST method

--- cla_backend/apps/knowledgebase/views.py
+++ cla_backend/apps/knowledgebase/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+#from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework import filters
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -6,7 +8,15 @@ from call_centre.views import CallCentrePermissionsViewSetMixin
 
 
 class ArticleViewSet(
-    CallCentrePermissionsViewSetMixin,
-    viewsets.ReadOnlyModelViewSet):
+        CallCentrePermissionsViewSetMixin,
+        viewsets.ReadOnlyModelViewSet):
     model = Article
     serializer_class = ArticleSerializer
+
+    filter_backends = (
+        filters.SearchFilter,
+    )
+
+    search_fields = ('organisation', 'service_name', 'description',
+                     'keywords', 'when_to_use', 'type_of_service',
+                     'address', 'article_category__name')

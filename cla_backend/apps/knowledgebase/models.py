0d6543e3202ab53a4a5cd56ac0aa368afb3dcd17
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-07-02 11:20:45 +0100

    PEP8 formatting

--- cla_backend/apps/knowledgebase/models.py
+++ cla_backend/apps/knowledgebase/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 
+
 class Article(TimeStampedModel):
 
     organisation = models.CharField(max_length=255, null=True, blank=True)
@@ -12,20 +13,24 @@ class Article(TimeStampedModel):
     when_to_use = models.TextField(blank=True, null=True)
     helpline = models.CharField(max_length=255, null=True, blank=True)
 
-    geographic_coverage = models.CharField(max_length=255, null=True, blank=True)
+    geographic_coverage = models.CharField(
+        max_length=255, null=True, blank=True)
     type_of_service = models.TextField(blank=True, null=True)
     resource_type = models.CharField(max_length=10,
-                                     choices=(('LEGAL', 'Legal'), ('OTHER', 'Other')))
+                                     choices=(('LEGAL', 'Legal'),
+                                              ('OTHER', 'Other')))
     address = models.TextField(blank=True, null=True)
     opening_hours = models.CharField(max_length=255, null=True, blank=True)
     how_to_use = models.TextField(blank=True, null=True)
     accessibility = models.CharField(max_length=255, null=True, blank=True)
 
     article_category = models.ManyToManyField('ArticleCategory',
-                                          through='ArticleCategoryMatrix')
+                                              through='ArticleCategoryMatrix')
+
 
 class ArticleCategory(TimeStampedModel):
     name = models.CharField(max_length=25)
+
 
 class ArticleCategoryMatrix(TimeStampedModel):
     article = models.ForeignKey(Article)

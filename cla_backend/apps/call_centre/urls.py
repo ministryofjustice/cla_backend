97ae875022e69e297356da4b61e18411342b4a01
Marco Fucci <info@marcofucci.com>
2014-07-08 09:36:20 +0100

    made TimerRouter generic

--- cla_backend/apps/call_centre/urls.py
+++ cla_backend/apps/call_centre/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 
 from rest_framework import routers
-from timer import routers as timer_routers
+from core import routers as core_routers
 
 from . import views
 
@@ -9,7 +9,8 @@ router = routers.DefaultRouter()
 router.register(r'category', views.CategoryViewSet)
 router.register(r'provider/rota', views.OutOfHoursRotaViewSet)
 router.register(r'provider', views.ProviderViewSet)
-router.register(r'eligibility_check', views.EligibilityCheckViewSet, base_name='eligibility_check')
+router.register(r'eligibility_check', views.EligibilityCheckViewSet,
+                base_name='eligibility_check')
 router.register(r'case', views.CaseViewSet)
 router.register(r'user', views.UserViewSet, base_name='user')
 router.register(r'personal_details', views.PersonalDetailsViewSet)
@@ -19,7 +20,7 @@ router.register(r'adaptation_details', views.AdaptationDetailsViewSet)
 router.register(r'knowledgebase/article', views.ArticleViewSet)
 router.register(r'knowledgebase/category', views.ArticleCategoryViewSet)
 
-timer_router = timer_routers.TimerRouter()
+timer_router = core_routers.SingletonRouter()
 timer_router.register(r'timer', views.TimerViewSet, base_name='timer')
 
 urlpatterns = patterns('',

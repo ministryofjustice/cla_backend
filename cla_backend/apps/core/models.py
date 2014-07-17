8e1a55ea6f9d8ae20cd8b900a414ba662b1785f7
Marco Fucci <info@marcofucci.com>
2014-07-17 09:43:42 +0100

    deleted fixture, using get_or_create instead to avoid User.id collision. Added unit tests

--- cla_backend/apps/core/models.py
+++ cla_backend/apps/core/models.py
@@ -2,4 +2,8 @@ from django.contrib.auth.models import User
 
 
 def get_web_user():
-    return User.objects.get(username='web')
+    web_user, created = User.objects.get_or_create(username='web')
+    if created:
+        web_user.set_unusable_password()
+        web_user.save()
+    return web_user

555a587efe5644fbd999da4903fd4d205182a54b
Marco Fucci <info@marcofucci.com>
2014-08-12 11:00:47 +0100

    added tests for adaptation details endpoints

--- cla_backend/apps/call_centre/tests/api/test_adaptations_api.py
+++ cla_backend/apps/call_centre/tests/api/test_adaptations_api.py
@@ -2,7 +2,7 @@ from core.tests.test_base import CLAOperatorAuthBaseApiTestMixin
 from rest_framework.test import APITestCase
 
 from legalaid.tests.views.mixins.adaptations_api import \
-    AdaptationsMetadataAPIMixin
+    AdaptationsMetadataAPIMixin, AdaptationsDetailsAPIMixin
 
 
 class AdaptationsMetadataTestCase(
@@ -10,3 +10,12 @@ class AdaptationsMetadataTestCase(
     APITestCase
 ):
     API_URL_NAMESPACE = 'call_centre'
+
+
+class AdaptationsDetailsTestCase(
+    CLAOperatorAuthBaseApiTestMixin, AdaptationsDetailsAPIMixin,
+    APITestCase
+):
+
+    def get_http_authorization(self):
+        return 'Bearer %s' % self.token
--- cla_backend/apps/cla_provider/tests/api/test_adaptations_api.py
+++ cla_backend/apps/cla_provider/tests/api/test_adaptations_api.py
@@ -1,8 +1,13 @@
-from core.tests.test_base import CLAProviderAuthBaseApiTestMixin
 from rest_framework.test import APITestCase
+from rest_framework import status
+
+from core.tests.mommy_utils import make_recipe
+from core.tests.test_base import CLAProviderAuthBaseApiTestMixin
+
+from cla_common.constants import REQUIRES_ACTION_BY
 
 from legalaid.tests.views.mixins.adaptations_api import \
-    AdaptationsMetadataAPIMixin
+    AdaptationsMetadataAPIMixin, AdaptationsDetailsAPIMixin
 
 
 class AdaptationsMetadataTestCase(
@@ -10,3 +15,44 @@ class AdaptationsMetadataTestCase(
     APITestCase
 ):
     API_URL_NAMESPACE = 'cla_provider'
+
+
+class AdaptationsDetailsTestCase(
+    CLAProviderAuthBaseApiTestMixin, AdaptationsDetailsAPIMixin,
+    APITestCase
+):
+
+    def setUp(self):
+        super(AdaptationsDetailsTestCase, self).setUp()
+
+        self.check_case.provider = self.provider
+        self.check_case.requires_action_by = REQUIRES_ACTION_BY.PROVIDER
+        self.check_case.save()
+
+    def get_http_authorization(self):
+        return 'Bearer %s' % self.staff_token
+
+    # SECURITY
+
+    def test_get_not_found_if_not_belonging_to_provider(self):
+        self.check_case.provider = None
+        self.check_case.requires_action_by = REQUIRES_ACTION_BY.OPERATOR
+        self.check_case.save()
+
+        response = self.client.get(
+            self.detail_url, format='json',
+            HTTP_AUTHORIZATION=self.get_http_authorization()
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_not_found_if_belonging_to_different_provider(self):
+        other_provider = make_recipe('cla_provider.provider')
+
+        self.check_case.provider = other_provider
+        self.check_case.save()
+
+        response = self.client.get(
+            self.detail_url, format='json',
+            HTTP_AUTHORIZATION=self.get_http_authorization()
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

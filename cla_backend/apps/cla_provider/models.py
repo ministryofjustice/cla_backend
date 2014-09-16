5f1e44db82110003bb7502c219a0c9be6f76130c
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2014-09-16 17:17:50 +0100

    Added cla_provider.Staff creation/modification logging and email alerts

--- cla_backend/apps/cla_provider/models.py
+++ cla_backend/apps/cla_provider/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.db.models import Q
 from django.utils import timezone
 from django.utils.translation import ugettext as _
+from django.db.models.signals import post_save, pre_save
+
+from .signals import log_staff_created, log_staff_modified
 
 from model_utils.models import TimeStampedModel
 from uuidfield import UUIDField
@@ -125,3 +128,5 @@ class Feedback(TimeStampedModel):
     issue = models.CharField(choices=FEEDBACK_ISSUE, max_length=100)
 
 
+post_save.connect(log_staff_created, sender=Staff)
+pre_save.connect(log_staff_modified, sender=Staff)
---
+++ cla_backend/apps/cla_provider/signals.py
@@ -0,0 +1,70 @@
+import logging
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.timezone import now, localtime
+from django.utils.formats import date_format
+
+from django_statsd.clients import statsd
+
+logger = logging.getLogger(__name__)
+
+
+email_template = u"""
+Specialist user {0} at {1}:
+
+Username: {2}
+Provider: {3}
+Is manager: {4}
+"""
+
+
+def log_staff_created(sender, instance, created, **kwargs):
+    if created:
+        statsd.incr('specialiststaff.created')
+
+        logger.info('Specialist user created', extra={
+            'USERNAME': instance.user.username,
+            'PROVIDER': instance.provider.name,
+            'IS_MANAGER': unicode(instance.is_manager)
+        })
+
+        message = email_template.format(
+            'created',
+            date_format(localtime(now()), 'SHORT_DATETIME_FORMAT'),
+            instance.user.username,
+            instance.provider.name,
+            unicode(instance.is_manager),
+        )
+        send_mail('Specialist user added', message,
+                  settings.EMAIL_FROM_ADDRESS,
+                  settings.OPERATOR_USER_ALERT_EMAILS,
+                  fail_silently=True)
+
+
+def log_staff_modified(sender, instance, **kwargs):
+    try:
+        sender.objects.get(pk=instance.pk)
+    except sender.DoesNotExist:
+        return
+
+    statsd.incr('specialiststaff.modified')
+
+    logger.info('Specialist user modified', extra={
+        'USERNAME': instance.user.username,
+        'PROVIDER': instance.provider.name,
+        'IS_MANAGER': unicode(instance.is_manager)
+    })
+
+    message = email_template.format(
+        'modified',
+        date_format(localtime(now()), 'SHORT_DATETIME_FORMAT'),
+        instance.user.username,
+        instance.provider.name,
+        unicode(instance.is_manager),
+    )
+    send_mail('Specialist user modified', message,
+              settings.EMAIL_FROM_ADDRESS,
+              settings.OPERATOR_USER_ALERT_EMAILS,
+              fail_silently=True)
+

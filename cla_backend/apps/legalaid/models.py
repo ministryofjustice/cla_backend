5c55184000537f19de435803b9a43dcdea80f3f3
Andy Driver <andy.driver@digital.justice.gov.uk>
2014-09-24 15:58:06 +0100

    Add outcome code priority model for sorting cases by outcome code

--- cla_backend/apps/legalaid/models.py
+++ cla_backend/apps/legalaid/models.py
@@ -457,6 +457,14 @@ class MediaCode(TimeStampedModel):
     code = models.CharField(max_length=20)
 
 
+class OutcomeCodePriority(models.Model):
+    code = models.CharField(max_length=20, primary_key=True)
+    priority = models.IntegerField(default=1)
+
+    class Meta:
+        ordering = ['-priority']
+
+
 class Case(TimeStampedModel):
     reference = models.CharField(max_length=128, unique=True, editable=False)
     eligibility_check = models.OneToOneField(EligibilityCheck, null=True,

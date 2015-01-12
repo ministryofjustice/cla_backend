aeb876fbc116d060dcc8f3e1c39b63d8ee7ee42c
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2015-01-12 16:35:41 +0000

    Removed more sensitive info

--- cla_backend/apps/call_centre/sql/ExportProviderStaff.sql
+++ cla_backend/apps/call_centre/sql/ExportProviderStaff.sql
@@ -5,8 +5,8 @@ modified,
 user_id,
 provider_id,
 is_manager,
-chs_organisation,
-chs_user,
+'[deleted]' AS chs_organisation,
+'[deleted]' AS chs_user,
 '[deleted]' AS chs_password
 FROM cla_provider_staff
 WHERE modified >= %s::timestamp AND modified <= %s::timestamp)

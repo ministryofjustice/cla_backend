09edac4831f4509eef98e43f40c75f71927b7e6b
Ravi Kotecha <kotecha.ravi@gmail.com>
2015-01-14 12:37:02 +0000

    In addition to the sla_120 and sla_15 fields, add a sla_480 field on CBs
    This allows us to report on sms and voicemail cases breaking their 8hour
    sla.

--- cla_backend/apps/legalaid/forms.py
+++ cla_backend/apps/legalaid/forms.py
@@ -13,25 +13,26 @@ def is_in_business_hours(dt):
         dt = timezone.make_aware(dt, timezone.get_default_timezone())
 
     OPERATOR_HOURS = OpeningHours(**settings.OPERATOR_HOURS)
-    in_business_hours = dt in OPERATOR_HOURS
-    if in_business_hours:
-        return (True, None)
-    else:
-        available_slots = time_slots(dt.date())
-        remainder = timedelta(minutes=15)
-        if available_slots:
-            end_of_day = timezone.make_aware(available_slots[-1] + timedelta(minutes=15), timezone.get_default_timezone())
-            remainder = dt - end_of_day
+    return dt in OPERATOR_HOURS
 
-        return (False, remainder)
+def get_remainder_from_end_of_day(day, dt):
+    available_slots = time_slots(day)
+    remainder = timedelta(minutes=15)
+    if available_slots:
+        end_of_day = timezone.make_aware(available_slots[-1] + timedelta(minutes=15), timezone.get_default_timezone())
+        remainder = dt - end_of_day
+    assert remainder >= timedelta(minutes=0)
+    return remainder
+
 
 def get_sla_time(start_time, minutes):
     simple_delta = start_time + timedelta(minutes=minutes)
-    in_business_hours, remainder_delta = is_in_business_hours(simple_delta)
+    in_business_hours = is_in_business_hours(simple_delta)
     if not in_business_hours:
+        remainder_delta = get_remainder_from_end_of_day(start_time.date(), simple_delta)
         next_business_day = filter(lambda x: x.date() > start_time.date(), available_days(365))[0]
         start_of_next_business_day = timezone.make_aware(time_slots(next_business_day.date())[0], timezone.get_default_timezone())
-        return start_of_next_business_day + remainder_delta
+        return get_sla_time(start_of_next_business_day, remainder_delta.total_seconds() // 60 )
 
     return simple_delta
 
@@ -47,7 +48,8 @@ class BaseCallMeBackForm(BaseCaseLogForm):
         return {
             'requires_action_at': requires_action_at,
             'sla_15': get_sla_time(requires_action_at, 15),
-            'sla_120': get_sla_time(requires_action_at, 120)
+            'sla_120': get_sla_time(requires_action_at, 120),
+            'sla_480': get_sla_time(requires_action_at, 480)
         }
 
     def get_notes(self):

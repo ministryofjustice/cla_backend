f678f0eb91c82674e9b34f4f2ce8e0fbcb49bc7d
Josh Rowe <josh@s-block.com>
2015-01-27 12:22:01 +0000

    add public_description to admin

--- cla_backend/apps/knowledgebase/admin.py
+++ cla_backend/apps/knowledgebase/admin.py
@@ -18,9 +18,9 @@ class ArticleAdmin(admin.ModelAdmin):
 
     fields = (
         'resource_type', 'service_name', 'service_tag', 'organisation',
-        'website', 'email', 'description', 'how_to_use', 'when_to_use',
-        'address', 'opening_hours', 'keywords', 'geographic_coverage',
-        'type_of_service', 'accessibility'
+        'website', 'email', 'description', 'public_description', 'how_to_use',
+        'when_to_use', 'address', 'opening_hours', 'keywords',
+        'geographic_coverage', 'type_of_service', 'accessibility'
     )
     list_display = (
         'service_name', 'resource_type'

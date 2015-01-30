9a277b25c1513bf11634bc8e2d1837a41e41ea19
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2015-01-30 11:15:52 +0000

    Fixed OBIEE export test

--- cla_backend/apps/call_centre/tests/test_views.py
+++ cla_backend/apps/call_centre/tests/test_views.py
@@ -1,31 +1,38 @@
 from django.test import TestCase
-from django.conf.urls import patterns, url
 
-from rest_framework import permissions
 from rest_framework import status
 
-from call_centre.views import DBExportView
-
-
-class MockDBExportView(DBExportView):
-    authentication_classes = ()
-    permission_classes = (permissions.AllowAny,)
-
-
-urlpatterns = patterns('',
-    url(r'^mock_dbexport/$',
-        MockDBExportView.as_view(),
-        name='mock_db_export')
-)
+from mohawk import Sender
 
 
 class DBExportTestCase(TestCase):
-    urls = 'call_centre.tests.test_views'
+    fixtures = ['test_auth_clients']
 
     def test_export_sql_columns_exist(self):
         """
         Basic sanity check to ensure DB export SQL isn't trying to query
         non-existent tables/columns.
         """
-        response = self.client.get('/mock_dbexport/?passphrase=cla&from=2014-01-01T00:00:00&to=2014-12-31T23:59:59')
+
+        # sign request with Mohawk
+        creds = {
+            'id': '39d31fdffd8c89ebb6d5',
+            'key': '7ad5e3cbcdf656ececbd235557f99ec049bee604',
+            'algorithm': 'sha256'
+        }
+
+        host = 'http://testserver'
+        path = '/obiee/dbexport/'
+        params = ('?from=2014-08-28T00:00:00&to=2014-08-28T00:00:01'
+                  '&passphrase=cla')
+        content = ''
+        content_type = ''
+        method = 'GET'
+
+        sender = Sender(creds, (host + path + params), method, content=content,
+                        content_type=content_type)
+
+        response = self.client.get((path + params),
+                                   HTTP_AUTHORIZATION=sender.request_header,
+                                   HTTP_CONTENT_TYPE=content_type)
         self.assertEqual(response.status_code, status.HTTP_200_OK)

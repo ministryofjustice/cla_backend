ec7791663ed866d240edbaf5e0dd766e9418e1ff
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-04-28 14:50:33 +0100

    Add docstrings so that hubot can say what went wrong

--- cla_backend/apps/status/tests/smoketests.py
+++ cla_backend/apps/status/tests/smoketests.py
@@ -11,12 +11,14 @@ class SmokeTests(unittest.TestCase):
         pass
 
     def test_can_access_db(self):
+        "access the database"
         cursor = connection.cursor()
         cursor.execute('SELECT 1')
         row = cursor.fetchone()
         self.assertEqual(1, row[0])
 
     def test_can_access_celery(self):
+        "connect to SQS"
         if not getattr(settings, 'CELERY_ALWAYS_EAGER', False):
             conn = Celery('cla_backend').connection()
             conn.connect()

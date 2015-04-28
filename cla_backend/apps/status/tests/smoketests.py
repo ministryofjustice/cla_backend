611c6b88718aff28c56cdd0fa17045f59f105bf9
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-04-28 13:08:55 +0100

    Add smoketest for celery

--- cla_backend/apps/status/tests/smoketests.py
+++ cla_backend/apps/status/tests/smoketests.py
@@ -1,5 +1,7 @@
 import unittest
 
+from celery import Celery
+from django.conf import settings
 from django.db import connection
 
 
@@ -13,3 +15,9 @@ class SmokeTests(unittest.TestCase):
         cursor.execute('SELECT 1')
         row = cursor.fetchone()
         self.assertEqual(1, row[0])
+
+    def test_can_access_celery(self):
+        if not getattr(settings, 'CELERY_ALWAYS_EAGER', False):
+            conn = Celery('cla_backend').connection()
+            conn.connect()
+            conn.release()

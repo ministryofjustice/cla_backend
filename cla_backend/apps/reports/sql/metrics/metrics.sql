a4aeeee2402faf440d0d94a04cb50496e951c372
Josh Rowe <josh@s-block.com>
2015-05-13 10:08:18 +0100

    add diagnosis counts to export

--- cla_backend/apps/reports/sql/metrics/metrics.sql
+++ cla_backend/apps/reports/sql/metrics/metrics.sql
@@ -10,11 +10,25 @@ WITH
     FROM legalaid_case 
     WHERE created >= '2015-04-01 00:00'::timestamp AND created <= '2015-04-30 00:00'::timestamp
     GROUP BY date_trunc('day', created), legalaid_case.id
+  ), diagnosis AS (
+    SELECT
+      id,
+      state,
+      date_trunc('day', created) AS day
+    FROM diagnosis_diagnosistraversal
+    WHERE created >= '2015-04-01 00:00'::timestamp AND created <= '2015-04-30 00:00'::timestamp
+    GROUP BY date_trunc('day', created), diagnosis_diagnosistraversal.id
   )
 SELECT
-  report_dates.day,
-  COUNT(report_cases.id)
+  report_dates.day as "Date",
+  COUNT(diagnosis.id) as "Diagnosis",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'UNKNOWN' AND diagnosis.day = report_dates.day) as "Scope_unknown",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'OUTOFSCOPE' AND diagnosis.day = report_dates.day) as "Outofscope",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'CONTACT' AND diagnosis.day = report_dates.day) as "Scope_contact",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'INSCOPE' AND diagnosis.day = report_dates.day) as "Inscope",
+  COUNT(report_cases.id) as "Cases"
 FROM report_dates
 LEFT OUTER JOIN report_cases ON report_cases.day = report_dates.day
+LEFT OUTER JOIN diagnosis ON diagnosis.day = report_dates.day
 GROUP BY report_dates.day
 ORDER BY report_dates.day;

269b9b85978ea7486f692a44e55168a696b1e898
Josh Rowe <josh@s-block.com>
2015-05-15 13:30:47 +0100

    alias report_dates

--- cla_backend/apps/reports/sql/metrics.sql
+++ cla_backend/apps/reports/sql/metrics.sql
@@ -32,37 +32,37 @@ WITH
     GROUP BY date_trunc('day', created), legalaid_eligibilitycheck.id
   )
 SELECT
-  report_dates.day as "Date",
-  (SELECT COUNT(*) FROM diagnosis WHERE diagnosis.day = report_dates.day) as "Diagnosis_total",
-  (SELECT COUNT(*) FROM diagnosis WHERE state = 'UNKNOWN' AND diagnosis.day = report_dates.day) as "Scope_unknown",
-  (SELECT COUNT(*) FROM diagnosis WHERE state = 'OUTOFSCOPE' AND diagnosis.day = report_dates.day) as "Outofscope",
-  (SELECT COUNT(*) FROM diagnosis WHERE state = 'CONTACT' AND diagnosis.day = report_dates.day) as "Scope_contact",
-  (SELECT COUNT(*) FROM diagnosis WHERE state = 'INSCOPE' AND diagnosis.day = report_dates.day) as "Inscope",
+  rd.day as "Date",
+  (SELECT COUNT(*) FROM diagnosis WHERE diagnosis.day = rd.day) as "Diagnosis_total",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'UNKNOWN' AND diagnosis.day = rd.day) as "Scope_unknown",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'OUTOFSCOPE' AND diagnosis.day = rd.day) as "Outofscope",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'CONTACT' AND diagnosis.day = rd.day) as "Scope_contact",
+  (SELECT COUNT(*) FROM diagnosis WHERE state = 'INSCOPE' AND diagnosis.day = rd.day) as "Inscope",
   
-  (SELECT COUNT(*) FROM eligibility_check WHERE eligibility_check.day = report_dates.day) as "Eligibility_check_total",
-  (SELECT COUNT(*) FROM eligibility_check WHERE state = 'unknown' AND eligibility_check.day = report_dates.day) as "Eligibility_check_unknown",
-  (SELECT COUNT(*) FROM eligibility_check WHERE state = 'no' AND eligibility_check.day = report_dates.day) as "Eligibility_check_ineligible",
-  (SELECT COUNT(*) FROM eligibility_check WHERE state = 'yes' AND eligibility_check.day = report_dates.day) as "Eligibility_check_eligible",
+  (SELECT COUNT(*) FROM eligibility_check WHERE eligibility_check.day = rd.day) as "Eligibility_check_total",
+  (SELECT COUNT(*) FROM eligibility_check WHERE state = 'unknown' AND eligibility_check.day = rd.day) as "Eligibility_check_unknown",
+  (SELECT COUNT(*) FROM eligibility_check WHERE state = 'no' AND eligibility_check.day = rd.day) as "Eligibility_check_ineligible",
+  (SELECT COUNT(*) FROM eligibility_check WHERE state = 'yes' AND eligibility_check.day = rd.day) as "Eligibility_check_eligible",
   
-  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = report_dates.day) as "Cases_total",
-  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'unknown') as "Cases_unknown",
-  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'no') as "Cases_ineligible",
-  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'yes') as "Cases_eligible",
+  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = rd.day) as "Cases_total",
+  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'unknown') as "Cases_unknown",
+  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'no') as "Cases_ineligible",
+  (SELECT COUNT(*) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'yes') as "Cases_eligible",
 
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day) as "Time_total",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'unknown') as "Time_unknown",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'no') as "Time_ineligible",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'yes') as "Time_eligible",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day) as "Time_total",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'unknown') as "Time_unknown",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'no') as "Time_ineligible",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'yes') as "Time_eligible",
 
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.source = 'WEB') as "Time_web_total",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'unknown'AND report_cases.source = 'WEB') as "Time_web_unknown",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'no'AND report_cases.source = 'WEB') as "Time_web_ineligible",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'yes'AND report_cases.source = 'WEB') as "Time_web_eligible",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.source = 'WEB') as "Time_web_total",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'unknown'AND report_cases.source = 'WEB') as "Time_web_unknown",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'no'AND report_cases.source = 'WEB') as "Time_web_ineligible",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'yes'AND report_cases.source = 'WEB') as "Time_web_eligible",
 
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.source = 'PHONE') as "Time_phone_total",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'unknown' AND report_cases.source = 'PHONE') as "Time_phone_unknown",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'no' AND report_cases.source = 'PHONE') as "Time_phone_ineligible",
-  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = report_dates.day AND report_cases.state = 'yes' AND report_cases.source = 'PHONE') as "Time_phone_eligible"
-FROM report_dates
-GROUP BY report_dates.day
-ORDER BY report_dates.day;
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.source = 'PHONE') as "Time_phone_total",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'unknown' AND report_cases.source = 'PHONE') as "Time_phone_unknown",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'no' AND report_cases.source = 'PHONE') as "Time_phone_ineligible",
+  (SELECT COALESCE(SUM(report_cases.billable_time), 0) FROM report_cases WHERE report_cases.day = rd.day AND report_cases.state = 'yes' AND report_cases.source = 'PHONE') as "Time_phone_eligible"
+FROM report_dates rd
+GROUP BY rd.day
+ORDER BY rd.day;

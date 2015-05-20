a48131e52e430acb4f718fcc9663c473f6af8ba0
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-05-20 15:46:16 +0100

    fixed round-trip inconsistencies between Django and Transifex .po files; Welsh plural forms were wrong

--- cla_backend/apps/core/management/commands/translations.py
+++ cla_backend/apps/core/management/commands/translations.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import LabelCommand, CommandError
 from django.core.management.utils import popen_wrapper
+from django.utils.translation import to_locale
+import polib
 
 
 class Command(LabelCommand):
@@ -16,52 +18,83 @@ class Command(LabelCommand):
     args = 'update|push|pull'
     label = 'action'
 
+    standard_options = {}
     verbosity = 1
 
     def handle_label(self, label, **options):
+        self.standard_options.update(options)  # NB: exclude any custom options specific to this command
         self.verbosity = int(options['verbosity'])
+
         try:
-            getattr(self, 'action_%s' % label)(**options)
+            getattr(self, 'action_%s' % label)()
         except AttributeError:
             raise CommandError('Unknown action')
 
-    def action_update(self, **options):
+    def _run_cmd(self, cmd):
+        msgs, errors, status = popen_wrapper(shlex.split(cmd))
+        if errors:
+            if status != 0:
+                raise CommandError('Command `%s` error:\n%s' % (cmd, errors))
+            self.stderr.write(errors)
+        if msgs:
+            self.stdout.write(msgs)
+        if status != 0:
+            raise CommandError('Command `%s` exited with status %s' % (cmd, status))
+
+    def _messages(self, do_make=True, do_compile=True):
+        cwd = os.getcwd()
+        os.chdir(settings.PROJECT_ROOT)
+        if do_make:
+            call_command('makemessages', all=True, keep_pot=True, no_wrap=True, extensions=["html", "txt", "tpl"],
+                         **self.standard_options)
+        if do_compile:
+            call_command('compilemessages', **self.standard_options)
+        os.chdir(cwd)
+
+    def action_update(self):
         if self.verbosity:
             self.stdout.write(self.style.MIGRATE_HEADING('Updating Django translation .pot and.po files'))
 
         def graphml_file_map(graphml_path):
             return os.path.abspath(os.path.join(settings.PROJECT_ROOT, 'apps', 'diagnosis', *graphml_path))
 
+        # internationalise all graphml files
         graphml_paths = [
             ('data', settings.DIAGNOSIS_FILE_NAME),
             ('data', settings.CHECKER_DIAGNOSIS_FILE_NAME),
         ]
         graphml_files = map(graphml_file_map, graphml_paths)
-        call_command('internationalise_graphs', *graphml_files, **options)
+        call_command('internationalise_graphs', *graphml_files, **self.standard_options)
 
-        cwd = os.getcwd()
-        os.chdir(settings.PROJECT_ROOT)
-        call_command('makemessages', all=True, keep_pot=True, no_wrap=True, extensions=["html", "txt", "tpl"], **options)
-        call_command('compilemessages', **options)
-        os.chdir(cwd)
+        # make and compile messages
+        self._messages()
 
-    def _run(self, cmd):
-        msgs, errors, status = popen_wrapper(shlex.split(cmd))
-        if errors:
-            if status != 0:
-                raise CommandError('Command error:\n%s' % errors)
-            self.stderr.write(errors)
-        if msgs:
-            self.stdout.write(msgs)
-        if status != 0:
-            raise CommandError('Command exited with status %s' % status)
-
-    def action_push(self, **options):
+    def action_push(self):
         if self.verbosity:
             self.stdout.write(self.style.MIGRATE_HEADING('Pushing all translations to Transifex'))
-        self._run('tx push --source --translations')
 
-    def action_pull(self, **options):
+        # push source .pot and all translated .po files to Transifex
+        self._run_cmd('tx push --source --translations')
+
+    def action_pull(self):
         if self.verbosity:
             self.stdout.write(self.style.MIGRATE_HEADING('Pulling all translations from Transifex'))
-        self._run('tx pull --all')
+        self.stdout.write(self.style.WARNING('This command will also merge in the latest local strings!'))
+
+        # pull all known translated .po files except the source language from Transifex
+        trans_langs = [lang_code for lang_code, lang_name in settings.LANGUAGES if lang_code != settings.LANGUAGE_CODE]
+        self._run_cmd('tx pull --force --language=%s' % ','.join(trans_langs))
+
+        # re-normalise/fix Transifex output to match Django's
+        source_locale = to_locale(settings.LANGUAGE_CODE)
+        translations_path = os.path.join(settings.PROJECT_ROOT, 'translations')
+        for lang_code, lang_name in settings.LANGUAGES:
+            locale_code = to_locale(lang_code)
+            messages_path = os.path.join(translations_path, locale_code, 'LC_MESSAGES', 'django.po')
+            messages_path = os.path.abspath(messages_path)
+            messages = polib.pofile(messages_path, wrapwidth=0)
+            messages.header = messages.metadata['Project-Id-Version']
+            messages.metadata['Language'] = messages.metadata['X-Language'] = locale_code
+            messages.metadata['X-Source-Language'] = source_locale
+            messages.save(messages_path)
+        self._messages()

2fac8edd7d7114dde220e0d45a6daeb2aedba4ac
Josh Rowe <josh@s-block.com>
2015-06-17 15:34:06 +0100

    add notification api

--- cla_backend/apps/notifications/serializers.py
+++ cla_backend/apps/notifications/serializers.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+from rest_framework import serializers
+
+from .models import Notification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = (
+            'type',
+            'notification',
+            'description',
+            'start_time',
+            'end_time'
+        )

462237cebff2fd3221b7376c2bfe0f872021e62f
Josh Rowe <josh@s-block.com>
2015-08-03 15:44:25 +0100

    migrate legalaid for related query

---
+++ cla_backend/apps/legalaid/migrations/0004_auto_20150803_1443.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('legalaid', '0003_eod_details'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='property',
+            name='eligibility_check',
+            field=models.ForeignKey(related_query_name=b'property_set', to='legalaid.EligibilityCheck'),
+            preserve_default=True,
+        ),
+    ]

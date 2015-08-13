11d6706437bfc766224e4398de1ce408bd37f89d
Josh Rowe <josh@s-block.com>
2015-08-13 17:23:21 +0100

    add form action mixin to complaints

--- cla_backend/apps/complaints/views.py
+++ cla_backend/apps/complaints/views.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from rest_framework import mixins, viewsets
+from core.drf.mixins import FormActionMixin
 
 from .models import Complaint, Category
 from .serializers import ComplaintSerializerBase, CategorySerializerBase
 
 
+class ComplaintFormActionMixin(FormActionMixin):
+    """
+    This is for backward compatibility
+    """
+    FORM_ACTION_OBJ_PARAM = 'complaint'
+
+
 class BaseComplaintViewSet(
+    ComplaintFormActionMixin,
     mixins.CreateModelMixin,
     mixins.UpdateModelMixin,
     mixins.RetrieveModelMixin,

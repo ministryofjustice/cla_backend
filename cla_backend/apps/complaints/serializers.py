78f45f8ce94f763bc3071c2b7b9785e9991888cc
Josh Rowe <josh@s-block.com>
2015-08-17 16:23:12 +0100

    simplify complaints serializer and add read_only_fields

--- cla_backend/apps/complaints/serializers.py
+++ cla_backend/apps/complaints/serializers.py
@@ -10,29 +10,20 @@ class CategorySerializerBase(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
-class ClaSerializerMethodField(serializers.SerializerMethodField):
-    def field_to_native(self, obj, field_name):
-        meth = getattr(self.parent, self.method_name)
-        try:
-            return self.to_native(meth(obj))
-        except AttributeError:
-            pass
-
-
 class ComplaintSerializerBase(serializers.ModelSerializer):
     category = CategorySerializerBase()
-    full_name = ClaSerializerMethodField('_get_full_name')
-    category_of_law = ClaSerializerMethodField('_get_category_of_law')
-    case_reference = ClaSerializerMethodField('_get_case_reference')
+    full_name = serializers.CharField(
+        source='eod.case.personal_details.full_name',
+        read_only=True)
+    category_of_law = serializers.CharField(
+        source='eod.case.eligibility_check.category',
+        read_only=True)
+    case_reference = serializers.CharField(
+        source='eod.case.reference',
+        read_only=True)
 
     class Meta:
         model = Complaint
-
-    def _get_full_name(self, instance):
-        return instance.eod.case.personal_details.full_name
-
-    def _get_category_of_law(self, instance):
-        return instance.eod.case.eligibility_check.category
-
-    def _get_case_reference(self, instance):
-        return instance.eod.case.reference
+        read_only_fields = (
+            'created', 'modified', 'created_by',
+        )

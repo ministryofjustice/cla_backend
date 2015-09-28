f5b7c713c27e138d90481fe5c90548fd87a26b0c
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-09-28 17:36:23 +0100

    complaints updates • a single case (i.e. its one-to-one EOD) can have multiple open complaints • cases API reports the open complaint count

--- cla_backend/apps/call_centre/tests/api/test_case_api.py
+++ cla_backend/apps/call_centre/tests/api/test_case_api.py
@@ -219,6 +219,11 @@ class CreateCaseTestCase(BaseCaseTestCase):
 
 
 class UpdateCaseTestCase(BaseUpdateCaseTestCase, BaseCaseTestCase):
+    @property
+    def response_keys(self):
+        return super(UpdateCaseTestCase, self).response_keys + \
+               ['complaint_count']
+
     def test_patch_operator_notes_allowed(self):
         """
         Test that provider cannot post provider notes

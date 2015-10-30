036bbbf3cb3e110134022880a0268e3ca3c8f2e3
Josh Rowe <josh@s-block.com>
2015-10-30 11:24:42 +0000

    reset ProviderAllocation.modified for all so weightings will start from now

--- cla_backend/apps/legalaid/migrations/0011_case_assigned_out_of_hours.py
+++ cla_backend/apps/legalaid/migrations/0011_case_assigned_out_of_hours.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models, migrations
 from django.conf import settings
 
@@ -8,12 +10,24 @@ from django.conf import settings
 def add_assigned_at(apps, schema_editor):
     Case = apps.get_model("legalaid", "Case")
     for case in Case.objects.all():
-        case.assigned_out_of_hours = case.provider_assigned_at not in \
-            settings.PROVIDER_OPENING_HOURS
-        case.save()
+        if case.provider_assigned_at:
+            case.assigned_out_of_hours = case.provider_assigned_at not in \
+                settings.NON_ROTA_OPENING_HOURS
+            case.save()
 
 
 def reverse_assigned(*args, **kwargs):
+    pass
+
+
+def reset_provider_allocation_modified(apps, schema_editor):
+    ProviderAllocation = apps.get_model("cla_provider", "ProviderAllocation")
+    for pa in ProviderAllocation.objects.all():
+        pa.modified = datetime.datetime.now()
+        pa.save()
+
+
+def reverse_reset(*args, **kwargs):
     pass
 
 
@@ -30,5 +44,7 @@ class Migration(migrations.Migration):
             field=models.NullBooleanField(),
             preserve_default=True,
         ),
-        migrations.RunPython(add_assigned_at, reverse_assigned)
+        migrations.RunPython(add_assigned_at, reverse_assigned),
+        migrations.RunPython(reset_provider_allocation_modified,
+                             reverse_reset),
     ]

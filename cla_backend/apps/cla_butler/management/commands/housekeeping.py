6a633cbeb9415d07c75d074962814cbb0cb09350
Josh Rowe <josh@s-block.com>
2015-11-30 15:44:33 +0000

    4% - remove eligibility checks and diagnosis objeces with no case and case objects older thatn 2 years

--- cla_backend/apps/cla_butler/management/commands/housekeeping.py
+++ cla_backend/apps/cla_butler/management/commands/housekeeping.py
@@ -1,41 +1,81 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+from dateutil.relativedelta import relativedelta
+from django.contrib.admin.models import LogEntry
+from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
 from django.db.models import Q
-from django.core.management.base import NoArgsCommand
 from django.utils import timezone
 
+from cla_eventlog.models import Log
 from diagnosis.models import DiagnosisTraversal
 from legalaid.models import Case, EligibilityCheck
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
     help = 'Deletes public diagnosis that are more than a day old'
 
-    def handle_noargs(self, *args, **options):
+    def handle(self, *args, **options):
+        self._setup()
         self.cleanup_diagnosis()
+        self.cleanup_eligibility_check()
+        self.cleanup_cases()
+
+    def _setup(self):
+        self.now = timezone.now()
+
+    def _delete_logs(self, qs):
+        ct = ContentType.objects.get_for_model(qs.model)
+        logs = Log.objects.filter(
+            content_type=ct,
+            object_id__in=[i.pk for i in qs]
+        )
+        logs.delete()
+        log_entries = LogEntry.objects.filter(
+            content_type=ct,
+            object_id__in=[i.pk for i in qs]
+        )
+        log_entries.delete()
+
+    def _delete_objects(self, qs):
+        self._delete_logs(qs)
+
+        name = qs.model.__name__
+        self.stdout.write('Total {name} objects: {count}'.format(
+            count=qs.model.objects.all().count(),
+            name=name))
+
+        self.stdout.write('Deleting {count} {name} objects'.format(
+            count=qs.count(),
+            name=name))
+
+        qs.delete()
+
+        self.stdout.write('Total {name} objects: {count}'.format(
+            count=qs.model.objects.all().count(),
+            name=name))
 
     def cleanup_diagnosis(self, *args, **options):
-        self.stdout.write('Total DiagnosisTraversal objects: {count}'.format(
-            count=DiagnosisTraversal.objects.all().count()))
-        yesterday = timezone.now() - datetime.timedelta(days=1)
+        yesterday = self.now - datetime.timedelta(days=1)
         diags = DiagnosisTraversal.objects.filter(
             case__isnull=True,
             modified__lte=yesterday,
         )
-        self.stdout.write('Deleting %s DiagnosisTraversal objects'.format(
-            count=diags.count()))
-
-        diags.delete()
-
-        self.stdout.write('Total DiagnosisTraversal objects: {count}'.format(
-            count=DiagnosisTraversal.objects.all().count()))
-
-    def cleanup_cases(self):
-        for case in Case.objects.all():
-            pass
+        self._delete_objects(diags)
 
     def cleanup_eligibility_check(self):
-        for ec in EligibilityCheck.objects.all():
-            pass
+        yesterday = self.now - datetime.timedelta(days=1)
+        ecs = EligibilityCheck.objects.filter(
+            case__isnull=True,
+            modified__lte=yesterday,
+        )
+        self._delete_objects(ecs)
+
+    def cleanup_cases(self):
+        two_years = self.now - relativedelta(years=2)
+        cases = Case.objects.filter(
+            modified__lte=two_years,
+        )
+        self._delete_objects(cases)

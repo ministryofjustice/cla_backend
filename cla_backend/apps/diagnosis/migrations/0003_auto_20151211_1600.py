f4247ef6134d395f4b6cd3de376c48180331d7d6
Josh Rowe <josh@s-block.com>
2015-12-11 16:01:11 +0000

    add db_index to diagnosis state

---
+++ cla_backend/apps/diagnosis/migrations/0003_auto_20151211_1600.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('diagnosis', '0002_auto_20150127_1536'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='diagnosistraversal',
+            name='state',
+            field=models.CharField(default=b'UNKNOWN', max_length=50, null=True, db_index=True, blank=True),
+            preserve_default=True,
+        ),
+    ]

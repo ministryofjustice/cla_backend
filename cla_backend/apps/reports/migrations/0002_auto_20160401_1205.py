6ae157620061579d8eeca5fe0fdd7b50bc0abab3
Josh Rowe <josh@s-block.com>
2016-04-01 16:12:27 +0100

    add migration and operation so I can use crosstab psql function

---
+++ cla_backend/apps/reports/migrations/0002_auto_20160401_1205.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from core.operations import LoadExtension
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reports', '0001_initial'),
+    ]
+
+    operations = [
+        LoadExtension(name='tablefunc'),
+    ]

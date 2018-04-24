937764c83dade0f191b9595d89f119fc26618cb5
Ian Brechin <ian.brechin@gmail.com>
2018-04-24 14:17:46 +0100

    Add trigram indexes to case search fields and restructure query to use them
    Trigram indexes can be helpful when making database queries of the form
    `LIKE '%term%'` which do not use normal indexes due to the leading
    wildcard.
    
    However, doing an `OR` between columns on two different tables in an
    `OUTER JOIN` does not hit these indexes anyway, so it was necessary to
    change the query to using `UNION` and `INTERSECT` to combine the
    search terms instead. This part of the query is done in a `TEMPORARY VIEW`
    which is then `INNER JOIN`ed to the rest of the query to filter the cases.

---
+++ cla_backend/apps/legalaid/migrations/0018_auto_20180425_1558.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import connection, migrations
+
+
+def add_trigram_indexes(apps, schema_editor):
+    with connection.cursor() as cursor:
+        cursor.execute('CREATE EXTENSION pg_trgm;')
+        cursor.execute(
+            'CREATE INDEX case_reference_trgm_idx ON '
+            'legalaid_case USING gist (UPPER(reference) gist_trgm_ops);'
+        )
+        cursor.execute(
+            'CREATE INDEX case_search_field_trgm_idx ON '
+            'legalaid_case USING gist (UPPER(search_field) gist_trgm_ops);'
+        )
+        cursor.execute(
+            'CREATE INDEX case_laa_reference_trgm_idx ON '
+            'legalaid_case USING gist (UPPER(text(laa_reference)) gist_trgm_ops);'
+        )
+        cursor.execute(
+            'CREATE INDEX personaldetails_full_name_trgm_idx ON '
+            'legalaid_personaldetails USING gist (UPPER(full_name) gist_trgm_ops);'
+        )
+        cursor.execute(
+            'CREATE INDEX personaldetails_street_trgm_idx ON '
+            'legalaid_personaldetails USING gist (UPPER(street) gist_trgm_ops);'
+        )
+        cursor.execute(
+            'CREATE INDEX personaldetails_postcode_trgm_idx ON '
+            'legalaid_personaldetails USING gist (UPPER(postcode) gist_trgm_ops);'
+        )
+        cursor.execute(
+            'CREATE INDEX personaldetails_search_field_trgm_idx ON '
+            'legalaid_personaldetails USING gist (UPPER(search_field) gist_trgm_ops);'
+        )
+
+
+def remove_trigram_indexes(apps, schema_editor):
+    with connection.cursor() as cursor:
+        cursor.execute('DROP INDEX case_reference_trgm_idx;')
+        cursor.execute('DROP INDEX case_search_field_trgm_idx;')
+        cursor.execute('DROP INDEX case_laa_reference_trgm_idx;')
+        cursor.execute('DROP INDEX personaldetails_full_name_trgm_idx;')
+        cursor.execute('DROP INDEX personaldetails_street_trgm_idx;')
+        cursor.execute('DROP INDEX personaldetails_postcode_trgm_idx;')
+        cursor.execute('DROP INDEX personaldetails_search_field_trgm_idx;')
+        cursor.execute('DROP EXTENSION pg_trgm;')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('legalaid', '0017_case_is_urgent'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_trigram_indexes, remove_trigram_indexes)
+    ]

f8010022cb3a4cd9a9bc1767e4aecfdaabd5094d
Nick Rowlands <nick@rowlando.com>
2018-07-11 10:53:24 +0100

    LGA-83 Move unit tests and Docker build to Circle CI (#470)
    * Add CircleCI configuration based on cla_public's config
    * Configure Circle CI to set up Postgres and run tests
    * Upgrade Psychopg2 and Pyminizip so project builds in Python2.7
    * Fixed OBIEEExportOutputsZipTestCase that broke because of Pyminizip upgrade
    * Skip flaky Python test
    * Add Circle CI status badge for develop branch

---
+++ cla_backend/settings/circle.py
@@ -0,0 +1,26 @@
+import os
+from .testing import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('CLA', 'cla-alerts@digital.justice.gov.uk'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'cla_backend.apps.reports.db.backend',
+        'NAME': os.environ.get('DB_NAME', 'circle_test'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', ''),
+    }
+}
+
+ALLOWED_HOSTS = [
+    '*'
+]

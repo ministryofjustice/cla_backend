32564bec9abba8553f760e24492f8ebd5721cee6
Patrick Farrell <patrick.farrell@digital.justice.gov.uk>
2018-11-20 16:41:30 +0000

    Simplify data migration
    Update migration dependencies
    Use historical version of model

--- cla_backend/apps/legalaid/migrations/0020_fill_missing_outcome_codes.py
+++ cla_backend/apps/legalaid/migrations/0020_fill_missing_outcome_codes.py
@@ -8,20 +8,19 @@ logger = logging.getLogger(__name__)
 
 
 def re_denormalize_outcome_codes_to_cases(apps, schema_editor):
-    try:
-        from cla_eventlog.constants import LOG_LEVELS, LOG_TYPES
-        from cla_eventlog.models import Log
-    except ImportError:
-        pass
-    else:
-        outcomes_that_should_be_denormed = Log.objects.filter(level=LOG_LEVELS.HIGH, type=LOG_TYPES.OUTCOME)
-        outcomes_missing_denormed_code = outcomes_that_should_be_denormed.filter(case__outcome_code='')
+    from cla_eventlog.constants import LOG_LEVELS, LOG_TYPES
+    Log = apps.get_model('cla_eventlog', 'Log')
 
-        logger.info('\nLGA-275 data migration: {} outcomes_missing_denormed_code'.format(outcomes_missing_denormed_code.count()))
-        for outcome in outcomes_missing_denormed_code:
-            outcome.case.outcome_code = outcome.code
-            outcome.case.save()
-            logger.info('LGA-275 data migration: Filled missing outcome code for case {}'.format(outcome.case.reference))
+    outcome_kwargs = {'level': LOG_LEVELS.HIGH, 'type': LOG_TYPES.OUTCOME}
+    outcomes_that_should_be_denormed = Log.objects.filter(**outcome_kwargs).order_by('created')  # Oldest to newest
+    outcomes_missing_denormed_code = outcomes_that_should_be_denormed.filter(case__outcome_code='')
+
+    logger.info('\nLGA-275 data migration: {} outcomes_missing_denormed_code'.format(outcomes_missing_denormed_code.count()))
+
+    for outcome in outcomes_missing_denormed_code:
+        outcome.case.outcome_code = outcome.code
+        outcome.case.save()
+        logger.info('LGA-275 data migration: Filled missing outcome code for case {}'.format(outcome.case.reference))
 
 
 def noop(apps, schema_editor):
@@ -31,6 +30,7 @@ def noop(apps, schema_editor):
 class Migration(migrations.Migration):
     dependencies = [
         ('legalaid', '0019_null_to_empty_string'),
+        ('cla_eventlog', '0004_auto_20151210_1231'),
     ]
 
     operations = [

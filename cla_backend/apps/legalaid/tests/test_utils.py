c6174a0c85fddb08a3db5cbbe7636a504838bfde
Patrick Farrell <patrick.farrell@digital.justice.gov.uk>
2019-01-03 15:31:45 +0000

    Set custom hours to next year, always in the future
    Refactor circle to testing settings

--- cla_backend/apps/legalaid/tests/test_utils.py
+++ cla_backend/apps/legalaid/tests/test_utils.py
@@ -1,22 +1,21 @@
 from datetime import date, datetime, time
 
 from django.test import TestCase
-from legalaid.utils.sla import is_in_business_hours
+from legalaid.utils.sla import is_in_business_hours, operator_hours
 
-from cla_common.call_centre_availability import OpeningHours
-from django.conf import settings
+# from cla_common.call_centre_availability import OpeningHours
 
 
 class SlaCustomBusinessHoursTestCase(TestCase):
-    operator_hours = OpeningHours(**settings.OPERATOR_HOURS)
+    next_year = date.today().year + 1
 
-    xmas_eve = date(year=2018, month=12, day=24)
+    xmas_eve = date(year=next_year, month=12, day=24)
     xmas_eve_before_hours = datetime.combine(xmas_eve, time(hour=8, minute=59))
     xmas_eve_noon = datetime.combine(xmas_eve, time(hour=12))
     xmas_eve_last_minute = datetime.combine(xmas_eve, time(hour=17, minute=29))
     xmas_eve_after_hours = datetime.combine(xmas_eve, time(hour=18))
 
-    new_years_eve = date(year=2018, month=12, day=31)
+    new_years_eve = date(year=next_year, month=12, day=31)
     new_years_eve_before_hours = datetime.combine(new_years_eve, time(hour=8, minute=59))
     new_years_eve_noon = datetime.combine(new_years_eve, time(hour=12))
     new_years_eve_last_minute = datetime.combine(new_years_eve, time(hour=17, minute=29))
@@ -34,10 +33,10 @@ class SlaCustomBusinessHoursTestCase(TestCase):
         self.assertFalse(is_in_business_hours(self.new_years_eve_after_hours))
 
     def test_custom_day_timeslots(self):
-        slots = self.operator_hours.time_slots(self.xmas_eve)
-        self.assertEquals(min(slots), datetime(2018, 12, 24, 9, 0))
-        self.assertEquals(max(slots), datetime(2018, 12, 24, 17, 0))
+        slots = operator_hours.time_slots(self.xmas_eve)
+        self.assertEquals(min(slots), datetime(self.next_year, 12, 24, 9, 0))
+        self.assertEquals(max(slots), datetime(self.next_year, 12, 24, 17, 0))
 
-        slots = self.operator_hours.time_slots(self.new_years_eve)
-        self.assertEquals(min(slots), datetime(2018, 12, 31, 9, 0))
-        self.assertEquals(max(slots), datetime(2018, 12, 31, 17, 0))
+        slots = operator_hours.time_slots(self.new_years_eve)
+        self.assertEquals(min(slots), datetime(self.next_year, 12, 31, 9, 0))
+        self.assertEquals(max(slots), datetime(self.next_year, 12, 31, 17, 0))
--- cla_backend/settings/circle.py
+++ cla_backend/settings/circle.py
@@ -1,26 +1,14 @@
 import os
 from .testing import *
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     ('CLA', 'cla-alerts@digital.justice.gov.uk'),
 )
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'cla_backend.apps.reports.db.backend',
-        'NAME': os.environ.get('DB_NAME', 'circle_test'),
-        'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', ''),
-    }
-}
-
-ALLOWED_HOSTS = [
-    '*'
-]
+DATABASES['default']['NAME'] = os.environ.get('DB_NAME', 'circle_test')
+DATABASES['default']['USER'] = os.environ.get('DB_USER', 'root')
+DATABASES['default']['PASSWORD'] = os.environ.get('DB_PASSWORD', '')
+DATABASES['default']['HOST'] = os.environ.get('DB_HOST', 'localhost')
+DATABASES['default']['PORT'] = os.environ.get('DB_PORT', '')

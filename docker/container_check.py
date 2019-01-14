58827f29c2eb3edf65ce102ae08acb75248b826c
Patrick Farrell <patrick.farrell@digital.justice.gov.uk>
2019-01-14 14:44:58 +0000

    Black and flake8 fixes

--- docker/container_check.py
+++ docker/container_check.py
@@ -2,14 +2,16 @@
 
 import requests
 
+
 def get_website(url):
     return requests.get(url)
 
-if __name__ == '__main__':
-    response = get_website('http://localhost:8000/admin')
+
+if __name__ == "__main__":
+    response = get_website("http://localhost:8000/admin")
 
     if response.status_code == 200:
-        print 'The container is up and the app returned HTTP Status Code: OK'
+        print("The container is up and the app returned HTTP Status Code: OK")
     else:
-        print 'Error: The container returned an unexpected HTTP Status Code %d' % (response.status_code)
+        print("Error: The container returned an unexpected HTTP Status Code %d" % (response.status_code))
         exit(1)
--- scripts/jenkins/build.py
+++ scripts/jenkins/build.py
@@ -7,16 +7,16 @@ import sys
 from Queue import Queue
 
 # use python scripts/jenkins/build.py integration
-PROJECT_NAME = 'cla_backend'
+PROJECT_NAME = "cla_backend"
 
 background_processes = Queue()
 
 
 def run(command, background=False, **kwargs):
-    if 'shell' not in kwargs:
-        kwargs['shell'] = True
+    if "shell" not in kwargs:
+        kwargs["shell"] = True
 
-    print 'Running {command}'.format(command=command)
+    print("Running {command}".format(command=command))
 
     if background:
         process = subprocess.Popen(command, **kwargs)
@@ -29,13 +29,10 @@ def run(command, background=False, **kwargs):
 
 
 def kill_child_processes(pid, sig=signal.SIGTERM):
-    ps_cmd = subprocess.Popen(
-        'ps -o pid --ppid {0} --noheaders'.format(pid),
-        shell=True,
-        stdout=subprocess.PIPE)
+    ps_cmd = subprocess.Popen("ps -o pid --ppid {0} --noheaders".format(pid), shell=True, stdout=subprocess.PIPE)
     ps_out = ps_cmd.stdout.read()
     ps_cmd.wait()
-    for pid_str in ps_out.split('\n')[:-1]:
+    for pid_str in ps_out.split("\n")[:-1]:
         os.kill(int(pid_str), sig)
 
 
@@ -51,38 +48,37 @@ def kill_all_background_processes():
 
 def main():
     # args
-    parser = argparse.ArgumentParser(
-        description='Build project ready for testing by Jenkins.'
-    )
-    parser.add_argument('envname', help='e.g. integration, production, etc.')
-    parser.add_argument('--skip-tests', action='store_true',
-                        help='do not run Django unit tests')
+    parser = argparse.ArgumentParser(description="Build project ready for testing by Jenkins.")
+    parser.add_argument("envname", help="e.g. integration, production, etc.")
+    parser.add_argument("--skip-tests", action="store_true", help="do not run Django unit tests")
     args = parser.parse_args()
 
     env = args.envname
-    env_name = '%s-%s' % (PROJECT_NAME, env)
-    env_path = '/tmp/jenkins/envs/%s' % env_name
-    bin_path = '%s/bin' % env_path
+    env_name = "%s-%s" % (PROJECT_NAME, env)
+    env_path = "/tmp/jenkins/envs/%s" % env_name
+    bin_path = "%s/bin" % env_path
 
     # setting up virtualenv
     if not os.path.isdir(env_path):
-        run('virtualenv %s' % env_path)
+        run("virtualenv %s" % env_path)
 
-    run('%s/pip install -U setuptools pip wheel' % bin_path)
-    run('%s/pip install -r requirements/jenkins.txt' % bin_path)
+    run("%s/pip install -U setuptools pip wheel" % bin_path)
+    run("%s/pip install -r requirements/jenkins.txt" % bin_path)
 
     # Remove .pyc files from the project
     run("find . -name '*.pyc' -delete")
 
     if not args.skip_tests:
-        print 'Starting tests...'
+        print("Starting tests...")
 
-        run('%s/python manage.py jenkins '
-            '--coverage-rcfile=.coveragerc '
-            '--settings=cla_backend.settings.jenkins' % bin_path)
+        run(
+            "%s/python manage.py jenkins "
+            "--coverage-rcfile=.coveragerc "
+            "--settings=cla_backend.settings.jenkins" % bin_path
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     finally:

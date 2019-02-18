f56f36d5a0d6c67a98ae7ffecb8dc0b6fe7a032d
Patrick Farrell <p@farrell.io>
2019-02-18 14:45:27 +0800

    Add education outcome code EAA

--- cla_backend/apps/legalaid/utils/csvupload/contracts.py
+++ cla_backend/apps/legalaid/utils/csvupload/contracts.py
@@ -239,6 +239,7 @@ contract_2013_category_spec = {
 discrimination_category_spec["OUTCOME_CODES"].update({u"QAA"})
 family_category_spec["OUTCOME_CODES"].update({u"FAA", u"FAB", u"FAC", u"DAA"})
 housing_category_spec["OUTCOME_CODES"].update({u"HAA", u"HAC", u"HAB"})
+education_category_spec["OUTCOME_CODES"].update({u"EAA"})
 
 contract_2018_category_spec = {
     u"debt": deepcopy(debt_category_spec),

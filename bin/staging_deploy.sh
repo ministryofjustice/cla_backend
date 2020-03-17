d0dd5452e1baec219f19ea7650e11f6df44472dd
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-17 17:10:02 +0000

    Add migration job with pre-upgrade hook
    This job executes before new deployment pods are created
    Updates the deploy scripts to separate upgrading the chart from
    installing it. The install can be done automatically with the first
    upgrade, but doing that doesn't trigger the pre-upgrade hook

--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -4,8 +4,15 @@ set -e
 ROOT=$(dirname "$0")
 HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
+if ! helm ls | grep cla-backend
+then
+  helm upgrade cla-backend \
+    $HELM_DIR \
+    --values ${HELM_DIR}/values-staging.yaml \
+    --set image.tag=$IMAGE_TAG
+fi
+
 helm upgrade cla-backend \
-$HELM_DIR \
---values ${HELM_DIR}/values-staging.yaml \
---install \
---set image.tag=$IMAGE_TAG
+  $HELM_DIR \
+  --values ${HELM_DIR}/values-staging.yaml \
+  --set image.tag=$IMAGE_TAG

852e1cbbf646b46ea34521b8557a6cac156b888e
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-17 17:10:02 +0000

    Add migration job with pre-upgrade hook
    This job executes before new deployment pods are created
    Updates the deploy scripts to separate upgrading the chart from
    installing it. The install can be done automatically with the first
    upgrade, but doing that doesn't trigger the pre-upgrade hook

--- bin/local_deploy.sh
+++ bin/local_deploy.sh
@@ -6,7 +6,13 @@ HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
 kubectl config use-context docker-for-desktop
 
+if ! helm ls | grep cla-backend
+then
+  helm install cla-backend \
+    $HELM_DIR \
+    --values ${HELM_DIR}/values-dev.yaml
+fi
+
 helm upgrade cla-backend \
-$HELM_DIR \
---values ${HELM_DIR}/values-dev.yaml \
---install
+  $HELM_DIR \
+  --values ${HELM_DIR}/values-dev.yaml

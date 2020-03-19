ac5d13193d2fb317ce00e0f7ab29b76caa5c3f28
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-19 11:57:50 +0000

    Switch back to upgrade --install and add create_db script
    Instead of forcing it to install and then upgrade, to guarantee we
    always get an upgrade, we're going back to install-or-upgrade as we now
    have a hook for post-install that can do what we needed the upgrade hook
    for (migrating the db). The pre-upgrade hook is still used for upgrades,
    but is no longer needed on installs

---
+++ bin/create_db.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+migrations() {
+    python manage.py install_postgres_extensions
+    python manage.py migrate --noinput
+
+    python manage.py loaddata initial_groups.json
+    python manage.py loaddata initial_category.json
+    python manage.py loaddata initial_mattertype.json
+    python manage.py loaddata initial_media_codes.json
+    python manage.py loaddata initial_complaint_categories.json
+}
+
+load_test_data() {
+    if [ "$LOAD_TEST_DATA" == "True" ]; then
+
+        python manage.py loaddata test_provider.json
+        python manage.py loaddata test_provider_allocations.json
+        python manage.py loaddata test_auth_clients.json
+        python manage.py loaddata test_rotas.json
+
+    fi
+}
+
+admin_password() {
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('cla_admin', 'jags.parbha@digital.justice.gov.uk', '$ADMIN_PASSWORD')" | ./manage.py shell || echo "user already exists"
+}
+
+cd /home/app/
+
+migrations
+# admin_password
+load_test_data
+
+exec "$@"

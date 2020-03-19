64b8b23fb3eed0a4b5619112f12a1ca006de2f65
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-19 11:57:50 +0000

    Switch back to upgrade --install and add create_db script
    Instead of forcing it to install and then upgrade, to guarantee we
    always get an upgrade, we're going back to install-or-upgrade as we now
    have a hook for post-install that can do what we needed the upgrade hook
    for (migrating the db). The pre-upgrade hook is still used for upgrades,
    but is no longer needed on installs

--- bin/local_deploy.sh
+++ bin/local_deploy.sh
@@ -6,13 +6,7 @@ HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
 kubectl config use-context docker-for-desktop
 
-if ! helm ls | grep cla-backend
-then
-  helm install cla-backend \
-    $HELM_DIR \
-    --values ${HELM_DIR}/values-dev.yaml
-fi
-
 helm upgrade cla-backend \
   $HELM_DIR \
-  --values ${HELM_DIR}/values-dev.yaml
+  --values ${HELM_DIR}/values-dev.yaml \
+  --install
--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -4,15 +4,8 @@ set -e
 ROOT=$(dirname "$0")
 HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
-if ! helm ls | grep cla-backend
-then
-  helm upgrade cla-backend \
-    $HELM_DIR \
-    --values ${HELM_DIR}/values-staging.yaml \
-    --set image.tag=$IMAGE_TAG
-fi
-
 helm upgrade cla-backend \
   $HELM_DIR \
   --values ${HELM_DIR}/values-staging.yaml \
-  --set image.tag=$IMAGE_TAG
+  --set image.tag=$IMAGE_TAG \
+  --install

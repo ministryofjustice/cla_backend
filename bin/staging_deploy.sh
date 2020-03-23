0bcc27026c7ba19518a3682b54ea2606f88a54b0
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-23 12:08:28 +0000

    Specify namespace in staging deploy

--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -6,6 +6,7 @@ HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
 helm upgrade cla-backend \
   $HELM_DIR \
+  --namespace=${KUBE_ENV_STAGING_NAMESPACE} \
   --values ${HELM_DIR}/values-staging.yaml \
   --set image.tag=$IMAGE_TAG \
   --install

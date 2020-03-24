c74a5e1d74ac388871962c29efc19d866e66bc72
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-24 10:59:37 +0000

    Further tweaks to env var setting

--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -8,5 +8,6 @@ helm upgrade cla-backend \
   $HELM_DIR \
   --namespace=${KUBE_ENV_STAGING_NAMESPACE} \
   --values ${HELM_DIR}/values-staging.yaml \
+  --set image.repository=$DOCKER_REPOSITORY \
   --set image.tag=$IMAGE_TAG \
   --install

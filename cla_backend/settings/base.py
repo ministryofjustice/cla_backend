56c0459d2c4d92a96ea42b94d5a07967944fa7c1
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-25 15:02:24 +0000

    Get ALLOWED_HOSTS from env vars

--- cla_backend/settings/base.py
+++ cla_backend/settings/base.py
@@ -82,7 +82,7 @@ SITE_HOSTNAME = os.environ.get("SITE_HOSTNAME", "cla.local:8000")
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name

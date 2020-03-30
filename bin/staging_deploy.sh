b9f732f476d65f2e43dcfa35928045497597c0f6
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-03-30 15:15:23 +0100

    Change multideploy hosts to dashes rather than dots

--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -6,7 +6,7 @@ HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
 CLEANED_BRANCH_NAME=$(echo $CIRCLE_BRANCH | sed 's/^feature[-/]//' | sed 's:^\w*\/::' | tr -s ' _/[]().' '-' | tr '[:upper:]' '[:lower:]' | cut -c1-28 | sed 's/-$//')
 RELEASE_NAME=${CLEANED_BRANCH_NAME}
-RELEASE_HOST="$RELEASE_NAME.$STAGING_HOST"
+RELEASE_HOST="$RELEASE_NAME-$STAGING_HOST"
 
 helm upgrade $RELEASE_NAME \
   $HELM_DIR \

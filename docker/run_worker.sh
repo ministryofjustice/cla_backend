8ef16bc6cb9a084422f8fbfe62e4add3a41688c1
said-moj <45761276+said-moj@users.noreply.github.com>
2020-05-19 11:28:37 +0100

    Add Custom SQS transportation channel that works the limited permissions
    Blanks out all Listing, Creation and Deletion of actions
    Added extra functionality to predefine list of queues to use

--- docker/run_worker.sh
+++ docker/run_worker.sh
@@ -1,3 +1,3 @@
-                                                                                                          #!/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 exec celery worker -A cla_backend --concurrency=${WORKER_APP_CONCURRENCY:-4} --loglevel=${LOG_LEVEL:-INFO}

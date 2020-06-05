41fffb9b40e1eafbfa44f5ff9d3e7feb5e3c6e31
said-moj <45761276+said-moj@users.noreply.github.com>
2020-06-05 16:21:07 +0100

    Add static directory to project root

--- bin/local_deploy.sh
+++ bin/local_deploy.sh
@@ -5,7 +5,7 @@ ROOT=$(dirname "$0")
 HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
 kubectl config use-context docker-for-desktop
-
+docker build -t cla_backend_local "$ROOT/../"
 helm upgrade cla-backend \
   $HELM_DIR \
   --values ${HELM_DIR}/values-dev.yaml \

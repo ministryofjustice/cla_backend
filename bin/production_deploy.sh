9850613f91dc2fe17e0e1423cdc846a7329a76c6
said-moj <45761276+said-moj@users.noreply.github.com>
2020-07-07 10:52:40 +0100

    Added production deploy job and deploy script

---
+++ bin/production_deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT=$(dirname "$0")
+HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
+
+helm upgrade $RELEASE_NAME \
+  $HELM_DIR \
+  --namespace=${KUBE_ENV_PRODUCTION_NAMESPACE} \
+  --set fullnameOverride=$RELEASE_NAME \
+  --set host=$RELEASE_HOST \
+  --set image.repository=$DOCKER_REPOSITORY \
+  --set image.tag=$IMAGE_TAG \
+  --install

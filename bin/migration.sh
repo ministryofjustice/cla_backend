bc83bd6daf517c31837621b6fef8c1ec80232835
said-moj <said.shire@digital.justice.gov.uk>
2020-07-14 12:15:19 +0100

    Dump source database and restore on master
    Followed documentation on https://user-guide.cloud-platform.service.justice.gov.uk/documentation/other-topics/aws-rds-migration.html#step-2-sequences
    However skipped the step about dump/restoring sequences as a separate step because
    the sequence table structures are dumped as part of the pre-data and the sequences are inserted as part of the data dump

--- bin/migration.sh
+++ bin/migration.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-# check if environment variables exist
-# variable names
-#
-TARGET_DB_HOST='localhost'
-TARGET_DB_NAME='cla_backend_target2'
-TARGET_DB_USER='postgres'
-TARGET_DB_PASS=''
-
+# Make sure we can connect to the target database
 if PGPASSWORD=$TARGET_DB_PASS psql \
 -U $TARGET_DB_USER \
 -h $TARGET_DB_HOST \
@@ -20,11 +13,7 @@ else
   exit 1
 fi
 
-SOURCE_DB_HOST='localhost'
-SOURCE_DB_NAME='cla_backend'
-SOURCE_DB_USER='postgres'
-SOURCE_DB_PASS=''
-
+# Make sure we can connect to the source database
 if PGPASSWORD=$SOURCE_DB_PASS psql \
 -U $SOURCE_DB_USER \
 -h $SOURCE_DB_HOST \
@@ -36,63 +25,53 @@ else
   exit 1
 fi
 
-echo 'Dumping source database...'
-PGPASSWORD=$SOURCE_DB_PASS pg_dump -c -U $SOURCE_DB_USER \
-     -h $SOURCE_DB_HOST \
-     -d $SOURCE_DB_NAME \
-     > database.sql
+# Clear out the old database
+PGPASSWORD=$TARGET_DB_PASS psql -d $TARGET_DB_NAME -c 'drop schema public cascade;create schema public;'
 
-echo 'Restoring to target database...'
-PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
-     -h $TARGET_DB_HOST \
-     -d $TARGET_DB_NAME \
-     -f database.sql 2> errors.txt
-cat errors.txt
+##################################################################################################################
+# Dump the source database
+##################################################################################################################
+PGPASSWORD=$SOURCE_DB_PASS pg_dump -U $SOURCE_DB_USER \
+      -h $SOURCE_DB_HOST \
+      -d $SOURCE_DB_NAME \
+      -O \
+     --section=pre-data > pre-data.sql
 
-# echo 'Dumping sequences...'
-# PGPASSWORD=$SOURCE_DB_PASS pg_dump -U $SOURCE_DB_USER \
-#      -h $SOURCE_DB_HOST \
-#      -d $SOURCE_DB_NAME \
-#      -t '*_seq' > sequences.sql
+ echo 'Dumping data...'
+ PGPASSWORD=$SOURCE_DB_PASS pg_dump -U $SOURCE_DB_USER \
+      -h $SOURCE_DB_HOST \
+      -d $SOURCE_DB_NAME \
+      -O \
+      --section=data > data.sql
 
-# echo 'Dumping data...'
-# PGPASSWORD=$SOURCE_DB_PASS pg_dump -U $SOURCE_DB_USER \
-#      -h $SOURCE_DB_HOST \
-#      -d $SOURCE_DB_NAME \
-#      -O \
-#      --section=data > data.sql
-
-# echo 'Dumping post-data...'
-# PGPASSWORD=$SOURCE_DB_PASS pg_dump -U $SOURCE_DB_USER \
-#      -h $SOURCE_DB_HOST \
-#      -d $SOURCE_DB_NAME \
-#      -O \
-#      --section=post-data > post-data.sql
+ echo 'Dumping post-data...'
+ PGPASSWORD=$SOURCE_DB_PASS pg_dump -U $SOURCE_DB_USER \
+      -h $SOURCE_DB_HOST \
+      -d $SOURCE_DB_NAME \
+      -O \
+      --section=post-data > post-data.sql
 
 
-# echo 'Starting restore process'
+##################################################################################################################
+# Restore the target database
+##################################################################################################################
+ echo 'Doing restore pre-data'
+ PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
+      -h $TARGET_DB_HOST \
+      -d $TARGET_DB_NAME \
+      -f pre-data.sql \
+      2>> errors.txt
 
+ echo 'Doing restore data'
+ PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
+      -h $TARGET_DB_HOST \
+      -d $TARGET_DB_NAME \
+      -f data.sql \
+      2>> errors.txt
 
-# echo 'Doing restore pre-data'
-# PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
-#      -h $TARGET_DB_HOST \
-#      -d $TARGET_DB_NAME \
-#      -f pre-data.sql
-
-# echo 'Doing restore sequences'
-# PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER  \
-#      -h $TARGET_DB_HOST \
-#      -d $TARGET_DB_NAME \
-#      -f sequences.sql
-
-# echo 'Doing restore data'
-# PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
-#      -h $TARGET_DB_HOST \
-#      -d $TARGET_DB_NAME \
-#      -f data.sql
-
-# echo 'Doing restore post-data'
-# PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
-#      -h $TARGET_DB_HOST \
-#      -d $TARGET_DB_NAME \
-#      -f post-data.sql
+ echo 'Doing restore post-data'
+ PGPASSWORD=$TARGET_DB_PASS psql -U $TARGET_DB_USER \
+      -h $TARGET_DB_HOST \
+      -d $TARGET_DB_NAME \
+      -f post-data.sql \
+      2>> errors.txt

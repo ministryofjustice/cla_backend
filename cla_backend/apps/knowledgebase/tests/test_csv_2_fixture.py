0d27b236d67131989297e9cc2d32d84966f13c9a
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2020-07-16 11:23:38 +0100

    Add type check tests, tests for date times and modified expected data

--- cla_backend/apps/knowledgebase/tests/test_csv_2_fixture.py
+++ cla_backend/apps/knowledgebase/tests/test_csv_2_fixture.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
-from datetime import datetime
-from knowledgebase.management.commands._csv_2_fixture import KnowledgebaseCsvParse
-from django.utils import timezone
 import json
+from datetime import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.utils.dateparse import parse_datetime
+
+from knowledgebase.management.commands._csv_2_fixture import KnowledgebaseCsvParse
 
 
 class TestCSV2Fixture(TestCase):
@@ -14,133 +17,192 @@ class TestCSV2Fixture(TestCase):
 
     def compare_dicts(self, outputDict, expectedDict):
         for key, value in expectedDict.items():
-            self.assertEqual(outputDict[key], value)
+            parsed_datetime = parse_datetime(value)
+            if parse_datetime and isinstance(parsed_datetime, datetime):
+                self.compare_datetimes(parse_datetime(outputDict[key]), parsed_datetime)
+            self.assertIsInstance(value, type(outputDict[key]))
+
+    def compare_datetimes(self, outputDatetime, expectedDatetime):
+        time_diff = outputDatetime - expectedDatetime
+        time_diff_in_seconds = time_diff.total_seconds()
+        self.assertLessEqual(time_diff_in_seconds, 0.5)
 
     def compare_lists(self, outputList, expectedList):
         for expectedField in expectedList:
             for outputField in outputList:
                 if expectedField["pk"] == outputField["pk"]:
                     for key, value in expectedField.items():
-                        print(value)
                         if isinstance(value, dict):
-                            self.compare_dicts(outputField, expectedField)
-                        self.assertEqual(outputField[key], value)
+                            self.compare_dicts(outputField[key], value)
+                        self.assertIsInstance(value, type(outputField[key]))
 
     def test_fixture_with_required_article_category_fields(self):
-        file = open("./KnowledgebaseTestFiles/testcsv.csv")
+        file = open("./cla_backend/apps/knowledgebase/tests/CSVData/testcsv.csv")
         csv = KnowledgebaseCsvParse(file)
         outputJSON = csv.fixture_as_json()
         expectedList = [
             {
                 u"pk": 1,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": u"Debt", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Debt",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 2,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Education", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Education",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 3,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Discrimination", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Discrimination",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 4,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Housing", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Housing",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 5,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Family", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Family",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 6,
-                u"model": "knowledgebase.articlecategory",
+                u"model": u"knowledgebase.articlecategory",
                 u"fields": {
-                    u"name": "Welfare benefits",
-                    u"created": self.datetime_now,
-                    u"modified": self.datetime_now,
+                    u"name": u"Welfare benefits",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
                 },
             },
             {
                 u"pk": 7,
-                u"model": "knowledgebase.articlecategory",
+                u"model": u"knowledgebase.articlecategory",
                 u"fields": {
-                    u"name": "Action against police",
-                    u"created": self.datetime_now,
-                    u"modified": self.datetime_now,
+                    u"name": u"Action against police",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
                 },
             },
             {
                 u"pk": 8,
-                u"model": "knowledgebase.articlecategory",
+                u"model": u"knowledgebase.articlecategory",
                 u"fields": {
-                    u"name": "Clinical negligence",
-                    u"created": self.datetime_now,
-                    u"modified": self.datetime_now,
+                    u"name": u"Clinical negligence",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
                 },
             },
             {
                 u"pk": 9,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Community care", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Community care",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 10,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Consumer", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Consumer",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 11,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Crime", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Crime",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 12,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Employment", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Employment",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 13,
-                u"model": "knowledgebase.articlecategory",
+                u"model": u"knowledgebase.articlecategory",
                 u"fields": {
-                    u"name": "Immigration and asylum",
-                    u"created": self.datetime_now,
-                    u"modified": self.datetime_now,
+                    u"name": u"Immigration and asylum",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
                 },
             },
             {
                 u"pk": 14,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Mental health", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Mental health",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 15,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Miscelaneous", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Miscellaneous",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 16,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Personal injury", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Personal injury",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 17,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Publi", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Public",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
             {
                 u"pk": 18,
-                u"model": "knowledgebase.articlecategory",
-                u"fields": {u"name": "Generic", u"created": self.datetime_now, u"modified": self.datetime_now},
+                u"model": u"knowledgebase.articlecategory",
+                u"fields": {
+                    u"name": u"Generic",
+                    u"created": u"""{date}""".format(date=self.datetime_now),
+                    u"modified": u"""{date}""".format(date=self.datetime_now),
+                },
             },
         ]
         outputList = json.loads(outputJSON)
         self.compare_lists(outputList, expectedList)
-
-
-#         expectedJSON = self.format_json(expectedList)
-#         self.assertEqual(outputJSON, expectedJSON)

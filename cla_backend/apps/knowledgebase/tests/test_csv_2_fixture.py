f84bf9e66a021cd6d7ceb00d79d29c51edf84e3b
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2020-08-21 14:29:00 +0100

    Remove try - except blocks and unindent leftover space
    Was no longer necessary as the SystemExit exception could be raised from unexpected places e.g. from the machine instead of the executed code. Could lead to misleading error messages too

--- cla_backend/apps/knowledgebase/tests/test_csv_2_fixture.py
+++ cla_backend/apps/knowledgebase/tests/test_csv_2_fixture.py
@@ -93,199 +93,171 @@ class TestCSV2Fixture(TestCase):
     def test_fixture_with_required_article_category_fields(self):
         csv_file = self.find_path_to_csvfile("testcsv")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("SystemExit Exception with a error code of -1")
-        else:
-            outputJSON = csv.fixture_as_json()
-            outputList = json.loads(outputJSON)
-            expectedList = [
-                {u"name": u"Debt"},
-                {u"name": u"Education"},
-                {u"name": u"Discrimination"},
-                {u"name": u"Housing"},
-                {u"name": u"Family"},
-                {u"name": u"Welfare benefits"},
-                {u"name": u"Action against police"},
-                {u"name": u"Clinical negligence"},
-                {u"name": u"Community care"},
-                {u"name": u"Consumer"},
-                {u"name": u"Crime"},
-                {u"name": u"Employment"},
-                {u"name": u"Immigration and asylum"},
-                {u"name": u"Mental health"},
-                {u"name": u"Miscellaneous"},
-                {u"name": u"Personal injury"},
-                {u"name": u"Public"},
-                {u"name": u"Generic"},
-            ]
-            self.assertEqual(len(outputList), 18)
-            self.assertEqual(outputList[0].keys(), [u"pk", u"model", u"fields"])
-            self.assertEqual(outputList[0]["fields"].keys(), [u"created", u"name", u"modified"])
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        outputList = json.loads(outputJSON)
+        expectedList = [
+            {u"name": u"Debt"},
+            {u"name": u"Education"},
+            {u"name": u"Discrimination"},
+            {u"name": u"Housing"},
+            {u"name": u"Family"},
+            {u"name": u"Welfare benefits"},
+            {u"name": u"Action against police"},
+            {u"name": u"Clinical negligence"},
+            {u"name": u"Community care"},
+            {u"name": u"Consumer"},
+            {u"name": u"Crime"},
+            {u"name": u"Employment"},
+            {u"name": u"Immigration and asylum"},
+            {u"name": u"Mental health"},
+            {u"name": u"Miscellaneous"},
+            {u"name": u"Personal injury"},
+            {u"name": u"Public"},
+            {u"name": u"Generic"},
+        ]
+        self.assertEqual(len(outputList), 18)
+        self.assertEqual(outputList[0].keys(), [u"pk", u"model", u"fields"])
+        self.assertEqual(outputList[0]["fields"].keys(), [u"created", u"name", u"modified"])
 
-            for output, expectedDict in zip(outputList, expectedList):
-                for expectedKey, expectedValue in expectedDict.items():
-                    self.assertEqual(output["fields"][expectedKey], expectedValue)
+        for output, expectedDict in zip(outputList, expectedList):
+            for expectedKey, expectedValue in expectedDict.items():
+                self.assertEqual(output["fields"][expectedKey], expectedValue)
 
-            self.load_JSON_fixture_into_DB("testcsv")
+        self.load_JSON_fixture_into_DB("testcsv")
 
     def test_fixture_with_valid_entry_type_field(self):
         csv_file = self.find_path_to_csvfile("legal_resource_entry_type")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("Missing required fields")
-        else:
-            outputJSON = csv.fixture_as_json()
-            expectedDict = {
-                "resource_type": u"LEGAL",
-                "website": u"https://www.google.com",
-                "geographic_coverage": u"Baz",
-                "type_of_service": u"Baz",
-                "description": u"Bar",
-                "service_name": u"Bar",
-                "organisation": u"Foo",
-                "accessibility": u"Foo",
-                "when_to_use": u"Baz",
-                "how_to_use": u"Foo",
-                "address": u"Foo",
-                "keywords": u"Bar",
-                "opening_hours": u"Bar",
-            }
-            outputList = json.loads(outputJSON)
-            output_article_list = filter(lambda x: x["model"] == "knowledgebase.article", outputList)
-            self.assertEqual(len(output_article_list), 1)
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        expectedDict = {
+            "resource_type": u"LEGAL",
+            "website": u"https://www.google.com",
+            "geographic_coverage": u"Baz",
+            "type_of_service": u"Baz",
+            "description": u"Bar",
+            "service_name": u"Bar",
+            "organisation": u"Foo",
+            "accessibility": u"Foo",
+            "when_to_use": u"Baz",
+            "how_to_use": u"Foo",
+            "address": u"Foo",
+            "keywords": u"Bar",
+            "opening_hours": u"Bar",
+        }
+        outputList = json.loads(outputJSON)
+        output_article_list = filter(lambda x: x["model"] == "knowledgebase.article", outputList)
+        self.assertEqual(len(output_article_list), 1)
 
-            output_article = output_article_list[0]
-            for expectedKey, expectedValue in expectedDict.items():
-                self.assertEqual(output_article["fields"][expectedKey], expectedValue)
+        output_article = output_article_list[0]
+        for expectedKey, expectedValue in expectedDict.items():
+            self.assertEqual(output_article["fields"][expectedKey], expectedValue)
 
-            self.load_JSON_fixture_into_DB("legal_resource_entry_type")
+        self.load_JSON_fixture_into_DB("legal_resource_entry_type")
 
     def test_fixture_handling_with_entry_type_of_legal_resource_for_clients(self):
         csv_file = self.find_path_to_csvfile("legal_resource_entry_type")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("Missing required fields")
-        else:
-            outputJSON = csv.fixture_as_json()
-            outputList = json.loads(outputJSON)
-            outputArticle = outputList[-1]
-            self.assertEqual(outputArticle["fields"]["resource_type"], u"LEGAL")
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        outputList = json.loads(outputJSON)
+        outputArticle = outputList[-1]
+        self.assertEqual(outputArticle["fields"]["resource_type"], u"LEGAL")
 
     def test_fixture_handling_with_entry_type_of_other_resource_for_clients(self):
         csv_file = self.find_path_to_csvfile("csv_with_entry_type")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("Missing required fields")
-        else:
-            outputJSON = csv.fixture_as_json()
-            outputList = json.loads(outputJSON)
-            outputArticle = outputList[-1]
-            self.assertEqual(outputArticle["fields"]["resource_type"], u"OTHER")
-            self.load_JSON_fixture_into_DB("csv_with_entry_type")
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        outputList = json.loads(outputJSON)
+        outputArticle = outputList[-1]
+        self.assertEqual(outputArticle["fields"]["resource_type"], u"OTHER")
+        self.load_JSON_fixture_into_DB("csv_with_entry_type")
 
     def test_fixture_with_complete_article(self):
         csv_file = self.find_path_to_csvfile("complete_article")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("Missing required fields")
-        else:
-            outputJSON = csv.fixture_as_json()
-            outputList = json.loads(outputJSON)
-            expectedList = [
-                {u"article": 1, u"article_category": 1, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 2, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 3, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 4, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 5, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 6, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 7, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 8, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 9, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 10, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 11, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 12, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 13, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 14, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 15, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 16, u"preferred_signpost": False},
-                {u"article": 1, u"article_category": 17, u"preferred_signpost": True},
-                {u"article": 1, u"article_category": 18, u"preferred_signpost": True},
-            ]
-            outputList = json.loads(outputJSON)
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        outputList = json.loads(outputJSON)
+        expectedList = [
+            {u"article": 1, u"article_category": 1, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 2, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 3, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 4, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 5, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 6, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 7, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 8, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 9, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 10, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 11, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 12, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 13, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 14, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 15, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 16, u"preferred_signpost": False},
+            {u"article": 1, u"article_category": 17, u"preferred_signpost": True},
+            {u"article": 1, u"article_category": 18, u"preferred_signpost": True},
+        ]
+        outputList = json.loads(outputJSON)
 
-            output_acm = filter(lambda x: x["model"] == "knowledgebase.articlecategorymatrix", outputList)
-            output_acm_sorted, expected_acm_sorted = self.sort_article_category_matrices(output_acm, expectedList)
-            for output, expected in zip(output_acm_sorted, expected_acm_sorted):
-                for expectedKey, expectedValue in expected.items():
-                    self.assertEqual(output["fields"][expectedKey], expectedValue)
+        output_acm = filter(lambda x: x["model"] == "knowledgebase.articlecategorymatrix", outputList)
+        output_acm_sorted, expected_acm_sorted = self.sort_article_category_matrices(output_acm, expectedList)
+        for output, expected in zip(output_acm_sorted, expected_acm_sorted):
+            for expectedKey, expectedValue in expected.items():
+                self.assertEqual(output["fields"][expectedKey], expectedValue)
 
-            self.assertEqual(len(output_acm), 18)
+        self.assertEqual(len(output_acm), 18)
 
-            min_pk, max_pk = self.calculate_pk_range(output_acm)
-            self.assertEqual(min_pk, 1)
-            self.assertEqual(max_pk, 18)
-            self.load_JSON_fixture_into_DB("complete_article")
+        min_pk, max_pk = self.calculate_pk_range(output_acm)
+        self.assertEqual(min_pk, 1)
+        self.assertEqual(max_pk, 18)
+        self.load_JSON_fixture_into_DB("complete_article")
 
     def test_fixture_with_multiple_articles(self):
         csv_file = self.find_path_to_csvfile("multiple_articles")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("Missing required fields")
-        else:
-            outputJSON = csv.fixture_as_json()
-            outputList = json.loads(outputJSON)
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        outputList = json.loads(outputJSON)
 
-            sorted_records = defaultdict(list)
+        sorted_records = defaultdict(list)
 
-            for record in outputList:
-                sorted_records[record["model"]].append(record)
+        for record in outputList:
+            sorted_records[record["model"]].append(record)
 
-            article_category_records = sorted_records["knowledgebase.articlecategory"]
-            self.assertEqual(len(article_category_records), 18)
+        article_category_records = sorted_records["knowledgebase.articlecategory"]
+        self.assertEqual(len(article_category_records), 18)
 
-            article_records = sorted_records["knowledgebase.article"]
-            article_1, article_2 = article_records
+        article_records = sorted_records["knowledgebase.article"]
+        article_1, article_2 = article_records
 
-            self.assertEqual(len(article_records), 2)
-            self.assertEqual(article_1["fields"]["website"], "http://Baz")
-            self.assertEqual(article_2["fields"]["website"], "http://Website 2")
-            self.assertNotEqual(article_1["pk"], article_2["pk"])
+        self.assertEqual(len(article_records), 2)
+        self.assertEqual(article_1["fields"]["website"], "http://Baz")
+        self.assertEqual(article_2["fields"]["website"], "http://Website 2")
+        self.assertNotEqual(article_1["pk"], article_2["pk"])
 
-            article_category_matrices = sorted_records["knowledgebase.articlecategorymatrix"]
-            min_pk, max_pk = self.calculate_pk_range(article_category_matrices)
+        article_category_matrices = sorted_records["knowledgebase.articlecategorymatrix"]
+        min_pk, max_pk = self.calculate_pk_range(article_category_matrices)
 
-            self.assertEqual(len(article_category_matrices), 36)
-            self.assertEqual(min_pk, 1)
-            self.assertEqual(max_pk, 36)
+        self.assertEqual(len(article_category_matrices), 36)
+        self.assertEqual(min_pk, 1)
+        self.assertEqual(max_pk, 36)
 
-            first_set_of_acm = filter(lambda x: x["fields"]["article"] == 1, article_category_matrices)
-            second_set_of_acm = filter(lambda x: x["fields"]["article"] == 2, article_category_matrices)
+        first_set_of_acm = filter(lambda x: x["fields"]["article"] == 1, article_category_matrices)
+        second_set_of_acm = filter(lambda x: x["fields"]["article"] == 2, article_category_matrices)
 
-            self.assertEqual(len(first_set_of_acm), 18)
-            self.assertEqual(len(second_set_of_acm), 18)
-            self.load_JSON_fixture_into_DB("multiple_articles")
+        self.assertEqual(len(first_set_of_acm), 18)
+        self.assertEqual(len(second_set_of_acm), 18)
+        self.load_JSON_fixture_into_DB("multiple_articles")
 
     def test_fixture_with_empty_csv(self):
         csv_file = self.find_path_to_csvfile("empty_csv")
         file = open(csv_file)
-        try:
-            csv = KnowledgebaseCsvParse(file)
-        except SystemExit:
-            self.fail("SystemExit Exception with a error code of -1")
-        else:
-            outputJSON = csv.fixture_as_json()
-            outputList = json.loads(outputJSON)
-            self.assertEqual(len(outputList), 18)
-            self.load_JSON_fixture_into_DB("empty_csv")
+        csv = KnowledgebaseCsvParse(file)
+        outputJSON = csv.fixture_as_json()
+        outputList = json.loads(outputJSON)
+        self.assertEqual(len(outputList), 18)
+        self.load_JSON_fixture_into_DB("empty_csv")

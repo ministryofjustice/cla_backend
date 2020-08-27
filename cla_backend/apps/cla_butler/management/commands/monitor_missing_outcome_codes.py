eb2d0c1f7fe3a09bcfa92b5ba4ecd548b5614266
said-moj <said.shire@digital.justice.gov.uk>
2020-08-27 11:48:21 +0100

    Remove check to see if we are in an EC2 instance
    Removed checks to see if we are in the first EC2 instance before running the cla_butler management commands

--- cla_backend/apps/cla_butler/management/commands/monitor_missing_outcome_codes.py
+++ cla_backend/apps/cla_butler/management/commands/monitor_missing_outcome_codes.py
@@ -2,8 +2,6 @@
 import logging
 from django.core.management.base import BaseCommand
 
-from cla_butler.stack import is_first_instance, InstanceNotInAsgException, StackException
-
 from cla_eventlog.constants import LOG_LEVELS, LOG_TYPES
 from cla_eventlog.models import Log
 from legalaid.models import Case
@@ -18,10 +16,7 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        if self.should_run_housekeeping(**options):
-            self.check_for_missing_outcome_codes()
-        else:
-            logger.info("Skip check_for_missing_outcome_codes because running on secondary instance")
+        self.check_for_missing_outcome_codes()
 
     @staticmethod
     def check_for_missing_outcome_codes():
@@ -39,16 +34,3 @@ class Command(BaseCommand):
             )
         else:
             logger.info("LGA-275 No cases found missing denormalized outcome codes")
-
-    @staticmethod
-    def should_run_housekeeping(**options):
-        if options.get("force", False):
-            return True
-        try:
-            return is_first_instance()
-        except InstanceNotInAsgException:
-            logger.info("EC2 instance not in an ASG")
-            return True
-        except StackException:
-            logger.info("Not running on EC2 instance")
-            return True

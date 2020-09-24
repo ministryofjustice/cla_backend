dfa829975c38f7ffc7cf7e10942c16b5c1456f54
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-09-24 16:13:14 +0100

    Stop non-UAT releases using unique names
    Sets $RELEASE_NAME to 'cla-backend' on staging and production, so that
    the Helm chart is consistently named and therefore future releases
    update the chart regardless of branch name (although the branch name
    will usually be consistent for these deploy types anyway).
    
    Removes the fullnameOverride on staging and production, as we don't want
    or need our Kubernetes objects to have branch-specific names - again,
    that's a UAT environment thing allowing multiple releases to exist at
    once, and is detrimental on staging and production for similar reasons
    to the release naming. Without the override, these releases will default
    to being called 'cla-backend', from the Chart.yaml definition, and this
    is what will be the basis of the naming of the deployments, ingresses
    etc created for the release.

--- bin/production_deploy.sh
+++ bin/production_deploy.sh
@@ -7,7 +7,6 @@ HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 helm upgrade $RELEASE_NAME \
   $HELM_DIR \
   --namespace=${KUBE_ENV_PRODUCTION_NAMESPACE} \
-  --set fullnameOverride=$RELEASE_NAME \
   --set host=$RELEASE_HOST \
   --set secretName=tls-certificate \
   --set image.repository=$DOCKER_REPOSITORY \

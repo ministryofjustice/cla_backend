2cf9d6779c6f7a6bb1f70590cc1a2b0eeb4cb2bc
said-moj <said.shire@digital.justice.gov.uk>
2020-10-07 10:18:50 +0100

    Add certificate for new staging domain
    Also removed redundant Values.ingress.hosts and Values.ingress.paths as the .Values.host is set in the deploy script and used frequently

--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -9,6 +9,7 @@ helm upgrade $RELEASE_NAME \
   --namespace=${KUBE_ENV_STAGING_NAMESPACE} \
   --values ${HELM_DIR}/values-staging.yaml \
   --set host=$RELEASE_HOST \
+  --set secretName=tls-certificate \
   --set image.repository=$DOCKER_REPOSITORY \
   --set image.tag=$IMAGE_TAG \
   --set-string pingdomIPs=$PINGDOM_IPS \

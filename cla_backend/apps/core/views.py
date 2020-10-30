767b9867a1e28063fae33ea46478372818b5a129
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-10-30 14:35:30 +0000

    Tag sentry event with each part of path

--- cla_backend/apps/core/views.py
+++ cla_backend/apps/core/views.py
@@ -4,8 +4,9 @@ from sentry_sdk import capture_message, push_scope
 
 def page_not_found(request, *args, **kwargs):
     with push_scope() as scope:
-        scope.set_tag("type", "404")
-        scope.set_extra("path", request.path)
+        scope.set_tag("path", request.path)
+        for i, part in enumerate(request.path.strip("/").split("/")):
+            scope.set_tag("path_{}".format(i), part)
 
         capture_message("Page not found", level="error")
 

f576004e7d1352c7e8c1e203ae0a8b6769ce1b08
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-10-30 13:55:50 +0000

    Set some event data on 404 logging

--- cla_backend/apps/core/views.py
+++ cla_backend/apps/core/views.py
@@ -1,8 +1,12 @@
 from django.views import defaults
-from sentry_sdk import capture_message
+from sentry_sdk import capture_message, push_scope
 
 
-def page_not_found(*args, **kwargs):
-    capture_message("Page not found", level="error")
+def page_not_found(request, *args, **kwargs):
+    with push_scope() as scope:
+        scope.set_tag("type", "404")
+        scope.set_extra("path", request.path)
 
-    return defaults.page_not_found(*args, **kwargs)
+        capture_message("Page not found", level="error")
+
+    return defaults.page_not_found(request, *args, **kwargs)

257eec792c21dbb270efd6be3e5512d5afa537f2
said-moj <said.shire@digital.justice.gov.uk>
2020-11-05 12:23:49 +0000

    Using rds database instead of local database

--- bin/uat_deploy.sh
+++ bin/uat_deploy.sh
@@ -2,12 +2,16 @@
 set -e
 
 ROOT=$(dirname "$0")
+DYNAMIC_HOSTNAME=$1
 HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
-
+VALUES="values-uat-static.yaml"
+if [ $DYNAMIC_HOSTNAME = true ]; then
+  VALUES="values-uat.yaml"
+fi
 helm upgrade $RELEASE_NAME \
   $HELM_DIR \
   --namespace=${KUBE_ENV_UAT_NAMESPACE} \
-  --values ${HELM_DIR}/values-uat.yaml \
+  --values "${HELM_DIR}/$VALUES" \
   --set fullnameOverride=$RELEASE_NAME \
   --set environment=$RELEASE_NAME \
   --set host=$RELEASE_HOST \

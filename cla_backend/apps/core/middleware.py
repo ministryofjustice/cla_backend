e2919039a20a255232fefe3b78e173587710baf0
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2020-12-01 15:23:23 +0000

    Remove statsd code and resultant redundant code

--- cla_backend/apps/core/middleware.py
+++ cla_backend/apps/core/middleware.py
@@ -1,12 +1,3 @@
-from django.http import Http404
-from django_statsd.clients import statsd
-
-
 class GraphiteMiddleware(object):
     def process_response(self, request, response):
-        statsd.incr("response.%s" % response.status_code)
         return response
-
-    def process_exception(self, request, exception):
-        if not isinstance(exception, Http404):
-            statsd.incr("response.500")

8756a8a66f8f812010b575bf068c55f81d558201
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2020-12-07 10:09:15 +0000

    Undo commit due to deleting necessary code
    Specifically line 9

--- cla_backend/apps/core/signals.py
+++ cla_backend/apps/core/signals.py
@@ -1,18 +1,23 @@
 import logging
 
+from django_statsd.clients import statsd
+
 logger = logging.getLogger(__name__)
 
 
 def log_user_created(sender, instance, created, **kwargs):
-    logger.info(
-        "User created",
-        extra={
-            "USERNAME": instance.username,
-            "IS_STAFF": unicode(instance.is_staff),
-            "IS_ACTIVE": unicode(instance.is_active),
-            "IS_SUPERUSER": unicode(instance.is_superuser),
-        },
-    )
+    if created:
+        statsd.incr("user.created")
+
+        logger.info(
+            "User created",
+            extra={
+                "USERNAME": instance.username,
+                "IS_STAFF": unicode(instance.is_staff),
+                "IS_ACTIVE": unicode(instance.is_active),
+                "IS_SUPERUSER": unicode(instance.is_superuser),
+            },
+        )
 
 
 def log_user_modified(sender, instance, **kwargs):
@@ -20,6 +25,8 @@ def log_user_modified(sender, instance, **kwargs):
         sender.objects.get(pk=instance.pk)
     except sender.DoesNotExist:
         return
+
+    statsd.incr("user.modified")
 
     logger.info(
         "User modified",

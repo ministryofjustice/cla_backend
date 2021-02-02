0f7195ef33c5bbfa366ff6b63a704d5584599397
said-moj <said.shire@digital.justice.gov.uk>
2021-02-02 13:16:05 +0000

    Bulk import  knowledge.Article models from csv

---
+++ cla_backend/apps/knowledgebase/tests/test_csv_user_import.py
@@ -0,0 +1,137 @@
+import datetime
+from django.test import TestCase
+from knowledgebase.utils.csv_user_import import KnowledgebaseCSVImporter
+from knowledgebase.utils.csv_user_import_mappings import (
+    ARTICLE_COLUMN_FIELD_MAPPING,
+    TELEPHONE_COLUMN_FIELD_MAPPING,
+    ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING,
+)
+from core.tests.mommy_utils import make_recipe
+
+
+class KnowledgebaseCSVImporterTester(TestCase):
+    def setUp(self):
+        self.importer = KnowledgebaseCSVImporter(None)
+        self.row = self.get_csv_row()
+
+    def test_article_row_import__new_article(self):
+        article = self.importer.get_article_from_row(ARTICLE_COLUMN_FIELD_MAPPING, self.row)
+        # Set the pk field in the csv row to None
+        self.row[0] = None
+        self._assert_row_against_article(self.row, article)
+
+    def test_article_row_import__existing_article(self):
+        existing_article = make_recipe("knowledgebase.article")
+        # Set the pk field in the csv row to to an existing Article pk
+        self.row[0] = existing_article.pk
+        article = self.importer.get_article_from_row(ARTICLE_COLUMN_FIELD_MAPPING, self.row)
+        self._assert_row_against_article(self.row, article)
+
+    def test_telephone_numbers_row(self):
+        article = make_recipe("knowledgebase.article")
+        telephone_numbers = self.importer.get_telephone_numbers_from_row(
+            article, TELEPHONE_COLUMN_FIELD_MAPPING, self.row
+        )
+        self.assertEqual(len(telephone_numbers), 2)
+        self._assert_row_telephone(self.row, TELEPHONE_COLUMN_FIELD_MAPPING[0], telephone_numbers[0])
+        self._assert_row_telephone(self.row, TELEPHONE_COLUMN_FIELD_MAPPING[1], telephone_numbers[1])
+
+    def test_article_category_matrices_from_row(self):
+        employment_category = make_recipe("knowledgebase.article_category", name="Employment")
+        discrimination_category = make_recipe("knowledgebase.article_category", name="Discrimination")
+        article = make_recipe("knowledgebase.article")
+        matrices = self.importer.get_get_article_category_matrices_from_row(
+            article, ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING, self.row
+        )
+        self.assertEqual(len(matrices), 2)
+        self.assertEqual(matrices[0].article_category, employment_category)
+        self.assertFalse(matrices[0].preferred_signpost)
+        self.assertEqual(matrices[1].article_category, discrimination_category)
+        self.assertTrue(matrices[1].preferred_signpost)
+
+    def test_article_category_matrices_from_row__non_existing_category(self):
+        article = make_recipe("knowledgebase.article")
+        with self.assertRaisesMessage(ValueError, "Could not find category with name: Employment"):
+            self.importer.get_get_article_category_matrices_from_row(
+                article, ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING, self.row
+            )
+
+    def test_article_category_matrices_from_row__invalid_preferred_signpost(self):
+        make_recipe("knowledgebase.article_category", name="Employment")
+        article = make_recipe("knowledgebase.article")
+        with self.assertRaisesMessage(ValueError, "Yes is an invalid value for Preferred signpost"):
+            signpost_index = ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING[0][1][0]
+            self.row[signpost_index] = "Yes"
+            self.importer.get_get_article_category_matrices_from_row(
+                article, ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING, self.row
+            )
+
+    def _assert_row_telephone(self, row, fields, telephone):
+        for column, field_name in fields:
+            expected_value = row[column]
+            if expected_value == "":
+                expected_value = None
+            model_value = getattr(telephone, field_name)
+            error_msg = "TelephoneNumber.%s expected value %s` got %s instead" % (
+                field_name,
+                expected_value,
+                model_value,
+            )
+            self.assertEqual(expected_value, model_value, error_msg)
+
+    def _assert_row_against_article(self, row, article):
+        for column, field_name in ARTICLE_COLUMN_FIELD_MAPPING:
+            expected_value = row[column]
+            model_value = getattr(article, field_name)
+            if type(model_value) == datetime.datetime:
+                model_value = str(model_value)
+            error_msg = "Article.%s expected value %s` got %s instead" % (field_name, expected_value, model_value)
+            self.assertEqual(expected_value, model_value, error_msg)
+
+    def get_csv_row(self):
+        return [
+            "",
+            "2014-06-27 14:52:55.547000+00:00",
+            "2015-01-28 15:06:18.718000+00:00",
+            "LEGAL",
+            "LAA",
+            "[PREF DISCRIMINATION]",
+            "LAA (Legal Aid Agency)",
+            "https,//www.gov.uk/government/organisations/legal-aid-agency",
+            "",
+            "This is the article description",
+            "This is the article description",
+            "This ios how to use it",
+            "This is when to use it",
+            """102 Petty France
+            LONDON
+            SW1H 9AJ
+            """,
+            """Monday-Friday, 8am-8pm
+            Saturday, 9am-1pm
+            """,
+            "employment, helpline, employers, contract, tribunal, grievance",
+            "UK",
+            "Employment advice helpline",
+            "This is accessible text",
+            "",
+            "0300 123 1100",
+            "Text relay",
+            "18001 0300 123 1100",
+            "",
+            "",
+            "",
+            "",
+            "Employment",
+            "FALSE",
+            "Discrimination",
+            "TRUE",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+        ]
---
+++ cla_backend/apps/knowledgebase/utils/csv_user_import.py
@@ -0,0 +1,117 @@
+import csv
+from django.db import transaction
+from knowledgebase.models import Article, TelephoneNumber, ArticleCategoryMatrix
+from .csv_user_import_mappings import (
+    ARTICLE_COLUMN_FIELD_MAPPING,
+    TELEPHONE_COLUMN_FIELD_MAPPING,
+    ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING,
+)
+
+
+class KnowledgebaseCSVImporter:
+    def __init__(self, csv_file_absolute_path):
+        self.csv_file_absolute_path = csv_file_absolute_path
+
+    def parse(self):
+        rows = []
+        with open(self.csv_file_absolute_path, "rb") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            # Skip header
+            next(reader)
+            for row in reader:
+                rows.append(self.process_row(row))
+        return rows
+
+    def save(self, rows):
+        with transaction.atomic():
+            for row in rows:
+                row["article"].save()
+                self.save_telephone_numbers(row["article"], row["telephone_numbers"])
+                self.save_article_category_matrices(row["article"], row["article_category_matrices"])
+
+    def save_telephone_numbers(self, article, telephone_numbers):
+        existing_telephone_numbers = TelephoneNumber.objects.get_or_none(article=article)
+        if existing_telephone_numbers:
+            existing_telephone_numbers.delete()
+
+        for telephone_number in telephone_numbers:
+            telephone_number.save()
+
+    def save_article_category_matrices(self, article, matrices):
+        existing_matrices = ArticleCategoryMatrix.objects.get_or_none(article=article)
+        if existing_matrices:
+            existing_matrices.delete()
+
+        for matrix in matrices:
+            matrix.save()
+
+    def process_row(self, row):
+        article = self.get_article_from_row(ARTICLE_COLUMN_FIELD_MAPPING, row)
+        telephone_numbers = self.get_telephone_numbers_from_row(article, TELEPHONE_COLUMN_FIELD_MAPPING, row)
+        article_category_matrices = self.get_get_article_category_matrices_from_row(
+            article, ARTICLE_CATEGORY_MATRIX_COLUMN_FIELD_MAPPING, row
+        )
+        return {
+            "article": article,
+            "telephone_numbers": telephone_numbers,
+            "article_category_matrices": article_category_matrices,
+        }
+
+    def get_article_from_row(self, fields, row):
+        data = {}
+        pk = None
+        for column, field in fields:
+            if field == "pk":
+                pk = row[column]
+            else:
+                data[field] = row[column]
+
+        if pk:
+            article = Article.objects.get(pk=pk)
+            for field, value in data.items():
+                setattr(article, field, value)
+        else:
+            article = Article(**data)
+
+        article.full_clean()
+        return article
+
+    def get_telephone_numbers_from_row(self, article, mappings, row):
+        telephone_numbers = []
+        for fields in mappings:
+            telephone_number = self._get_telephone_number_from_row(article, fields, row)
+            if telephone_number:
+                telephone_numbers.append(telephone_number)
+        return telephone_numbers
+
+    def get_get_article_category_matrices_from_row(self, article, mappings, row):
+        matrices = []
+        for fields in mappings:
+            matrix = self.get_get_article_category_matrix_from_row(article, fields, row)
+            if matrix:
+                matrices.append(matrix)
+        return matrices
+
+    def _get_telephone_number_from_row(self, article, fields, row):
+        data = {}
+        telephone_number = None
+        for column, field in fields:
+            if row[column]:
+                data[field] = row[column]
+        if data:
+            data["article"] = article
+            telephone_number = TelephoneNumber(**data)
+            telephone_number.full_clean()
+        return telephone_number
+
+    def get_get_article_category_matrix_from_row(self, article, fields, row):
+        data = {}
+        matrix = None
+        for column, field, value_transformer in fields:
+            if row[column]:
+                data[field] = value_transformer(row[column])
+        if data:
+            data["article"] = article
+            matrix = ArticleCategoryMatrix(**data)
+            matrix.full_clean()
+        return matrix

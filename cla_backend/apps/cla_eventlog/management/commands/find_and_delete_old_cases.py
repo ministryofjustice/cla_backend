30b1e02a74b15d763cc115fae037fbe247cabf08
MayowaAderohunmu-Adeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2021-04-30 16:00:00 +0100

    Refactor
    In the FindAndDeleteCasesUsingCreationTime method, we filter for cases that are created 2 years ago and do not have any logs created within 2 years (implicitly including cases that do not have any logs too!).

--- cla_backend/apps/cla_eventlog/management/commands/find_and_delete_old_cases.py
+++ cla_backend/apps/cla_eventlog/management/commands/find_and_delete_old_cases.py
@@ -3,25 +3,13 @@ from dateutil.relativedelta import relativedelta
 
 from legalaid.models import Case
 from cla_butler.tasks import DeleteOldData
-from cla_eventlog.models import Log
 
 
 class FindAndDeleteCasesUsingCreationTime(DeleteOldData):
     def get_eligible_cases(self):
         two_years = self.now - relativedelta(years=2)
-        old_cases = []
 
-        filtered_cases = Case.objects.filter(created__lte=two_years)
-        for case in filtered_cases:
-            try:
-                latest_log = Log.objects.filter(case__id=case.id).latest("created")
-                if latest_log.created <= two_years:
-                    old_cases.append(case.id)
-            except Log.DoesNotExist:
-                # Old case without any event logs
-                old_cases.append(case.id)
-
-        return Case.objects.filter(id__in=old_cases)
+        return Case.objects.filter(created__lte=two_years).exclude(log__created__gte=two_years)
 
 
 class Command(BaseCommand):

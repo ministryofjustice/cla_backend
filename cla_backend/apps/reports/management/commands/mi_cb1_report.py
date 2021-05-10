70d435e1176a1132db6a04c34c04567df354d1d9
said-moj <said.shire@digital.justice.gov.uk>
2021-05-10 15:50:34 +0100

    Send weekly report to aws

--- cla_backend/apps/reports/management/commands/mi_cb1_report.py
+++ cla_backend/apps/reports/management/commands/mi_cb1_report.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 import logging
 from django.core.management.base import BaseCommand
-
+from reports.tasks import ExportTask
+from core.models import get_web_user
+from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
 
@@ -12,10 +14,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.create_report()
 
-    def create_report():
-        print("stuff goes here")
-
-        # '{"action": "Export", "csrfmiddlewaretoken": "PQk4Pt55CL0NBapx9hSqZTJkSn6tL6TL", "date_from": "08/05/2021", "date_to": "10/05/2021"}'
+    @csrf_exempt
+    def create_report(self):
+        report_data = '{"action": "Export", "csrfmiddlewaretoken": "PQk4Pt55CL0NBapx9hSqZTJkSn6tL6TL", "date_from": "2021-05-08", "date_to": "2021-05-10"}'
 
         # report_data = json_stuff_goes_here
-        #         ExportTask().delay(user_person.pk, filename_of_report, mi_cb1_extract_agilisys, report_data)
+        web_user = get_web_user()
+        filename_of_report = "WEEKLY-REPORT-TEST.csv"
+        ExportTask().delay(web_user.pk, filename_of_report, "MICB1Extract", report_data)

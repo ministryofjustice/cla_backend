6aa904fe42759e1d9f9736ce3be61110fed020cd
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-06-02 17:11:14 +0100

    amended extract name and dates

--- cla_backend/apps/reports/management/commands/mi_cb1_report.py
+++ cla_backend/apps/reports/management/commands/mi_cb1_report.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import logging
+from datetime import date, timedelta
 from django.core.management.base import BaseCommand
 from reports.tasks import ExportTask
 from core.models import get_web_user
@@ -16,8 +17,13 @@ class Command(BaseCommand):
 
     @csrf_exempt
     def create_report(self):
-        report_data = '{"action": "Export", "csrfmiddlewaretoken": "PQk4Pt55CL0NBapx9hSqZTJkSn6tL6TL", "date_from": "2021-05-08", "date_to": "2021-05-10"}'
+        date_from = (date.today() - timedelta(days=7)).strftime("%Y-%m-%d")
+        date_to = date.today().strftime("%Y-%m-%d")
+
+        report_data = '{{"action": "Export", "csrfmiddlewaretoken": "PQk4Pt55CL0NBapx9hSqZTJkSn6tL6TL", "date_from": {0}, "date_to": {1}}}'.format(
+            date_from, date_to
+        )
 
         web_user = get_web_user()
-        filename_of_report = "WEEKLY-REPORT-TEST.csv"
+        filename_of_report = "scheduled-mi-cb1-extract.csv"
         ExportTask().delay(web_user.pk, filename_of_report, "MICB1Extract", report_data)

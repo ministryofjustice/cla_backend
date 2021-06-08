9fc4387a281662767222a010c17847d24ab626d1
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2021-06-08 15:18:06 +0100

    Make command modify the cases that need modifying
    When passed a "commit" arg

--- cla_backend/apps/legalaid/management/commands/recalculate_assigned_out_of_hours.py
+++ cla_backend/apps/legalaid/management/commands/recalculate_assigned_out_of_hours.py
@@ -8,6 +8,10 @@ from legalaid.models import Case
 class Command(BaseCommand):
     help = "Recalculate case.assigned_out_of_hours since a given date"
 
+    unchanged = []
+    change_to_true = []
+    change_to_false = []
+
     def handle(self, *args, **options):
         try:
             date_string = args[0]
@@ -22,27 +26,42 @@ class Command(BaseCommand):
         cases = Case.objects.filter(provider_assigned_at__gte=dt)
         count = cases.count()
 
-        self.stdout.write("{count} cases assigned since {dt}.".format(count=count, dt=dt))
+        self.stdout.write(u"{count} cases assigned since {dt}.".format(count=count, dt=dt))
         if not count:
             return
 
-        unchanged = []
-        changed_to_true = []
-        changed_to_false = []
+        self.group_cases(cases)
 
+        self.stdout.write(u"{count} cases already had the correct value.".format(count=len(self.unchanged)))
+        self.stdout.write(u"{count} cases will be changed to `True`.".format(count=len(self.change_to_true)))
+        self.stdout.write(u"{count} cases will be changed to `False`.".format(count=len(self.change_to_false)))
+
+        try:
+            commit = args[1] == "commit"
+        except IndexError:
+            commit = False
+
+        if commit:
+            number_changed_to_true = Case.objects.filter(pk__in=self.change_to_true).update(assigned_out_of_hours=True)
+            number_changed_to_false = Case.objects.filter(pk__in=self.change_to_false).update(
+                assigned_out_of_hours=False
+            )
+            self.stdout.write(u"Making changes")
+            self.stdout.write(u"  {count} cases changed to `True`.".format(count=number_changed_to_true))
+            self.stdout.write(u"  {count} cases changed to `False`.".format(count=number_changed_to_false))
+        else:
+            self.stdout.write(u"Not making changes (add 'commit' to make changes)")
+
+    def group_cases(self, cases):
         for case in cases:
             current_value = case.assigned_out_of_hours
             new_value = self.recalculate_field(case)
             if new_value == current_value:
-                unchanged.append(case.pk)
+                self.unchanged.append(case.pk)
             elif new_value:
-                changed_to_true.append(case.pk)
+                self.change_to_true.append(case.pk)
             else:
-                changed_to_false.append(case.pk)
-
-        self.stdout.write("{count} cases already had the correct value.".format(count=len(unchanged)))
-        self.stdout.write("{count} cases will be changed to `True`.".format(count=len(changed_to_true)))
-        self.stdout.write("{count} cases will be changed to `False`.".format(count=len(changed_to_false)))
+                self.change_to_false.append(case.pk)
 
     def recalculate_field(self, case):
         case_category = getattr(case.eligibility_check.category, "code") if case.eligibility_check else None

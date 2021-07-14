e5bb3e313bcb504df215fb20c4d3631a6b4b3079
said-moj <said.shire@digital.justice.gov.uk>
2021-07-14 09:28:11 +0100

    Add additional logging data to when login fails

--- cla_backend/apps/cla_auth/views.py
+++ cla_backend/apps/cla_auth/views.py
@@ -1,6 +1,5 @@
 import logging
-from sentry_sdk import capture_exception
-
+import json
 
 from ipware.ip import get_ip
 from rest_framework.exceptions import Throttled
@@ -42,14 +41,7 @@ class AccessTokenView(Oauth2AccessTokenView):
         try:
             self.check_throttles(request)
         except Throttled as exc:
-            logger.info(
-                "login throttled",
-                extra={
-                    "IP": get_ip(request),
-                    "HTTP_REFERER": request.META.get("HTTP_REFERER"),
-                    "HTTP_USER_AGENT": request.META.get("HTTP_USER_AGENT"),
-                },
-            )
+            logger.info("login throttled: {}".format(self._get_request_log_extras(request)))
             response = self.error_response({"error": "throttled", "detail": exc.detail}, status=exc.status_code)
 
             if exc.wait:
@@ -61,16 +53,9 @@ class AccessTokenView(Oauth2AccessTokenView):
     def get_password_grant(self, request, data, client):
         form = ClientIdPasswordGrantForm(data, client=client)
         if not form.is_valid():
-            logger.info(
-                "login failed",
-                extra={
-                    "IP": get_ip(request),
-                    "USERNAME": request.POST.get("username"),
-                    "CLIENT_SECRET": request.POST.get("client_secret"),
-                    "HTTP_REFERER": request.META.get("HTTP_REFERER"),
-                    "HTTP_USER_AGENT": request.META.get("HTTP_USER_AGENT"),
-                },
-            )
+            log_extras = self._get_request_log_extras(request)
+            log_extras["FORM_ERRORS"] = form.errors
+            logger.info("login failed: {}".format(json.dumps(log_extras)))
 
             form.on_form_invalid()
 
@@ -78,21 +63,21 @@ class AccessTokenView(Oauth2AccessTokenView):
         else:
             form.on_form_valid()
 
-        logger.info(
-            "login succeeded",
-            extra={
-                "IP": get_ip(request),
-                "USERNAME": request.POST.get("username"),
-                "CLIENT_SECRET": request.POST.get("client_secret"),
-                "HTTP_REFERER": request.META.get("HTTP_REFERER"),
-                "HTTP_USER_AGENT": request.META.get("HTTP_USER_AGENT"),
-            },
-        )
+        logger.info("login succeeded: {}".format(json.dumps(self._get_request_log_extras(request))))
         return form.cleaned_data
+
+    def _get_request_log_extras(self, request):
+        return {
+            "IP": get_ip(request),
+            "USERNAME": request.POST.get("username"),
+            "CLIENT_ID": request.POST.get("client_id"),
+            "GRANT_TYPE": request.POST.get("grant_type"),
+            "HTTP_REFERER": request.META.get("HTTP_REFERER"),
+            "HTTP_USER_AGENT": request.META.get("HTTP_USER_AGENT"),
+        }
 
     def error_response(self, error, content_type="application/json", status=400, **kwargs):
         response = super(AccessTokenView, self).error_response(error, content_type, status, **kwargs)
         message = "INVESTIGATE-LGA-1746: {} {}".format(response.status_code, response.content)
-        capture_exception()
         logging.info(message)
         return response

60a84b2e70f2a963c4420902b8733a27713d6081
mayowaadeniyi <mayowa.adeniyi@digital.justice.gov.uk>
2021-10-18 13:23:00 +0100

    Remove defaults and set values as env vars in circleci context
    Cluster name is INGRESS_CLUSTER_NAME and is set to blue. Cluster weight is INGRESS_CLUSTER_WEIGHT and is set to 100

--- bin/uat_deploy.sh
+++ bin/uat_deploy.sh
@@ -15,8 +15,8 @@ helm upgrade $RELEASE_NAME \
   --set fullnameOverride=$RELEASE_NAME \
   --set environment=$RELEASE_NAME \
   --set host=$RELEASE_HOST \
-  --set ingress.cluster.name=${INGRESS_CLUSTER_NAME:-blue} \
-  --set ingress.cluster.weight=${INGRESS_CLUSTER_WEIGHT:-100} \
+  --set ingress.cluster.name=${INGRESS_CLUSTER_NAME} \
+  --set ingress.cluster.weight=${INGRESS_CLUSTER_WEIGHT} \
   --set image.repository=$DOCKER_REPOSITORY \
   --set image.tag=$IMAGE_TAG \
   --set-string pingdomIPs=$PINGDOM_IPS \

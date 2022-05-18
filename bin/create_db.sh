b99de24529f2c6d3f3bbb05d5f6a095d78391c6c
Heather Poole <heather.poole@digital.justice.gov.uk>
2022-05-18 13:26:15 +0100

    amended script for clarity

--- bin/create_db.sh
+++ bin/create_db.sh
@@ -21,10 +21,14 @@ load_test_data() {
         python manage.py loaddata test_rotas.json
         python manage.py loaddata kb_from_knowledgebase.json
 
-        if [ "$LOAD_END_TO_END_FIXTURES" == "True"]; then
-            python manage.py loaddata test_special_provider_case.json
-            python manage.py loaddata test_callbacks.json
-        fi
+    fi
+}
+
+load_end_to_end_test_data() {
+    if [ "$LOAD_END_TO_END_FIXTURES" == "True" ]; then
+
+        python manage.py loaddata test_special_provider_case.json
+        python manage.py loaddata test_callbacks.json
 
     fi
 }
@@ -40,5 +44,6 @@ cd /home/app/
 migrations
 admin_password
 load_test_data
+load_end_to_end_test_data
 
 exec "$@"

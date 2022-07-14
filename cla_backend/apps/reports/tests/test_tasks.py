0119955f44f53ccff7d3d01d56ad6157d4c85a1f
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2022-07-14 15:56:25 +0100

    Created unit test to check for when aws not available and running locally.

--- cla_backend/apps/reports/tests/test_tasks.py
+++ cla_backend/apps/reports/tests/test_tasks.py
@@ -3,7 +3,8 @@ import datetime
 import json
 from psycopg2 import InternalError
 from django.test import TestCase
-from reports.tasks import OBIEEExportTask
+from django.conf import settings
+from reports.tasks import ExportTask, OBIEEExportTask
 from core.tests.mommy_utils import make_user, make_recipe
 from legalaid.utils.diversity import save_diversity_data
 
@@ -40,3 +41,29 @@ class OBIEEExportTaskTestCase(TestCase):
         # Run task with the correct passphrase
         self.run_task("cla")
         self.assertEqual("", self.task.message)
+
+
+class MICB1ExportTaskTestCase(TestCase):
+    # test to check and see whether the code tries to contact aws if running locally.
+    def setUp(self):
+        # run(self, user_id, filename, form_class_name, post_data, *args, **kwargs):
+        self.task = ExportTask()
+        self.task._create_export = mock.MagicMock()
+        self.task.send_to_s3 = mock.MagicMock()
+        self.user = make_user()
+
+    def run_task(self):
+        date_from = "03/07/2022"
+        date_to = "03/07/2022"
+        filename = "xxx.csv"
+        post_data = {"action": "Export", "date_from": date_from, "date_to": date_to}
+        post_data = json.dumps(post_data)
+        form_class_name = "MICB1Extract"
+        self.task.run(user_id=self.user.id, form_class_name=form_class_name, post_data=post_data, filename=filename)
+
+    def test_no_aws(self):
+        # run task with debug set and locally
+        settings.AWS_REPORTS_STORAGE_BUCKET_NAME = False
+        self.run_task()
+        assert not self.task.send_to_s3.called
+

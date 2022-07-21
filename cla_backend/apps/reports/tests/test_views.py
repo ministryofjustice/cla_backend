a616b3c1d6e8ae5ddc5321bea7e9c80e66f972fa
psweeting <patrick.sweeting@digital.justice.gov.uk>
2022-07-21 09:54:50 +0100

    Tidy up and refactor

--- cla_backend/apps/reports/tests/test_views.py
+++ cla_backend/apps/reports/tests/test_views.py
@@ -2,7 +2,6 @@ from mock import patch, MagicMock, mock_open
 from django.test import TestCase
 from reports.views import download_file, get_s3_connection
 from django.conf import settings
-# from reports.views import get_s3_connection
 
 
 class DownloadFileTestCase(TestCase):
@@ -24,16 +23,14 @@ class DownloadFileTestCase(TestCase):
 
     @patch('reports.views.get_s3_connection', return_value=MagicMock())
     def test_download_with_aws(self, mock_s3):
-        with patch("__builtin__.open", mock_open(read_data="data")) as mock_file:
-            mock_request = MagicMock()
-            file_name = "scheduled"
-            settings.AWS_REPORTS_STORAGE_BUCKET_NAME = "AWS_TEST"
-            settings.DEBUG = True
-            # download_file(request, file_name="", *args, **kwargs):
-            download_file(mock_request, file_name)
-            assert open("path/to/open").read() == "data"
+        mock_request = MagicMock()
+        # if file_name contains string "schedule"
+        # delete from the database doesn't occur.
+        file_name = "scheduled"
+        settings.AWS_REPORTS_STORAGE_BUCKET_NAME = "AWS_TEST"
+        # download_file(request, file_name="", *args, **kwargs):
+        download_file(mock_request, file_name)
         assert mock_s3.called
-        mock_file.assert_called_with("path/to/open")
 
 
 

656b8edc81b5d8f6b420a5af73e21ee211e57a50
psweeting <patrick.sweeting@digital.justice.gov.uk>
2022-07-25 15:15:08 +0100

    Flake8 linting

--- cla_backend/apps/reports/tests/test_models.py
+++ cla_backend/apps/reports/tests/test_models.py
@@ -1,12 +1,11 @@
 from mock import MagicMock, patch
 from django.test import TestCase
-from reports.models import delete_export_file, get_s3_connection
+from reports.models import delete_export_file
 from django.conf import settings
 
 
 class DeleteExportFile(TestCase):
-
-    @patch('reports.models.get_s3_connection')
+    @patch("reports.models.get_s3_connection")
     def test_delete_export_file_no_aws(self, mock_s3):
         settings.AWS_REPORTS_STORAGE_BUCKET_NAME = ""
         sender = MagicMock()
@@ -15,7 +14,7 @@ class DeleteExportFile(TestCase):
         delete_export_file(sender, instance)
         assert not mock_s3.called
 
-    @patch('reports.models.get_s3_connection', return_value=MagicMock())
+    @patch("reports.models.get_s3_connection", return_value=MagicMock())
     def test_delete_export_file_with_aws(self, mock_s3):
         settings.AWS_REPORTS_STORAGE_BUCKET_NAME = "AWS_TEST"
         sender = MagicMock()
@@ -23,4 +22,3 @@ class DeleteExportFile(TestCase):
         # delete_export_file(sender, instance=None, **kwargs)
         delete_export_file(sender, instance)
         assert mock_s3.called
-
--- cla_backend/apps/reports/tests/test_views.py
+++ cla_backend/apps/reports/tests/test_views.py
@@ -1,12 +1,11 @@
 from mock import patch, MagicMock, mock_open
 from django.test import TestCase
-from reports.views import download_file, get_s3_connection
+from reports.views import download_file
 from django.conf import settings
 
 
 class DownloadFileTestCase(TestCase):
-
-    @patch('reports.views.get_s3_connection')
+    @patch("reports.views.get_s3_connection")
     def test_download_no_aws(self, mock_s3):
         # mock pythons open()
         with patch("__builtin__.open", mock_open(read_data="data")) as mock_file:
@@ -22,7 +21,7 @@ class DownloadFileTestCase(TestCase):
         assert not mock_s3.called
         mock_file.assert_called_with("path/to/open")
 
-    @patch('reports.views.get_s3_connection', return_value=MagicMock())
+    @patch("reports.views.get_s3_connection", return_value=MagicMock())
     def test_download_with_aws(self, mock_s3):
         mock_request = MagicMock()
         # if file_name contains string "schedule"
@@ -32,7 +31,3 @@ class DownloadFileTestCase(TestCase):
         # download_file(request, file_name="", *args, **kwargs):
         download_file(mock_request, file_name)
         assert mock_s3.called
-
-
-
-

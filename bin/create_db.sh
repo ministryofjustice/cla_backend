db6094a8d2aeb889be649558fee051820a56ec86
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-10-01 12:36:29 +0100

    LGS-2222: create_db.sh script updated to check value of CLIENT_DATA_COPY env var.

--- bin/create_db.sh
+++ bin/create_db.sh
@@ -49,7 +49,7 @@ admin_password() {
 }
 
 copy_old_client_data_to_new_table() {
-    if [ -n "$CLIENT_DATA_COPY" ]; then
+    iif [ "$CLIENT_DATA_COPY" == "True" ]; then
         python manage.py copy_client_data_to_new_table
     fi
 }

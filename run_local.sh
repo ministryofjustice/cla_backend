8eaa52102a579a84054e03f50afe88983b73bde2
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2023-01-12 11:28:44 +0000

    LGA-2305 edited run_local to allow passing in of environment so can run tests or dev environment.

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,13 @@
 export DOCKER_BUILDKIT=1
+export ENVIRONMENT=$1
 docker-compose down --remove-orphans
 docker-compose build cla_backend
-docker-compose run start_applications
-docker-compose exec cla_backend bin/create_db.sh
+if [ $ENVIRONMENT="test" ]; then
+  docker-compose run cla_backend
+else
+  docker-compose run start_applications
+  docker-compose exec cla_backend bin/create_db.sh
+fi
+
+
+

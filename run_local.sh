a2287444c11f991aaee73f40a6d5d8cb219ad630
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2023-01-13 11:41:07 +0000

    LGA-2305 add default environment variable value to run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 export DOCKER_BUILDKIT=1
-export ENVIRONMENT=$1
+export ENVIRONMENT=${1:-development}
+
+echo "running environment $ENVIRONMENT"
 docker-compose down --remove-orphans
 docker-compose build cla_backend
-if [ $ENVIRONMENT="test" ]; then
+if [ $ENVIRONMENT = "test" ]; then
   docker-compose run cla_backend
 else
   docker-compose run start_applications
   docker-compose exec cla_backend bin/create_db.sh
 fi
-
-
-

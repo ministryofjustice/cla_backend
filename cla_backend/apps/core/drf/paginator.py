50adddaa1685a555b61b76f27ce775b73983a413
Tawny-0wl <65071578+TawneeOwl@users.noreply.github.com>
2023-02-28 12:01:11 +0000

    LGA-1776-Remove-Export-Pagination

--- cla_backend/apps/core/drf/paginator.py
+++ cla_backend/apps/core/drf/paginator.py
@@ -12,7 +12,3 @@ class CaseNotesHistoryResultsSetPagination(PageNumberPagination):
     page_size_query_param = "page_size"
     max_page_size = 100
 
-
-class ExportResultsSetPagination(PageNumberPagination):
-    page_size = 1000
-    page_size_query_param = "page_size"

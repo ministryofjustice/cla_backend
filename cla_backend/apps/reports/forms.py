86579d0f3fefb140082b9b4defde29478489d876
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2023-05-11 09:31:26 +0100

    LGA-2443 ExportTask creates multiple csv files

--- cla_backend/apps/reports/forms.py
+++ cla_backend/apps/reports/forms.py
@@ -597,13 +597,16 @@ class MIExtractComplaintViewAuditLog(SQLFileDateRangeReport):
 
 class ReasonsForContactingReport(DateRangeReportForm):
     # initialise the dataset here and pass in the referrer...
-    top_referrer = None
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.top_referrer = None
+        super(ReportForm, self).__init__(*args, **kwargs)
 
-    def get_data_set(self, referrer=top_referrer):
+    def get_data_set(self):
         from_date, to_date = self.date_range
-        return ReasonForContacting.get_category_stats(start_date=from_date, end_date=to_date, referrer=referrer)[
-            "categories"
-        ]
+        return ReasonForContacting.get_category_stats(
+            start_date=from_date, end_date=to_date, top_referrer=self.top_referrer
+        )["categories"]
 
     def get_rows(self):
         for reason in self.get_data_set():

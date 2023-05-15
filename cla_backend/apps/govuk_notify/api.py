b66d48afa2a8db6978fac2a6703501f80ba50f78
said-moj <45761276+said-moj@users.noreply.github.com>
2023-05-15 09:43:38 +0100

    Log errors return gov notify

--- cla_backend/apps/govuk_notify/api.py
+++ cla_backend/apps/govuk_notify/api.py
@@ -1,6 +1,11 @@
+import logging
 from notifications_python_client.notifications import NotificationsAPIClient
+from notifications_python_client.errors import HTTPError
 
 from django.conf import settings
+
+
+logger = logging.getLogger(__name__)
 
 
 class GovUkNotify(object):
@@ -12,10 +17,15 @@ class GovUkNotify(object):
             raise ValueError("Missing API Key for GOVUK Notify")
 
     def send_email(self, email_address, template_id, personalisation):
-        if self.notifications_client:
+        if not self.notifications_client:
+            return
+        try:
             response = self.notifications_client.send_email_notification(
                 email_address=email_address,  # required string
                 template_id=template_id,  # required UUID string
                 personalisation=personalisation,
             )
-            return response
+        except HTTPError as error:
+            logger.error("GovUkNotify error: {msg}".format(msg=str(error)))
+            raise error
+        return response

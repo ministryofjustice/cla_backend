9e4f8e9ec9881735e3d8da623d7e7cd88f3e7be1
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2023-05-25 17:55:10 +0100

    LGA-2443 refactor get_category_stats

--- cla_backend/apps/checker/models.py
+++ cla_backend/apps/checker/models.py
@@ -18,51 +18,72 @@ class ReasonForContacting(TimeStampedModel):
         ordering = ("-created",)
 
     @classmethod
-    def get_category_stats(cls, start_date=None, end_date=None, referrer=None):
-        filters_count = None
-        filters_data = None
-        if start_date and end_date:
-            filters_count = models.Q(created__gte=start_date) & models.Q(created__lte=end_date)
-            filters_data = models.Q(
-                reason_for_contacting__created__lte=end_date, reason_for_contacting__created__gte=start_date
-            )
-            if referrer:
-                filters_count &= models.Q(referrer__endswith=referrer)
-                filters_data &= models.Q(reason_for_contacting__referrer__endswith=referrer)
-        if filters_count:
-            total_count = cls.objects.filter(filters_count).count()
-        else:
-            total_count = cls.objects.count()
+    def get_category_stats(cls):
+        # this is the method used by the checker admin page
+        # refactored so can be used by the new reports method
+        total_count = cls.objects.count()
+        data = cls.get_all_category_stats()
+        return cls.get_category_split(data, total_count)
 
-        if filters_data:
-            data = (
-                ReasonForContactingCategory.objects.filter(filters_data)
-                .values_list("category")
-                .annotate(count=models.Count("category"))
-                .order_by()
-            )
-        else:
-            data = (
-                ReasonForContactingCategory.objects.values_list("category")
-                .annotate(count=models.Count("category"))
-                .order_by()
-            )
-
-        data = dict(data)
+    @classmethod
+    def get_category_split(cls, category_data, reasons_for_contacting_count):
         # known categories, preserving order
         categories = [
-            {"key": choice[0], "description": choice[1], "percentage": data[choice[0]] if choice[0] in data else 0}
+            {
+                "key": choice[0],
+                "description": choice[1],
+                "percentage": category_data[choice[0]] if choice[0] in category_data else 0,
+            }
             for choice in REASONS_FOR_CONTACTING.CHOICES
         ]
         # unknown categories (perhaps have been removed)
-        for category, count in data.iteritems():
+        for category, count in category_data.iteritems():
             if category not in REASONS_FOR_CONTACTING.CHOICES_DICT:
                 categories.append({"key": category, "description": category, "percentage": count})
         # calculate percentage of all responses that chose each option
-        percentage_total = 100.0 / total_count if total_count else 0.0
+        percentage_total = 100.0 / reasons_for_contacting_count if reasons_for_contacting_count else 0.0
         for category in categories:
             category["percentage"] *= percentage_total
-        return dict(categories=categories, total_count=total_count)
+        return dict(categories=categories, total_count=reasons_for_contacting_count)
+
+    @classmethod
+    def get_all_category_stats(cls):
+        data = (
+            ReasonForContactingCategory.objects.values_list("category")
+            .annotate(count=models.Count("category"))
+            .order_by()
+        )
+
+        data = dict(data)
+        return data
+
+    @classmethod
+    def get_restricted_category_stats(cls, start_date, end_date, referrer=None):
+        # there must be a start date and an end date
+        if not start_date and end_date:
+            raise ValueError("Must provide a start date and an end date")
+        filters_count = models.Q(created__gte=start_date) & models.Q(created__lte=end_date)
+        filters_data = models.Q(
+            reason_for_contacting__created__lte=end_date, reason_for_contacting__created__gte=start_date
+        )
+        if referrer:
+            filters_count &= models.Q(referrer__endswith=referrer)
+            filters_data &= models.Q(reason_for_contacting__referrer__endswith=referrer)
+        total_count = cls.objects.filter(filters_count).count()
+        data = (
+            ReasonForContactingCategory.objects.filter(filters_data)
+            .values_list("category")
+            .annotate(count=models.Count("category"))
+            .order_by()
+        )
+        data = dict(data)
+        return total_count, data
+
+    @classmethod
+    def get_report_category_stats(cls, start_date=None, end_date=None, referrer=None):
+        # this returns limited results that can be downloaded as a report
+        total_count, data = cls.get_restricted_category_stats(start_date, end_date, referrer)
+        return cls.get_category_split(data, total_count)
 
     @classmethod
     def get_top_referrers(cls, count=8):

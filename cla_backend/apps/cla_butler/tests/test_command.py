0290220b38b58b40439db1f91c1bf24b8b014d1f
said-moj <45761276+said-moj@users.noreply.github.com>
2023-06-06 10:21:52 +0100

    Refactor test for the diversity_data_reencrypt management command to reuse the same mock schedule_tasks

--- cla_backend/apps/cla_butler/tests/test_command.py
+++ cla_backend/apps/cla_butler/tests/test_command.py
@@ -24,6 +24,20 @@ class DiversityCheckCommandTestCase(CreateSampleDiversityData, TestCase):
 
 
 class DiversityReencryptCommandTestCase(CreateSampleDiversityData, TestCase):
+    def setUp(self):
+        super(DiversityReencryptCommandTestCase, self).setUp()
+        self.scheduled_tasks = []
+        self.expected_tasks_count = 0
+
+    def mock_schedule_tasks(self, tasks, passphrase_old, **kwargs):
+        self.scheduled_tasks = tasks
+        # (count divided by chunk size) plus (count mod chunk size) - Required for when you have a count that is not
+        # divisible by the chunk size
+        tasks_count = (len(self.pd_records_ids) / ClaButlerDiversityReencryptCommand.chunk_size) + (
+            len(self.pd_records_ids) % ClaButlerDiversityReencryptCommand.chunk_size
+        )
+        self.assertEqual(len(tasks), tasks_count)
+
     @mock.patch(
         "cla_butler.management.commands.diversity_data_reencrypt.Command.get_old_key_passphrase", return_value="cla"
     )
@@ -32,11 +46,7 @@ class DiversityReencryptCommandTestCase(CreateSampleDiversityData, TestCase):
     def test_create_tasks_all(self, mock_schedule_tasks, get_old_key_passphrase):
         ClaButlerDiversityReencryptCommand.chunk_size = 2
 
-        def schedule_tasks(tasks, passphrase_old, **kwargs):
-            tasks_count = len(self.pd_records_ids) / ClaButlerDiversityReencryptCommand.chunk_size
-            self.assertEqual(len(tasks), tasks_count)
-
-        mock_schedule_tasks.side_effect = schedule_tasks
+        mock_schedule_tasks.side_effect = self.mock_schedule_tasks
         management.call_command("diversity_data_reencrypt")
         self.assertTrue(mock_schedule_tasks.called)
 
@@ -47,24 +57,15 @@ class DiversityReencryptCommandTestCase(CreateSampleDiversityData, TestCase):
     @mock.patch.dict("os.environ", {"PREVIOUS_DIVERSITY_PRIVATE_KEY": "I am not empty"})
     def test_create_tasks_some(self, mock_schedule_tasks, get_old_key_passphrase):
         ClaButlerDiversityReencryptCommand.chunk_size = 2
-        # Mark the first record as processed so that we can test that diversity_data_reencrypt process does not attempt to
-        # reencrypt this record
+        # Mark the first record as processed so that we can test that diversity_data_reencrypt process
+        # does not attempt to reencrypt this record
         DiversityDataCheck.objects.create(
             personal_details_id=self.pd_records_ids[0], action=ACTION.REENCRYPT, status=STATUS.OK
         )
 
-        def schedule_tasks(tasks, passphrase_old, **kwargs):
-            # (count divided by chunk size) plus (count mod chunk size) - Required for when you have a count that is not
-            # divisible by the chunk size
-            tasks_count = (len(self.pd_records_ids) / ClaButlerDiversityReencryptCommand.chunk_size) + (
-                len(self.pd_records_ids) % ClaButlerDiversityReencryptCommand.chunk_size
-            )
-            self.assertEqual(len(tasks), tasks_count)
-
-            # Make sure that the first record is not included
-            for task in tasks:
-                self.assertNotIn(self.pd_records_ids[0], task)
-
-        mock_schedule_tasks.side_effect = schedule_tasks
+        mock_schedule_tasks.side_effect = self.mock_schedule_tasks
         management.call_command("diversity_data_reencrypt")
         self.assertTrue(mock_schedule_tasks.called)
+        # Make sure that the first record is not included
+        for task in self.scheduled_tasks:
+            self.assertNotIn(self.pd_records_ids[0], task)

3955c61bc7c81f23774d244f953ff13a5b46c13e
pheonnexx <pheonn@hotmail.co.uk>
2023-08-25 13:12:09 +0100

    fix linting issue

--- cla_backend/apps/reports/tests/test_mi_sla_report.py
+++ cla_backend/apps/reports/tests/test_mi_sla_report.py
@@ -344,7 +344,7 @@ class MiSlaTestCaseWeb(MiSlaTestCaseBase, TestCase):
 
     def get_sla1_datetime(self):
         return self.get_requires_action_at() + datetime.timedelta(minutes=30)
-    
+
     def get_sla2_datetime(self):
         return _make_datetime(year=2020, month=9, day=15, hour=12, minute=30)
 

f5bb33e35efa83be60f3bfa3e81695b48197b21e
said-moj <45761276+said-moj@users.noreply.github.com>
2023-09-04 10:33:26 +0100

    Log notifications going to frontend

--- cla_backend/apps/notifications/tasks.py
+++ cla_backend/apps/notifications/tasks.py
@@ -2,6 +2,7 @@
 import json
 import requests
 from celery import shared_task
+from celery.utils.log import get_task_logger
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
 
@@ -9,15 +10,17 @@ from .models import Notification
 from .serializers import NotificationSerializer
 
 
+logger = get_task_logger(__name__)
+
+
 @shared_task(default_retry_delay=1, max_retries=12)
 def send_notifications():
     data = {"notifications": NotificationSerializer(Notification.objects.live(), many=True).data}
-
-    response = requests.post(
-        "%s:%s/admin/notifications/" % (settings.EXPRESS_SERVER_HOST, settings.EXPRESS_SERVER_PORT),
-        data=json.dumps(data, cls=DjangoJSONEncoder),
-        headers={"content-type": "application/json"},
-    )
+    url = "%s:%s/admin/notifications/" % (settings.EXPRESS_SERVER_HOST, settings.EXPRESS_SERVER_PORT)
+    data = json.dumps(data, cls=DjangoJSONEncoder)
+    logger.info("Sending notification - url %s" % url)
+    logger.info("Sending notification - data %s" % data)
+    response = requests.post(url, data=data, headers={"content-type": "application/json"})
 
     try:
         success = response.json()["success"]

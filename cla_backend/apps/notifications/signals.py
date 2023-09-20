411f338a4a192551c476ddb7800de03d6c1fdcb3
said-moj <45761276+said-moj@users.noreply.github.com>
2023-09-20 08:23:12 +0100

    LGA-2673 - Move notifications to to a periodic task (#993)
    * Move notifications to celery beat

--- cla_backend/apps/notifications/signals.py
+++ cla_backend/apps/notifications/signals.py
@@ -1,24 +1,20 @@
 # coding=utf-8
 from django.utils import timezone
 
-TASK_ID_PREFIX = "cla_backend.notifications.task.notifications"
 
-
-def get_update_client_times(instance):
-    times = []
+def schedule_notifications_to_users(sender, instance, **kwargs):
+    from notifications.models import Schedule
     now = timezone.now()
-    task_id = "%s-%s" % (TASK_ID_PREFIX, instance.pk)
 
-    times.append({})
+    defaults = {}
+    # Reset schedule when updating notification to start in the future
     if instance.start_time > now:
-        times.append({"eta": instance.start_time, "task_id": "%s-start" % task_id})
-    times.append({"eta": instance.end_time, "task_id": "%s-end" % task_id})
+        defaults["retried"] = 0
+        defaults["completed"] = False
+        defaults["status"] = "scheduled"
+    Schedule.objects.update_or_create(notification=instance, defaults=defaults)
 
-    return times
 
-
-def send_notifications_to_users(sender, instance, **kwargs):
-    from .tasks import send_notifications
-
-    for kwargs in get_update_client_times(instance):
-        send_notifications.apply_async(**kwargs)
+def un_schedule_notifications_to_users(sender, instance, **kwargs):
+    from notifications.models import Schedule
+    Schedule.objects.filter(notification=instance).delete()

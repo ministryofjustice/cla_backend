1aaee332b08bc74d2e055cc397d98c7ce96f51c5
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-26 14:28:39 +0100

    LGA 2770: Enable communication between CLA_Backend and Email Orchestrator API
    * Added additional Notify client that will be enabled with a Feature Flag variable.

---
+++ cla_backend/apps/govuk_notify/tests/test_notify_feature_flag.py
@@ -0,0 +1,22 @@
+from cla_backend.apps.govuk_notify.api import GovUkNotify, NotifyEmailOrchestrator
+from django.test import TestCase, override_settings
+
+
+class TestNotifyFeatureFlagEnabled(TestCase):
+
+    @override_settings(USE_EMAIL_ORCHESTRATOR_FLAG=True)
+    @override_settings(EMAIL_ORCHESTRATOR_URL="https://a-url.com")
+    def test_feature_flag_enabled(self):
+        client = GovUkNotify()
+        assert isinstance(client, NotifyEmailOrchestrator)
+        assert not isinstance(client, GovUkNotify)
+
+
+class TestNotifyFeatureFlagDisabled(TestCase):
+
+    @override_settings(USE_EMAIL_ORCHESTRATOR_FLAG=False)
+    @override_settings(EMAIL_ORCHESTRATOR_URL="https://a-url.com")
+    def test_feature_flag_disabled(self):
+        client = GovUkNotify()
+        assert isinstance(client, GovUkNotify)
+        assert not isinstance(client, NotifyEmailOrchestrator)

2e0d6eca2df7896d9ac7e841179e1ad9eb582c65
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-26 15:49:30 +0100

    Renamed EMAIL_ORCHESTRATOR_FLAG to USE_EMAIL_ORCHESTRATOR_FLAG

--- cla_backend/settings/base.py
+++ cla_backend/settings/base.py
@@ -464,7 +464,7 @@ SESSION_SECURITY_PASSIVE_URLS = []
 
 EMAIL_ORCHESTRATOR_URL = os.environ.get("EMAIL_ORCHESTRATOR_URL", "Not set")
 
-USE_EMAIL_ORCHESTRATOR_FLAG = os.environ.get("EMAIL_ORCHESTRATOR_FLAG", "False") == "True"
+USE_EMAIL_ORCHESTRATOR_FLAG = os.environ.get("USE_EMAIL_ORCHESTRATOR_FLAG", "False") == "True"
 
 # .local.py overrides all the common settings.
 try:

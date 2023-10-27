7e7da00da3793b77c37370061868cb4e4f790a02
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-10-27 15:48:41 +0100

    Improved unit testing

--- cla_backend/apps/govuk_notify/tests/test_notify_feature_flag.py
+++ cla_backend/apps/govuk_notify/tests/test_notify_feature_flag.py
@@ -1,5 +1,6 @@
 from cla_backend.apps.govuk_notify.api import GovUkNotify, NotifyEmailOrchestrator
 from django.test import TestCase, override_settings
+from django.conf import settings
 
 
 class TestNotifyFeatureFlagEnabled(TestCase):
@@ -7,6 +8,7 @@ class TestNotifyFeatureFlagEnabled(TestCase):
     @override_settings(USE_EMAIL_ORCHESTRATOR_FLAG=True)
     @override_settings(EMAIL_ORCHESTRATOR_URL="https://a-url.com")
     def test_feature_flag_enabled(self):
+        assert settings.USE_EMAIL_ORCHESTRATOR_FLAG == True
         client = GovUkNotify()
         assert isinstance(client, NotifyEmailOrchestrator)
         assert not isinstance(client, GovUkNotify)
@@ -17,6 +19,7 @@ class TestNotifyFeatureFlagDisabled(TestCase):
     @override_settings(USE_EMAIL_ORCHESTRATOR_FLAG=False)
     @override_settings(EMAIL_ORCHESTRATOR_URL="https://a-url.com")
     def test_feature_flag_disabled(self):
+        assert settings.USE_EMAIL_ORCHESTRATOR_FLAG == False 
         client = GovUkNotify()
         assert isinstance(client, GovUkNotify)
         assert not isinstance(client, NotifyEmailOrchestrator)

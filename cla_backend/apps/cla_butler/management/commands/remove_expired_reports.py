241d0410b55062e659ab47d7b566ed849fefac8e
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-11-09 14:19:19 +0000

    WIP Add logic to get all report data

--- cla_backend/apps/cla_butler/management/commands/remove_expired_reports.py
+++ cla_backend/apps/cla_butler/management/commands/remove_expired_reports.py
@@ -2,9 +2,7 @@
 import logging
 from django.core.management.base import BaseCommand
 
-
-from cla_eventlog.constants import LOG_LEVELS, LOG_TYPES
-from cla_eventlog.models import Log
+from reports.models import Export
 
 
 MAX_REPORT_AGE_DAYS = 730
@@ -18,7 +16,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.remove_expired_reports()
 
+
     @staticmethod
     def remove_expired_reports():
+
+        # Loop through local database to get m1 report name / time.
+        for report in Export.objects.all():
+            print(report.path)
+        # Take name / time and search s3 bucket to see if it exists.
+        # If yes:
+            # Check age:
+                # If old than two years old: Delete
+        # If no:
+            # Clean up
         
-        logger.info("")

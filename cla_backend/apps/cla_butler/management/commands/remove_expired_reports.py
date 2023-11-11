3b178f5007bd08f3dde64ab8d4ea7c67064dc224
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-11-11 01:06:49 +0000

    Added CronJob comments

--- cla_backend/apps/cla_butler/management/commands/remove_expired_reports.py
+++ cla_backend/apps/cla_butler/management/commands/remove_expired_reports.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Removes all reports from the db and S3 Buckets that are 2 years old or greater."
+    help = "Removes all extract reports that were created over 2 years ago."
 
     def handle(self, *args, **options):
         self.remove_expired_reports()

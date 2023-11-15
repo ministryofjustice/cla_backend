03fa761dbbf014bf0e6c62dcda1737da42978257
psweeting <patrick.sweeting@digital.justice.gov.uk>
2023-11-15 11:35:20 +0000

    LGA-2787 adding in test_mode and debug conditions for testing purposes

--- cla_backend/apps/govuk_notify/api.py
+++ cla_backend/apps/govuk_notify/api.py
@@ -22,8 +22,21 @@ class NotifyEmailOrchestrator(object):
         return base_url + self.endpoint
 
     def send_email(self, email_address, template_id, personalisation=None):
+        """
+        :param email_address: (str) - Email address of the receiver
+        :param template_id: (str) - The GOV.UK Notify template id
+        :param personalisation: (optional, dictionary) - The personalisation dictionary
+        :return: send_api_request (bool) - Was the request sent, will return True if the request was made successfully
+                                          will return False if the EMAIL_ORCHESTRATOR_URL is not set or
+                                          the application is in TEST_MODE or DEBUG mode
+        """
+        if settings.TEST_MODE or settings.DEBUG:
+            logger.info("Application is in TESTING mode, will not send the request")
+            return False
+
         if not self.base_url:
-            raise EnvironmentError("EMAIL_ORCHESTRATOR_URL is not set, unable to send email")
+            logger.error("EMAIL_ORCHESTRATOR_URL is not set, unable to send email")
+            return False
         data = {"email_address": email_address, "template_id": template_id}
         if personalisation:
             data["personalisation"] = personalisation
@@ -32,3 +45,4 @@ class NotifyEmailOrchestrator(object):
 
         if response.status_code != 201:
             raise HTTPError(response)
+        return True

1fa41337f25ee45e06c4fc0fed66e2ce50ada59d
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2023-11-15 15:03:56 +0000

    Remove comment

--- cla_backend/apps/reports/sql/ReasonsForContactingReportDisaggregated.sql
+++ cla_backend/apps/reports/sql/ReasonsForContactingReportDisaggregated.sql
@@ -8,7 +8,7 @@ left join legalaid_eligibilitycheck le on lc.eligibility_check_id = le.id
 left join legalaid_adaptationdetails la on lc.adaptation_details_id = la.id
 full join checker_reasonforcontacting cr on lc.id = cr.case_id
 full join checker_reasonforcontactingcategory cr2 on cr2.reason_for_contacting_id = cr.id
-where cr2.reason_for_contacting_id is not null --and cr.created >= %(from_date)s and cr.created < %(to_date)s
+where cr2.reason_for_contacting_id is not null and cr.created >= %(from_date)s and cr.created < %(to_date)s
 group by cr.id, lc.id, cr.created, le.notes, la.bsl_webcam,
 la.minicom, la.text_relay, la.skype_webcam, la."language", la.notes
 ORDER BY cr.created desc

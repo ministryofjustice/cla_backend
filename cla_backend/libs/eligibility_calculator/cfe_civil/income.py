ab0752779e8ea05be415b89dd06c493dbf0ae382
Ripan <er.ripankumar@gmail.com>
2023-12-11 14:32:58 +0000

    convert income to regular transactions

--- cla_backend/libs/eligibility_calculator/cfe_civil/income.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/income.py
@@ -1,46 +1,76 @@
+from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_to_pounds
+
 
 def translate_income(income_data):
-    cash_transactions = {"income": []}   # pension, maintenance_received, benefits
-    employment_income = [{"payments": []}]
-    irregular_incomes = {"payments": []}
-    other_incomes = [{"payments": []}]   # pension, maintenance_received, benefits
-    regular_transactions = []   # pension, maintenance_received, benefits
-    state_benefits = [{"payments": []}]   # benefits
-    employment_details = [{"income": {}}]   # earnings
-    self_employment_details = [{"income": {}}]   # self_employment_drawings
+    regular_transactions = []  # pension, maintenance_received, benefits
 
-    if not(income_data.self_employed) and (income_data.earnings > 0):
+    if not income_data.self_employed and (income_data.earnings > 0):
         print("employment_income, employment_details")
 
     if income_data.self_employed and (income_data.self_employment_drawings > 0):
         print("self_employment_details")
 
-    if income_data.benefits > 0:
-        print("cash_transactions, other_incomes, regular_transactions, state_benefits")
-
     if income_data.tax_credits > 0:
         print("?")
-
-    if income_data.child_benefits > 0:
-        print("?")
+        regular_transactions.append(
+            {
+                "category": "benefits",
+                "operation": "credit",
+                "frequency": "monthly",
+                "amount": pence_to_pounds(income_data.tax_credits),
+            })
 
     if income_data.maintenance_received > 0:
         print("cash_transactions, other_incomes, regular_transactions")
+        regular_transactions.append(
+            {
+                "category": "maintenance_in",
+                "operation": "credit",
+                "frequency": "monthly",
+                "amount": pence_to_pounds(income_data.maintenance_received),
+            })
 
     if income_data.pension > 0:
         print("cash_transactions, other_incomes, regular_transactions")
+        regular_transactions.append(
+            {
+                "category": "pension_contribution",
+                "operation": "credit",
+                "frequency": "monthly",
+                "amount": pence_to_pounds(income_data.pension),
+            })
+
+    if income_data.benefits > 0:
+        print("cash_transactions, other_incomes, regular_transactions, state_benefits")
+        regular_transactions.append(
+            {
+                "category": "benefits",
+                "operation": "credit",
+                "frequency": "monthly",
+                "amount": pence_to_pounds(income_data.benefits),
+            })
+
+    if income_data.child_benefits > 0:
+        print("?")
+        regular_transactions.append(
+            {
+                "category": "child_care",
+                "operation": "credit",
+                "frequency": "monthly",
+                "amount": pence_to_pounds(income_data.child_benefits),
+            })
 
     if income_data.other_income > 0:
         print("?")
+        regular_transactions.append(
+            {
+                "category": "friends_or_family",
+                "operation": "credit",
+                "frequency": "monthly",
+                "amount": pence_to_pounds(income_data.other_income),
+            })
 
     value = dict(
-        cash_transactions=cash_transactions,
-        employment_income=employment_income,
-        irregular_incomes=irregular_incomes,
-        other_incomes=other_incomes,
-        regular_transactions=regular_transactions,
-        state_benefits=state_benefits,
-        employment_details=employment_details,
-        self_employment_details=self_employment_details
+        regular_transactions=regular_transactions
     )
     return value
--- cla_backend/libs/eligibility_calculator/tests/cfe_civil/test_income.py
+++ cla_backend/libs/eligibility_calculator/tests/cfe_civil/test_income.py
@@ -7,40 +7,22 @@ from cla_backend.libs.eligibility_calculator.models import Income
 class TestTranslateIncome(TestCase):
     def test_assets(self):
         income = Income(earnings=0, self_employment_drawings=0, benefits=80000, tax_credits=0, child_benefits=0,
-                        maintenance_received=0, pension=0, other_income=0, self_employed=True)
+                        maintenance_received=10000, pension=0, other_income=0, self_employed=True)
         output = translate_income(income)
         expected = {
-            "cash_transactions": {
-                "income": []
-            },
-            "employment_income": [
+            "regular_transactions": [
                 {
-                    "payments": []
+                    "category": "maintenance_in",
+                    "operation": "credit",
+                    "frequency": "monthly",
+                    "amount": 100,
+                },
+                {
+                    "category": "benefits",
+                    "operation": "credit",
+                    "frequency": "monthly",
+                    "amount": 800,
                 }
             ],
-            "irregular_incomes": {
-                "payments": []
-            },
-            "other_incomes": [
-                {
-                    "payments": []
-                }
-            ],
-            "regular_transactions": [],
-            "state_benefits": [
-                {
-                    "payments": []
-                }
-            ],
-            "employment_details": [
-                {
-                    "income": {}
-                }
-            ],
-            "self_employment_details": [
-                {
-                    "income": {}
-                }
-            ]
         }
         self.assertEqual(expected, output)

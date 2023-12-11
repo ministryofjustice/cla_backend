3d9b5b110bb8cf0267bbfd15b17551bc4b8a9ba5
Stephen Dicks <github@starswan.com>
2023-12-11 15:42:29 +0000

    fixed calculator tests

--- cla_backend/libs/eligibility_calculator/cfe_civil/age.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/age.py
@@ -2,9 +2,9 @@ import datetime
 
 
 def translate_age(today, facts):
-    if facts.is_you_or_your_partner_over_60:
+    if hasattr(facts, "is_you_or_your_partner_over_60") and facts.is_you_or_your_partner_over_60:
         age = 70
-    elif facts.is_you_under_18:
+    elif hasattr(facts, "is_you_under_18") and facts.is_you_under_18:
         age = 17
     else:
         age = 50

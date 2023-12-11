7d662f7f8d4bcb26637baef2554384e80495ade8
Stephen Dicks <github@starswan.com>
2023-12-11 16:35:17 +0000

    Remove duplication in translate_savings code

--- cla_backend/libs/eligibility_calculator/cfe_civil/savings.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/savings.py
@@ -1,32 +1,31 @@
 from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_to_pounds
 
 
+def _savings_value(value, description):
+    if value > 0:
+        return {
+            "value": pence_to_pounds(value),
+            "description": description,
+            "subject_matter_of_dispute": False
+        }
+    else:
+        return None
+
 def translate_savings(savings_data):
     liquid_capital = []
     non_liquid_capital = []
-    if savings_data.bank_balance > 0:
-        liquid_capital.append(
-            {
-                "value": pence_to_pounds(savings_data.bank_balance),
-                "description": "Savings",
-                "subject_matter_of_dispute": False
-            })
-    if savings_data.investment_balance > 0:
-        liquid_capital.append(
-            {
-                "value": pence_to_pounds(savings_data.investment_balance),
-                "description": "Investment",
-                "subject_matter_of_dispute": False
-            })
-    if savings_data.asset_balance > 0:
-        non_liquid_capital.append(
-            {
-                "value": pence_to_pounds(savings_data.asset_balance),
-                "description": "Asset",
-                "subject_matter_of_dispute": False
-            })
-    value = dict(capitals={
+
+    bank_value = _savings_value(savings_data.bank_balance, "Savings")
+    invest_value = _savings_value(savings_data.investment_balance, "Investment")
+    asset_value = _savings_value(savings_data.asset_balance, "Asset")
+
+    if bank_value:
+        liquid_capital.append(bank_value)
+    if invest_value:
+        liquid_capital.append(invest_value)
+    if asset_value:
+        non_liquid_capital.append(asset_value)
+    return dict(capitals={
         "bank_accounts": liquid_capital,
         "non_liquid_capital": non_liquid_capital
     })
-    return value

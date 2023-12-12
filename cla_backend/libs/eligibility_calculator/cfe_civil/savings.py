9b9ec4947c00c45e52c63dc96478ffe70712d574
Stephen Dicks <github@starswan.com>
2023-12-12 14:38:17 +0000

    tidy up after review of LEP-345

--- cla_backend/libs/eligibility_calculator/cfe_civil/savings.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/savings.py
@@ -1,4 +1,4 @@
-from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_to_pounds
+from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_to_pounds, none_filter
 
 
 def _savings_value(value, description):
@@ -8,25 +8,18 @@ def _savings_value(value, description):
             "description": description,
             "subject_matter_of_dispute": False
         }
-    else:
-        return None
 
 
 def translate_savings(savings_data):
-    liquid_capital = []
-    non_liquid_capital = []
+    liquid_capital = [
+        _savings_value(savings_data.bank_balance, "Savings"),
+        _savings_value(savings_data.investment_balance, "Investment")
+    ]
+    non_liquid_capital = [
+        _savings_value(savings_data.asset_balance, "Asset")
+    ]
 
-    bank_value = _savings_value(savings_data.bank_balance, "Savings")
-    invest_value = _savings_value(savings_data.investment_balance, "Investment")
-    asset_value = _savings_value(savings_data.asset_balance, "Asset")
-
-    if bank_value:
-        liquid_capital.append(bank_value)
-    if invest_value:
-        liquid_capital.append(invest_value)
-    if asset_value:
-        non_liquid_capital.append(asset_value)
     return dict(capitals={
-        "bank_accounts": liquid_capital,
-        "non_liquid_capital": non_liquid_capital
+        "bank_accounts": none_filter(liquid_capital),
+        "non_liquid_capital": none_filter(non_liquid_capital)
     })

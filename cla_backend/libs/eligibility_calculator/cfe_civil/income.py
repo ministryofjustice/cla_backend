bbe182d523b2477eec75538c1bc2be9fe4e83f2b
Ripan <er.ripankumar@gmail.com>
2023-12-12 10:46:10 +0000

    Fix e2e tests

--- cla_backend/libs/eligibility_calculator/cfe_civil/income.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/income.py
@@ -4,22 +4,22 @@ from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_
 def translate_income(income_data):
     regular_transactions = []  # pension, maintenance_received, benefits
 
-    if income_data.tax_credits > 0:
+    if hasattr(income_data, "tax_credits") and income_data.tax_credits > 0:
         regular_transactions.append(regular_transaction("benefits", income_data.tax_credits))
 
-    if income_data.maintenance_received > 0:
+    if hasattr(income_data, "maintenance_received") and income_data.maintenance_received > 0:
         regular_transactions.append(regular_transaction("maintenance_in", income_data.maintenance_received))
 
-    if income_data.pension > 0:
+    if hasattr(income_data, "pension") and income_data.pension > 0:
         regular_transactions.append(regular_transaction("pension", income_data.pension))
 
-    if income_data.benefits > 0:
+    if hasattr(income_data, "benefits") and income_data.benefits > 0:
         regular_transactions.append(regular_transaction("benefits", income_data.benefits))
 
-    if income_data.child_benefits > 0:
+    if hasattr(income_data, "child_benefits") and income_data.child_benefits > 0:
         regular_transactions.append(regular_transaction("benefits", income_data.child_benefits))
 
-    if income_data.other_income > 0:
+    if hasattr(income_data, "other_income") and income_data.other_income > 0:
         regular_transactions.append(regular_transaction("friends_or_family", income_data.other_income))
 
     value = dict(

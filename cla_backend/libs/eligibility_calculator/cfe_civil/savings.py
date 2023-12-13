7fe284b38cdd40117b3875e5a44ae7157ab63181
Stephen Dicks <github@starswan.com>
2023-12-13 16:29:57 +0000

    Remove duplication in translate_savings code (#1041)

--- cla_backend/libs/eligibility_calculator/cfe_civil/savings.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/savings.py
@@ -1,32 +1,25 @@
-from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_to_pounds
+from cla_backend.libs.eligibility_calculator.cfe_civil.conversions import pence_to_pounds, none_filter
+
+
+def _savings_value(value, description):
+    if value > 0:
+        return {
+            "value": pence_to_pounds(value),
+            "description": description,
+            "subject_matter_of_dispute": False
+        }
 
 
 def translate_savings(savings_data):
-    liquid_capital = []
-    non_liquid_capital = []
-    if savings_data.bank_balance > 0:
-        liquid_capital.append(
-            {
-                "value": pence_to_pounds(savings_data.bank_balance),
-                "description": "Savings",
-                "subject_matter_of_dispute": False
-            })
-    if savings_data.investment_balance > 0:
-        liquid_capital.append(
-            {
-                "value": pence_to_pounds(savings_data.investment_balance),
-                "description": "Investment",
-                "subject_matter_of_dispute": False
-            })
-    if savings_data.asset_balance > 0:
-        non_liquid_capital.append(
-            {
-                "value": pence_to_pounds(savings_data.asset_balance),
-                "description": "Asset",
-                "subject_matter_of_dispute": False
-            })
-    value = dict(capitals={
-        "bank_accounts": liquid_capital,
-        "non_liquid_capital": non_liquid_capital
+    liquid_capital = [
+        _savings_value(savings_data.bank_balance, "Savings"),
+        _savings_value(savings_data.investment_balance, "Investment")
+    ]
+    non_liquid_capital = [
+        _savings_value(savings_data.asset_balance, "Asset")
+    ]
+
+    return dict(capitals={
+        "bank_accounts": none_filter(liquid_capital),
+        "non_liquid_capital": none_filter(non_liquid_capital)
     })
-    return value

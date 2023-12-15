9893d12dcad900888f24e51348425fee0ab09325
Ripan <er.ripankumar@gmail.com>
2023-12-15 10:26:38 +0000

    Fix behave test
    https://app.circleci.com/pipelines/github/ministryofjustice/cla_backend/3524/workflows/1839b246-53c8-4303-912d-0a0e811fd812/jobs/19959

--- cla_backend/libs/eligibility_calculator/cfe_civil/income.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/income.py
@@ -15,14 +15,15 @@ def translate_income(income_data):
     regular_transactions = []
 
     for income_category in INCOME_CATEGORY_TO_REGULAR_TRANSACTION:
-        amount_pence = getattr(income_data, income_category, None)
-        if amount_pence:
-            regular_transactions.append(
-                {
-                    "category": INCOME_CATEGORY_TO_REGULAR_TRANSACTION[income_category],
-                    "operation": "credit",
-                    "frequency": "monthly",
-                    "amount": pence_to_pounds(amount_pence)
-                }
-            )
+        if hasattr(income_data, income_category):
+            amount_pence = getattr(income_data, income_category, None)
+            if amount_pence:
+                regular_transactions.append(
+                    {
+                        "category": INCOME_CATEGORY_TO_REGULAR_TRANSACTION[income_category],
+                        "operation": "credit",
+                        "frequency": "monthly",
+                        "amount": pence_to_pounds(amount_pence)
+                    }
+                )
     return {"regular_transactions": regular_transactions} if regular_transactions else {}

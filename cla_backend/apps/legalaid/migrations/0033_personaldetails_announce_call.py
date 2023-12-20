b1e94add5d8d795889d1ae95e99458677657942b
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-12-20 11:25:16 +0000

    Changed model to use NullBooleanField

--- cla_backend/apps/legalaid/migrations/0033_personaldetails_announce_call.py
+++ cla_backend/apps/legalaid/migrations/0033_personaldetails_announce_call.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='personaldetails',
             name='announce_call',
-            field=models.BooleanField(default=False),
+            field=models.NullBooleanField(),
         ),
     ]

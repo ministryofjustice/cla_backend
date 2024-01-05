42a7a8b9ef31cead304b6db4010e81803415de31
Stephen Dicks <github@starswan.com>
2024-01-05 14:09:43 +0000

    LEP-460 CFE instance varies between environments

--- cla_backend/settings/base.py
+++ cla_backend/settings/base.py
@@ -463,9 +463,7 @@ SESSION_SECURITY_PASSIVE_URLS = []
 
 EMAIL_ORCHESTRATOR_URL = os.environ.get("EMAIL_ORCHESTRATOR_URL")
 
-# This should eventually come from the environment, but we can hard-code it to main UAT for now
-# CFE_URL = os.environ.get("CFE_URL")
-CFE_URL = "https://main-cfe-civil-uat.cloud-platform.service.justice.gov.uk/v6/assessments"
+CFE_URL = os.environ.get("CFE_HOST", "https://main-cfe-civil-uat.cloud-platform.service.justice.gov.uk") + "/v6/assessments"
 
 # .local.py overrides all the common settings.
 try:

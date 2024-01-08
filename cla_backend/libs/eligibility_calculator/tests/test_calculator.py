6215a26115e7b47929486290823ad8f34305e03c
Stephen Dicks <github@starswan.com>
2024-01-08 08:15:09 +0000

    Lep-445 disputed savings (#1071)

--- cla_backend/libs/eligibility_calculator/tests/test_calculator.py
+++ cla_backend/libs/eligibility_calculator/tests/test_calculator.py
@@ -2159,3 +2159,12 @@ class DoCfeCivilCheckTestCase(unittest.TestCase):
         cfe_response = self.do_cfe_civil_check(checker)
         self.assertEqual('ineligible', cfe_response.overall_result)
 
+    def test_smod_capital_below_limit_is_ignored(self):
+        checker = self.checker_with_disputed_assets(50000 * 100)
+        cfe_response = self.do_cfe_civil_check(checker)
+        self.assertEqual('eligible', cfe_response.overall_result)
+
+    def test_smod_capital_above_limit_is_not_ignored(self):
+        checker = self.checker_with_disputed_assets(150000 * 100)
+        cfe_response = self.do_cfe_civil_check(checker)
+        self.assertEqual('ineligible', cfe_response.overall_result)

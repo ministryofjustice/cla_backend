97b85326987d7623264dba5aeaa184b4857d3257
David Read <david.read@digital.justice.gov.uk>
2024-01-08 15:35:09 +0000

    Comment is more reasonable, now I have found the Child Benefit input box

--- cla_backend/libs/eligibility_calculator/calculator.py
+++ cla_backend/libs/eligibility_calculator/calculator.py
@@ -451,8 +451,9 @@ class EligibilityChecker(object):
                 return False
             income = person.income
             income_keys_if_complete = set(income.PROPERTY_META.keys())
-            # cla_public doesn't explicitly ask about child benefit, but it removes the key from CaseDict if you say
-            # you get "Any other benefits"! So this key is not required for income to be considered complete
+            # cla_public will remove the `income.child_benefits` key from CaseDict if you submit /benefits without
+            # checking the child benefit checkbox (or you have no children_young). So this key is not required for
+            # income to be considered complete
             income_keys_if_complete.remove("child_benefits")
             for key in income_keys_if_complete:
                 if not hasattr(income, key):

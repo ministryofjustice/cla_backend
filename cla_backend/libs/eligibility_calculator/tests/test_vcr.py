381baaed4a68dcda2e25b1f7c89b7e4ee8d96d5e
Stephen Dicks <github@starswan.com>
2024-01-16 08:24:48 +0000

    moved files to directory relative to test so that they don't move depending on CWD

--- cla_backend/libs/eligibility_calculator/tests/test_vcr.py
+++ cla_backend/libs/eligibility_calculator/tests/test_vcr.py
@@ -1,5 +1,6 @@
 import functools
 import vcr
+import os
 
 custom_vcr = vcr.VCR()
 
@@ -7,7 +8,8 @@ custom_vcr = vcr.VCR()
 def use_vcr_cassette(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
-        cassette_name = "cassettes/" + f.__name__ + ".yml"
+        test_dir = os.path.dirname(f.func_code.co_filename)
+        cassette_name = test_dir + "/cassettes/" + f.func_name + ".yml"
         with custom_vcr.use_cassette(cassette_name):
             return f(*args, **kwargs)
     return wrapper

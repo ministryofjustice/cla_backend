13fbfefd91f967380a17dc00b841057e0588bb93
Stephen Dicks <github@starswan.com>
2024-02-01 11:10:52 +0000

    wip

--- cla_backend/libs/eligibility_calculator/cfe_civil/conversions.py
+++ cla_backend/libs/eligibility_calculator/cfe_civil/conversions.py
@@ -2,11 +2,8 @@ from decimal import Decimal
 
 
 def pence_to_pounds(pence):
-    if pence is None:
-        return 0
-    else:
-        decimal_value = (Decimal(pence) / 100).quantize(Decimal(".01"))
-        return float(decimal_value)
+    decimal_value = (Decimal(pence) / 100).quantize(Decimal(".01"))
+    return float(decimal_value)
 
 
 def none_filter(array):

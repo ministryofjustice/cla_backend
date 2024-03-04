603d32847849095bcd0c1558d13d2fe7c1f8e926
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-03-04 15:44:53 +0000

    Add in new migration

---
+++ cla_backend/apps/legalaid/migrations/0034_create_anon_user.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("legalaid", "0033_personaldetails_announce_call")]
+
+    operations = [
+        migrations.RunSQL(
+            [("CREATE ROLE analytics WITH LOGIN")],
+            [("GRANT CONNECT ON DATABASE 'cla_backend' TO analytics")],
+            [("CREATE USER metabase WITH PASSWORD 'password'")],
+            [("GRANT analytics TO metabase")],
+        )
+    ]

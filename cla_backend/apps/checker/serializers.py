79da661a6862bb1a6fd732b16e8a5b43fab8a70d
said-moj <45761276+said-moj@users.noreply.github.com>
2024-03-06 14:36:49 +0000

    Update times

--- cla_backend/apps/checker/serializers.py
+++ cla_backend/apps/checker/serializers.py
@@ -22,7 +22,7 @@ from legalaid.serializers import (
 
 from legalaid.models import Case, EligibilityCheck
 
-from checker.models import ReasonForContacting, ReasonForContactingCategory, CallbackTimeSlot
+from checker.models import ReasonForContacting, ReasonForContactingCategory, CallbackTimeSlot, CALLBACK_TIME_SLOTS
 from core.serializers import ClaModelSerializer
 
 checker_graph = SimpleLazyObject(lambda: get_graph(file_name=settings.CHECKER_DIAGNOSIS_FILE_NAME))
@@ -217,6 +217,12 @@ class ReasonForContactingSerializer(ClaModelSerializer):
 
 
 class CallbackTimeSlotSerializer(ClaModelSerializer):
+    time_display = serializers.SerializerMethodField(source="get_time_display")
+
+    @staticmethod
+    def get_time_display(instance):
+        return CALLBACK_TIME_SLOTS.CHOICES_DICT[instance.time]
+
     class Meta(object):
         model = CallbackTimeSlot
-        fields = ("date", "time", "capacity")
+        fields = ("date", "time", "time_display", "capacity")

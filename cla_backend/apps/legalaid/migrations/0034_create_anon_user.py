05074ed85500958a7bc4399e83318a23952bcba8
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-03-11 11:05:18 +0000

    Add in K8 secret for Analytics password

--- cla_backend/apps/legalaid/migrations/0034_create_anon_user.py
+++ cla_backend/apps/legalaid/migrations/0034_create_anon_user.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 
 from django.db import migrations
 
@@ -7,11 +8,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [("legalaid", "0033_personaldetails_announce_call")]
+    password = os.environ["ANALYTICS_DB_PASSWORD"]
 
     operations = [
         migrations.RunSQL(
             [
-                "CREATE ROLE analytics WITH LOGIN PASSWORD 'password'",
+                "CREATE ROLE analytics WITH LOGIN PASSWORD '{password}'",
                 "GRANT CONNECT ON DATABASE cla_backend TO analytics",
             ]
         )

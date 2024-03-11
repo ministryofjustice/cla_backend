5a8509ed78c5538de11b1d776533d98ce5f26667
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-03-11 15:48:50 +0000

    Add in get to environ

--- cla_backend/apps/legalaid/migrations/0034_create_anon_user.py
+++ cla_backend/apps/legalaid/migrations/0034_create_anon_user.py
@@ -8,12 +8,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [("legalaid", "0033_personaldetails_announce_call")]
-    password = os.environ["ANALYTICS_DB_PASSWORD"]
+
+    db_password = os.environ.get("ANALYTICS_DB_PASSWORD")
 
     operations = [
         migrations.RunSQL(
             [
-                "CREATE ROLE analytics WITH LOGIN PASSWORD '{password}'",
+                "CREATE ROLE analytics WITH LOGIN PASSWORD '{db_password}'",
                 "GRANT CONNECT ON DATABASE cla_backend TO analytics",
             ]
         )

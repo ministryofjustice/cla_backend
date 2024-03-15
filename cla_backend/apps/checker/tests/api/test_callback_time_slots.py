44a0695a6ac44010984f004d17116c023e9ed84a
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-03-15 23:10:31 +0000

    Added third party unit test

--- cla_backend/apps/checker/tests/api/test_callback_time_slots.py
+++ cla_backend/apps/checker/tests/api/test_callback_time_slots.py
@@ -10,31 +10,72 @@ from checker.call_centre_availability import get_available_slots, DATE_KEY_FORMA
 class CallbackTimeSlotsTestCase(SimpleResourceAPIMixin, CLACheckerAuthBaseApiTestMixin, APITestCase):
     LOOKUP_KEY = "id"
     API_URL_BASE_NAME = "callback_time_slots"
-    RESOURCE_RECIPE = "checker.callback_time_slot"
+    CALLBACK_TIME_SLOT = "checker.callback_time_slot"
 
     def setup_resources(self):
         pass
 
     # Make sure all times are available
     @mock.patch("cla_common.call_centre_availability.OpeningHours.available", return_value=True)
-    def test_slots(self, mock_opening_hours_available):
+    def test_slots(self, _):
         # Create callback time slots with capacity
         tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
-        make_recipe("checker.callback_time_slot", capacity=2, date=tomorrow, time="0900")
-        make_recipe("checker.callback_time_slot", capacity=1, date=tomorrow, time="1000")
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=2, date=tomorrow, time="0900")
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=1, date=tomorrow, time="1000")
         # This callback has no capacity so it should not be bookable
-        make_recipe("checker.callback_time_slot", capacity=0, date=tomorrow, time="1100")
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=0, date=tomorrow, time="1100")
         slots = get_available_slots(num_days=2)
         slot = slots[tomorrow.strftime(DATE_KEY_FORMAT)]
-        self.assertTrue("0900" in slot)
-        self.assertTrue("1000" in slot)
-        self.assertFalse("1100" in slot)
+        self.assertIn("0900", slot)
+        self.assertIn("1000", slot)
+        self.assertNotIn("1100", slot)
 
         # Book a callback for 10am tomorrow. 10am slot only has a capacity of 1, after this it should not be available
         requires_action_at = datetime.datetime.combine(tomorrow, datetime.time(hour=10, minute=0))
         make_recipe("legalaid.case", requires_action_at=requires_action_at)
         slots = get_available_slots(num_days=2)
         slot = slots[tomorrow.strftime(DATE_KEY_FORMAT)]
-        self.assertTrue("0900" in slot)
-        self.assertFalse("1000" in slot)
-        self.assertFalse("1100" in slot)
+        self.assertIn("0900", slot)
+        self.assertNotIn("1000", slot)
+
+    @mock.patch("cla_common.call_centre_availability.OpeningHours.available", return_value=True)
+    def test_negative_capacity(self, _):
+        # This handles the case where an operator overrides the capacity rules and the capacity becomes negative.
+        tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=1, date=tomorrow, time="0900")
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=1, date=tomorrow, time="1000")
+
+        slots = get_available_slots(num_days=2)
+        slot = slots[tomorrow.strftime(DATE_KEY_FORMAT)]
+        self.assertIn("0900", slot)
+        self.assertIn("1000", slot)
+
+        # Book two callbacks for 10am tomorrow, this should make the capacity negative
+        requires_action_at = datetime.datetime.combine(tomorrow, datetime.time(hour=10, minute=0))
+        for _ in range(2):
+            make_recipe("legalaid.case", requires_action_at=requires_action_at)
+        slots = get_available_slots(num_days=2)
+        slot = slots[tomorrow.strftime(DATE_KEY_FORMAT)]
+        self.assertIn("0900", slot)
+        self.assertNotIn("1000", slot)
+
+    @mock.patch("cla_common.call_centre_availability.OpeningHours.available", return_value=True)
+    def test_get_third_party_slots(self, _):
+        # Third part callbacks should not care about capacity.
+        tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=1, date=tomorrow, time="0900")
+        make_recipe(self.CALLBACK_TIME_SLOT, capacity=1, date=tomorrow, time="1000")
+
+        slots = get_available_slots(num_days=2)
+        slot = slots[tomorrow.strftime(DATE_KEY_FORMAT)]
+        self.assertIn("0900", slot)
+        self.assertIn("1000", slot)
+
+        # Book a callback for 10am tomorrow
+        requires_action_at = datetime.datetime.combine(tomorrow, datetime.time(hour=10, minute=0))
+        make_recipe("legalaid.case", requires_action_at=requires_action_at)
+
+        slots = get_available_slots(num_days=2, is_third_party_callback=True)
+        slot = slots[tomorrow.strftime(DATE_KEY_FORMAT)]
+        self.assertIn("0900", slot)
+        self.assertIn("1000", slot)

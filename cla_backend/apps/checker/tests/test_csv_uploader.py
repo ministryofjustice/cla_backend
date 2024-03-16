803e5a5113c99106c3b4a96113e2bdc785cab41a
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-03-16 23:32:01 +0000

    Fixed linting escape sequence

--- cla_backend/apps/checker/tests/test_csv_uploader.py
+++ cla_backend/apps/checker/tests/test_csv_uploader.py
@@ -20,7 +20,7 @@ class TestValidateRow(TestCase):
 
     def test_malformed_date(self):
         malformed_csv_dates = [['-12/05/2024', '1330', '5'],
-                               ['31/2/2024', '1330', '5'],
+                               ['31/2/2025', '1330', '5'],
                                ['29/2/2025', '1330', '5'],
                                ['29-2-2025', '1330', '5'],
                                ['2922025', '1330', '5'],
@@ -32,18 +32,18 @@ class TestValidateRow(TestCase):
             self.assertRaisesRegexp(ValidationError, "Write the date in this format: dd/mm/yyyy", self.csv_importer.validate_row, row)
 
     def test_malformed_time(self):
-        malformed_csv_times = [['12/05/2024', '-1330', '5'],
-                               ['31/03/2024', 'abcd', '999999'],
-                               ['12/5/1999', '0901', '5'],
-                               ['12/06/2024', '900', '0'],
+        malformed_csv_times = [['12/06/2024', '-1330', '5'],
+                               ['31/04/2024', 'abcd', '999999'],
+                               ['12/5/2002', '0901', '5'],
+                               ['13/06/2024', '900', '0'],
                                ['12/05/2040', '9', '1234'],
                                ['1/1/2015', '', '5'],
-                               ['1/1/2015', ',', '5']]
+                               ['1/2/2015', ',', '5']]
         for row in malformed_csv_times:
-            self.assertRaisesRegexp(ValidationError, "Check the time is correct, for example, 1500 \(for the 1500 to 1530 slot\)", self.csv_importer.validate_row, row)
+            self.assertRaisesRegexp(ValidationError, "Check the time is correct, for example, 1500 \\(for the 1500 to 1530 slot\\)", self.csv_importer.validate_row, row)
 
     def test_malformed_capacity(self):
-        malformed_csv_capacity = [['12/05/2024', '1330', ''],
+        malformed_csv_capacity = [['12/08/2024', '1330', ''],
                                   ['31/03/2024', '0900', '1.1'],
                                   ['12/5/1999', '1330', 'a']]
         for row in malformed_csv_capacity:

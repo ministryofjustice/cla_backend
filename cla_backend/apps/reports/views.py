a55cb869e09591b61c3e7ffc5efc19b1e738eee4
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-03-18 13:08:00 +0000

    Fixed linting

--- cla_backend/apps/reports/views.py
+++ cla_backend/apps/reports/views.py
@@ -263,16 +263,19 @@ def download_file(request, file_name="", *args, **kwargs):
 @permission_required("legalaid.run_reports")
 def case_demographic_report(request):
     return report_view(request, CaseDemographicsReport, "Case Demographic Report")
-  
+
+
 @staff_member_required
 @permission_required("legalaid.run_reports")
 def minimal_case_demographic_report(request):
     return report_view(request, MinimalCaseDemographicsReport, "Minimal Case Demographic Report")
 
+
 @staff_member_required
 @permission_required("legalaid.run_reports")
 def problem_categorisation(request):
     return report_view(request, MITellUsMoreAboutYourProblem, "Tell us more about your problem")
+
 
 def delete_record(user_id, file_name):
     try:

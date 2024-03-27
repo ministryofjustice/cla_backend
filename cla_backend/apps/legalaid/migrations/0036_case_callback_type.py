14e9d6584384770370cdfcf16423221208f1b56a
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-03-27 14:08:32 +0000

    Renamed callback migration to resolve conflict

--- cla_backend/apps/legalaid/migrations/0036_case_callback_type.py
+++ cla_backend/apps/legalaid/migrations/0036_case_callback_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("legalaid", "0033_personaldetails_announce_call")]
+    dependencies = [("legalaid", "0035_grant_anon_user_permissions")]
 
     operations = [
         migrations.AddField(

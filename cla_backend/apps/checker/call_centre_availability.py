1ca2dc352463afb4d143278a67784b54ef74c7df
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-04-15 12:19:23 +0100

    Returns a list of callback times rather than full case objects

--- cla_backend/apps/checker/call_centre_availability.py
+++ cla_backend/apps/checker/call_centre_availability.py
@@ -2,12 +2,13 @@ from cla_common.constants import OPERATOR_HOURS, CALLBACK_TYPES
 from cla_common.call_centre_availability import OpeningHours, current_datetime, SLOT_INTERVAL_MINS
 from checker.models import CallbackTimeSlot
 from checker.utils import get_timeslot_of_datetime
+from django.utils import timezone
 from legalaid.models import Case
 import datetime
 
 
 class CheckerOpeningHours(OpeningHours):
-    def time_slots(self, cases_in_range, day=None, is_third_party_callback=False):
+    def time_slots(self, callback_times, day=None, is_third_party_callback=False):
         """Gets all callback time slots on a given day.
         If is_third_party_callback = False capacity is ignored,
         otherwise the capacity will be based from the set of cases in range.
@@ -28,28 +29,27 @@ class CheckerOpeningHours(OpeningHours):
         def has_capacity(slot):
             key = "%s %s" % (day.strftime(DATE_KEY_FORMAT), slot.strftime("%H%M"))
             if key not in capacity:
-                return self.check_capacity_compared_to_prev_week(slot, cases_in_range)
+                return self.check_capacity_compared_to_prev_week(slot, callback_times)
             return capacity[key] > 0
 
-        capacity = self.get_callback_with_capacity(day, cases_in_range)
+        capacity = self.get_callback_with_capacity(day, callback_times)
         slots = filter(has_capacity, slots)
         return slots
 
-    def get_callback_with_capacity(self, day, cases):
+    def get_callback_with_capacity(self, day, callback_times):
         callback_slots = CallbackTimeSlot.objects.filter(date=day).all()
         capacity = {}
         for callback_slot in callback_slots:
             key = "%s %s" % (day.strftime(DATE_KEY_FORMAT), callback_slot.time)
-            used_capacity = cases.filter(
-                requires_action_at__range=(
-                    callback_slot.callback_start_datetime(),
-                    callback_slot.callback_end_datetime() - datetime.timedelta(seconds=1),
-                )
-            ).count()
+            used_capacity = count_callbacks_in_range(
+                callback_times,
+                callback_slot.callback_start_datetime(),
+                callback_slot.callback_end_datetime() - datetime.timedelta(seconds=1),
+            )
             capacity[key] = callback_slot.capacity - used_capacity
         return capacity
 
-    def check_capacity_compared_to_prev_week(self, slot_dt, cases_in_range):
+    def check_capacity_compared_to_prev_week(self, slot_dt, callback_times):
         """In the situation where no CallbackTimeSlot capacity has been defined for a given date
         the capacity of same timeslot for the previous week should be used.
 
@@ -59,12 +59,14 @@ class CheckerOpeningHours(OpeningHours):
         Returns:
             Bool: If the slot would have capacity when compared to the capacity set by the previous week's slot.
         """
+        slot_dt = timezone.make_aware(slot_dt)
         previous_timeslot = get_timeslot_of_datetime(slot_dt - datetime.timedelta(weeks=1))
         if not previous_timeslot:
             return True  # If there are no timeslots found for the previous year then fallback safely.
-        num_slots_booked = cases_in_range.filter(
-            requires_action_at__range=(slot_dt, slot_dt + datetime.timedelta(minutes=SLOT_INTERVAL_MINS))
-        ).count()
+
+        num_slots_booked = count_callbacks_in_range(
+            callback_times, slot_dt, slot_dt + datetime.timedelta(minutes=SLOT_INTERVAL_MINS)
+        )
 
         # Compares the slots booked in the current week's timeslot to last weeks' capacity.
         return num_slots_booked < previous_timeslot.capacity
@@ -74,20 +76,38 @@ DATE_KEY_FORMAT = "%Y%m%d"
 CALL_CENTER_HOURS = CheckerOpeningHours(**OPERATOR_HOURS)
 
 
-def get_cases_with_callbacks_in_range(start_dt, end_dt):
-    """Gets all cases which have a callback booked via the Checker web form, excluding third party callbacks.
+def get_list_callback_times(start_dt, end_dt):
+    """ Gets a list of requested callback times made via the web form within the given range.
+        Excludes callbacks requested for a third party.
 
     Args:
         start_dt (datetime.datetime): Start of time range
         end_dt (datetime.datetime): End of time range
 
     Returns:
-        django.QuerySet: QuerySet of all relevant cases.
+        list[datetime.datetime]: List of requested callback times.
     """
     valid_cases = Case.objects.filter(
         requires_action_at__range=(start_dt, end_dt), callback_type=CALLBACK_TYPES.CHECKER_SELF
-    ).all()
+    ).values_list("requires_action_at", flat=True)
     return valid_cases
+
+
+def count_callbacks_in_range(callback_times, start_dt, end_dt):
+    """Returns the number of callbacks that are in the given range.
+
+    Args:
+        callback_times (list[datetime.datetime]): List of callback datetimes
+        start_dt (datetime.datetime]): Start of the time range
+        end_dt (datetime.datetime]): End of the time range
+
+    Returns:
+        Int: Num callbacks in the given range
+    """
+    times_in_range = list(
+        filter(lambda callback_time: callback_time >= start_dt and callback_time < end_dt, callback_times)
+    )
+    return len(times_in_range)
 
 
 def get_available_slots(num_days=7, is_third_party_callback=False):
@@ -107,13 +127,11 @@ def get_available_slots(num_days=7, is_third_party_callback=False):
     if num_days > 1:
         days.extend(CALL_CENTER_HOURS.available_days(num_days - 1))
 
-    # As making a Case query is expensive, we want to make a single query for relevant cases and compare all time slots to this set of cases.
-    cases_with_callbacks_in_range = get_cases_with_callbacks_in_range(start_dt, end_dt)
+    # As making a Case query is expensive, we want to make a single query for relevant callback times and compare all time slots to this set of times.
+    callback_times = get_list_callback_times(start_dt, end_dt)
 
     valid_slots = []
     for day in days:
-        valid_slots.extend(
-            CALL_CENTER_HOURS.time_slots(cases_with_callbacks_in_range, day.date(), is_third_party_callback)
-        )
+        valid_slots.extend(CALL_CENTER_HOURS.time_slots(callback_times, day.date(), is_third_party_callback))
 
     return valid_slots

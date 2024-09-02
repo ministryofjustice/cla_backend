f971ceaa6fda0125c5e1db634decd3292c16fe96
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-09-02 13:26:50 +0100

    Add additional IPs from shared LAA whitelist (#1237)
    * Add additional IPs from shared LAA whitelist
    
    * Remove duplicate IP addresses

--- bin/training_deploy.sh
+++ bin/training_deploy.sh
@@ -4,6 +4,10 @@ set -e
 ROOT=$(dirname "$0")
 HELM_DIR="$ROOT/../helm_deploy/cla-backend/"
 
+# Pull ranges from shared LAA IP ranges and then remove spaces,
+# replace linebreaks with commas, remove last comma, and escape commas for helm input
+SHARED_IP_RANGES_LAA=$(curl -s https://raw.githubusercontent.com/ministryofjustice/laa-ip-allowlist/main/cidrs.txt | tr -d ' ' | tr '\n' ',' | sed 's/,/\\,/g' | sed 's/\\,$//')
+
 helm upgrade $RELEASE_NAME \
   $HELM_DIR \
   --namespace=${KUBE_ENV_TRAINING_NAMESPACE} \
@@ -15,4 +19,5 @@ helm upgrade $RELEASE_NAME \
   --set image.repository=$DOCKER_REPOSITORY \
   --set image.tag=$IMAGE_TAG \
   --set-string pingdomIPs=$PINGDOM_IPS \
+  --set-string sharedIPRangesLAA=$SHARED_IP_RANGES_LAA \
   --install
--- bin/uat_deploy.sh
+++ bin/uat_deploy.sh
@@ -11,6 +11,10 @@ fi
 
 echo "Using values file:$VALUES"
 
+# Pull ranges from shared LAA IP ranges and then remove spaces,
+# replace linebreaks with commas, remove last comma, and escape commas for helm input
+SHARED_IP_RANGES_LAA=$(curl -s https://raw.githubusercontent.com/ministryofjustice/laa-ip-allowlist/main/cidrs.txt | tr -d ' ' | tr '\n' ',' | sed 's/,/\\,/g' | sed 's/\\,$//')
+
 helm upgrade $RELEASE_NAME \
   $HELM_DIR \
   --namespace=${KUBE_ENV_UAT_NAMESPACE} \
@@ -23,4 +27,5 @@ helm upgrade $RELEASE_NAME \
   --set image.repository=$DOCKER_REPOSITORY \
   --set image.tag=$IMAGE_TAG \
   --set-string pingdomIPs=$PINGDOM_IPS \
+  --set-string sharedIPRangesLAA=$SHARED_IP_RANGES_LAA \
   --install

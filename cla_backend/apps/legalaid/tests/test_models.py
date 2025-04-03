1dab75f09b05ae476809cb886d7224fa286b8a93
Ben Millar <ben.millar@digital.justice.gov.uk>
2025-04-03 07:37:15 +0100

    Add client notes to tests

--- cla_backend/apps/legalaid/tests/test_models.py
+++ cla_backend/apps/legalaid/tests/test_models.py
@@ -98,6 +98,7 @@ def get_full_case(matter_type1, matter_type2, provider=None):
         provider=provider,
         notes="Notes",
         provider_notes="Provider Notes",
+        client_notes="Client Notes",
         thirdparty_details=make_recipe("legalaid.thirdparty_details"),
         adaptation_details=make_recipe("legalaid.adaptation_details"),
         billable_time=2000,
@@ -1340,6 +1341,7 @@ class SplitCaseTestCase(CloneModelsTestCaseMixin, TestCase):
         self.assertEqual(new_case.requires_action_by, REQUIRES_ACTION_BY.OPERATOR)
         self.assertEqual(new_case.notes, "")
         self.assertEqual(new_case.provider_notes, "")
+        self.assertEqual(new_case.client_notes, "")
         self.assertNotEqual(case.laa_reference, new_case.laa_reference)
         self.assertEqual(new_case.billable_time, 0)
         self.assertEqual(new_case.matter_type1, self.cat2_data.matter_type1)
@@ -1412,6 +1414,7 @@ class SplitCaseTestCase(CloneModelsTestCaseMixin, TestCase):
             "personal_details",
             "notes",
             "provider_notes",
+            "client_notes",
             "media_code",
             "exempt_user",
             "exempt_user_reason",
@@ -1460,6 +1463,7 @@ class SplitCaseTestCase(CloneModelsTestCaseMixin, TestCase):
             # it should keep these values from the original case
             "notes": case.notes,
             "provider_notes": case.provider_notes,
+            "client_notes": case.client_notes,
             "media_code": case.media_code,
             "source": case.source,
             "exempt_user": case.exempt_user,

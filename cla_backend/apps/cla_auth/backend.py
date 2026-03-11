fdbfe62f2f8cb91038f328699dd3d70462a456b1
Chalston Pinnock <chalston.pinnock@justice.gov.uk>
2026-03-11 16:40:52 +0000

    update on mapping new users and providers

--- cla_backend/apps/cla_auth/backend.py
+++ cla_backend/apps/cla_auth/backend.py
@@ -3,10 +3,12 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class EntraAccessTokenAuthenticationBackend(ModelBackend):
-    def authenticate(self, entra_id_email):
-        try:
-            # Get the user or create one if they don't exist
-            user = User.objects.get(email=entra_id_email, is_active=True)
-            return user
-        except Exception:
-            return None
+
+    def authenticate(self, entra_id_email=None):
+  
+            # Try to find existing user
+        user = User.objects.get(email=entra_id_email, is_active=True)
+        return user 
+        
+
+   

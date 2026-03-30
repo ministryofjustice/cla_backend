26d20d4679e9427844b6de66f1d8290dcc20abe6
joselopez-moj <Jose.Lopez@justice.gov.uk>
2026-03-30 13:56:52 +0100

    LGA-3925: add new columns to Digital Case report to include Other comms (#1381)
    * fix: add new columns to Digital Case report to include Other comms need requests
    
    * fix: change the logic for notes so they are added to report regardless of callback

--- cla_backend/apps/reports/sql/MIDigitalCaseTypesWithCategory.sql
+++ cla_backend/apps/reports/sql/MIDigitalCaseTypesWithCategory.sql
@@ -10,6 +10,15 @@ SELECT
   ,c.outcome_code
   ,c.created
   ,COALESCE(NULLIF(s.category ->> 'code', ''), diag_category.code) AS category_code
+  ,CASE c.callback_type
+    WHEN 'web_form_self' THEN 'call_me_back'
+    WHEN 'web_form_third_party' THEN 'call_someone_else'
+    ELSE 'i_will_call'
+  END
+  ,pd.email
+  ,ad.bsl_webcam
+  ,COALESCE(ad.notes != '', false) AS "other_comms_needs"
+  ,NULLIF(ad.notes, '') AS other_comms_notes
 FROM legalaid_case as c
   LEFT JOIN cla_eventlog_log as log on log.case_id = c.id and log.code = 'CASE_CREATED'
   LEFT OUTER JOIN auth_user u on u.id = c.created_by_id
@@ -17,6 +26,8 @@ FROM legalaid_case as c
   LEFT JOIN legalaid_category diag_category on d.category_id = diag_category.id
   LEFT JOIN checker_scopetraversal s on s.id = c.scope_traversal_id
   LEFT JOIN legalaid_eligibilitycheck e on c.eligibility_check_id = e.id
+  LEFT JOIN legalaid_personaldetails pd on c.personal_details_id = pd.id
+  LEFT JOIN legalaid_adaptationdetails ad on c.adaptation_details_id = ad.id
     WHERE
     c.created >= %s
     and c.created < %s;

2033991d66e4d2cb8840f0fd0e01650951d7e8cc
said-moj <45761276+said-moj@users.noreply.github.com>
2026-04-30 13:45:52 +0100

    Update operator manager to match that of whats on NLE

--- cla_backend/apps/cla_auth/constants.py
+++ cla_backend/apps/cla_auth/constants.py
@@ -1,4 +1,4 @@
 OPERATOR_ROLE = "Civil Legal Advice - Helpline Operator"
-OPERATOR_MANAGER_ROLE = "Civil Legal Advice - Helpline Operator Manager"
+OPERATOR_MANAGER_ROLE = "Civil Legal Advice - Operator Manager"
 PROVIDER_ROLE = "Civil Legal Advice - Helpline Provider"
 PROVIDER_MCC_ROLE = "MCC Specialist Provider"

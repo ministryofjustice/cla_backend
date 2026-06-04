f1511837de4ecaa13ebdb45823443508f22bfa4b
said-moj <45761276+said-moj@users.noreply.github.com>
2026-06-04 14:18:25 +0100

    Change mcc role

--- cla_backend/apps/cla_auth/constants.py
+++ cla_backend/apps/cla_auth/constants.py
@@ -1,4 +1,4 @@
 OPERATOR_ROLE = "Civil Legal Advice - Helpline Operator"
 OPERATOR_MANAGER_ROLE = "Civil Legal Advice - Helpline Operator Manager"
 PROVIDER_ROLE = "Civil Legal Advice - Helpline Provider"
-PROVIDER_MCC_ROLE = "MCC Specialist Provider"
+PROVIDER_MCC_ROLE = "Manage your civil cases - Access"

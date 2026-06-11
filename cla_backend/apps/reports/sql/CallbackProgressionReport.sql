26faeaa1fe23bb3405183a19d24216fe7d5a92d9
joselopez-moj <jose.lopez@justice.gov.uk>
2026-06-11 16:13:14 +0100

    feat: improve SQL query to include all log details from CBs

--- cla_backend/apps/reports/sql/CallbackProgressionReport.sql
+++ cla_backend/apps/reports/sql/CallbackProgressionReport.sql
@@ -1,13 +1,15 @@
 SELECT
-  c.reference as Case_Ref
-  ,c.requires_action_at::date as callback_date
-  ,TO_CHAR(c.requires_action_at, 'HH24:MI') AS callback_slot
-  ,c.source
-  ,c.outcome_code as Callback_Type
-  ,CASE WHEN c.thirdparty_details_id IS NOT NULL THEN 'Y' ELSE 'N' END AS third_party
-  ,c.created::date as case_created_date
-  ,TO_CHAR(c.created, 'HH24:MI') AS case_created_time
-FROM legalaid_case c
-WHERE c.requires_action_at::date >= %(from_date)s
-  AND c.requires_action_at::date < %(to_date)s
-ORDER BY c.requires_action_at
+    c.reference AS case_reference,
+    trim((log.context->'requires_action_at')::text, '"')::timestamptz::date AS callback_date,
+    TO_CHAR(trim((log.context->'requires_action_at')::text, '"')::timestamptz, 'HH24:MI') AS booking_slot,
+    c.source,
+    log.code AS callback_type,
+    CASE WHEN c.thirdparty_details_id IS NOT NULL THEN 'Y' ELSE 'N' END AS third_party,
+    log.created::date AS created_on,
+    TO_CHAR(log.created, 'HH24:MI') AS created_at
+FROM cla_eventlog_log log
+JOIN legalaid_case c ON c.id = log.case_id
+WHERE log.code IN ('CB1', 'CB2', 'CB3', 'CB4')
+  AND trim((log.context->'requires_action_at')::text, '"')::timestamptz::date >= %(from_date)s
+  AND trim((log.context->'requires_action_at')::text, '"')::timestamptz::date < %(to_date)s
+ORDER BY c.reference, log.created

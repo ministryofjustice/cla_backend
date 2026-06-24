f30908e07aff4c4b1c3d4acf27c74de1021b21c6
charles <chalston.pinnock@justice.gov.uk>
2026-06-24 15:15:09 +0100

    adding the executeable to file

--- bin/automate_backup.sh
+++ bin/automate_backup.sh
@@ -7,7 +7,6 @@ createSnap() {
 
     new_fileName=$file_name-$current_time
     aws rds create-db-snapshot --region=eu-west-2 --db-instance-identifier $DB_IDENTIFIER --db-snapshot-identifier $new_fileName
-
 } 
 
 

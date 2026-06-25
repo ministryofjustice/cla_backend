bb3952ddb9a3c320fceac80ca7344e9a1390d6b4
charles <chalston.pinnock@justice.gov.uk>
2026-06-25 16:13:03 +0100

    still complaining about the path

--- Backups/automate_backup.sh
+++ Backups/automate_backup.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
+FROM amazon/aws-cli
 
+WORKDIR /app
 
-createSnap() {
-    current_time=$(date "+%Y%m%d-%H%M")
-    file_name="cla-backend-manual-snapshot"
+COPY . .
 
-    new_fileName=$file_name-$current_time
-    aws rds create-db-snapshot --region=eu-west-2 --db-instance-identifier $DB_IDENTIFIER --db-snapshot-identifier $new_fileName
-} 
+RUN chmod +x Backups/automate_backup.sh
 
-createSnap()
+CMD ["sh", "Backups/automate_backup.sh"]

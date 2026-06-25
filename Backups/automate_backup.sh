8eb1fc5687f182ac7790b4b01f1e9c7c6b91a16c
charles <chalston.pinnock@justice.gov.uk>
2026-06-25 12:13:07 +0100

    update ont the dockerfile

---
+++ Backups/automate_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+createSnap() {
+    current_time=$(date "+%Y%m%d-%H%M")
+    file_name="cla-backend-manual-snapshot"
+
+    new_fileName=$file_name-$current_time
+    aws rds create-db-snapshot --region=eu-west-2 --db-instance-identifier $DB_IDENTIFIER --db-snapshot-identifier $new_fileName
+} 
+
+
+createSnap()

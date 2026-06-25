bf8da033158a7fd36db72880956cb7fe1a24a733
charles <chalston.pinnock@justice.gov.uk>
2026-06-25 17:47:01 +0100

    wrong data inside file

--- Backups/automate_backup.sh
+++ Backups/automate_backup.sh
@@ -1,9 +1,12 @@
-FROM amazon/aws-cli
+#!/bin/sh
 
-WORKDIR /app
 
-COPY . .
+createSnap() {
+    current_time=$(date "+%Y%m%d-%H%M")
+    file_name="cla-backend-manual-snapshot"
 
-RUN chmod +x Backups/automate_backup.sh
+    new_fileName=$file_name-$current_time
+    aws rds create-db-snapshot --region=eu-west-2 --db-instance-identifier $DB_IDENTIFIER --db-snapshot-identifier $new_fileName
+} 
 
-CMD ["sh", "Backups/automate_backup.sh"]
+createSnap()

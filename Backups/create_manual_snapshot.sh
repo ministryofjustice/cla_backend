003e7b6a5f086bb85a5e5bb3ce81a3d3c7589e6b
charles <chalston.pinnock@justice.gov.uk>
2026-06-30 16:31:36 +0100

    update variable name to stays consistent

--- Backups/create_manual_snapshot.sh
+++ Backups/create_manual_snapshot.sh
@@ -3,6 +3,6 @@
 current_time=$(date "+%Y%m%d-%H%M")
 file_name="cla-backend-manual-snapshot"
 
-new_fileName=$file_name-$current_time
+new_file_name=$file_name-$current_time
 
-aws rds create-db-snapshot --region=eu-west-2 --db-instance-identifier $DB_IDENTIFIER --db-snapshot-identifier $new_fileName
+aws rds create-db-snapshot --region=eu-west-2 --db-instance-identifier $DB_IDENTIFIER --db-snapshot-identifier $new_file_name

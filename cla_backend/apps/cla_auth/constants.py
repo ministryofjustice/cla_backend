2760c88d55aeaa582176bd41c862ce37f2594ddb
joselopez-moj <jose.lopez@justice.gov.uk>
2026-09-03 11:05:50 +0100

    fix: add Contract_manager role to auth flow

--- cla_backend/apps/cla_auth/constants.py
+++ cla_backend/apps/cla_auth/constants.py
@@ -1,5 +1,6 @@
 OPERATOR_ROLE = "Civil Legal Advice - Helpline Operator"
 OPERATOR_MANAGER_ROLE = "Civil Legal Advice - Helpline Operator Manager"
+CONTRACT_MANAGER_ROLE = "Civil Legal Advice - Contract Manager"
 PROVIDER_ROLE = "Civil Legal Advice - Helpline Provider"
 PROVIDER_MCC_ROLE = "MCC Specialist Provider"
 ENTRA_ALLOWED_OFFICE_CODES = ["2Q209V", "2Q239C", "0U865U", "2R116F", "0L951D", "1G893T", "2Q724E", "2L632G", "1G969A"]
